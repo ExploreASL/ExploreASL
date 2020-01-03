86afaabf34d7e3a32d111b27f951581667cb1ab3
Petr <janpetr@saturn.fz-rossendorf.de>
2020-01-03 16:53:27 +0100

    Tanimoto added reference.

--- Functions/xASL_qc_TanimotoCoeff.m
+++ Functions/xASL_qc_TanimotoCoeff.m
@@ -1,7 +1,7 @@
-function TC = xASL_stat_TanimotoCoeff(im1, im2, imMask, type)
+function TC = xASL_qc_TanimotoCoeff(im1, im2, imMask, type)
 % Calculates the Tanimoto similarity coefficient for a binary/fuzzy set/continuous images.
 
-% FORMAT: TC = xASL_stat_TanimotoCoeff(im1, im2[, imMask, type])
+% FORMAT: TC = xASL_qc_TanimotoCoeff(im1, im2[, imMask, type])
 %
 % INPUT:
 %   im1    - First input image
@@ -15,14 +15,16 @@ function TC = xASL_stat_TanimotoCoeff(im1, im2, imMask, type)
 %   TC - Tanimoto similarity coefficient
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION: Compares images IM1 and IM2 over the mask IMMASK. TYPE specifies the input data type.
-% EXAMPLE: TC = xASL_stat_TanimotoCoeff([0,1],[0,0],[1,1],1)
+% EXAMPLE: TC = xASL_qc_TanimotoCoeff([0,1],[0,0],[1,1],1)
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % REFERENCES:
-%    Tanimoto, Taffee T. (17 Nov 1958). "An Elementary Mathematical theory of Classification and Prediction". 
+%    Tanimoto, Taffee T. (17 Nov 1958). "An Elementary Mathematical theory of Classification and Prediction".
 %        Internal IBM Technical Report. 1957
 %
-%    W. R. Crum, O. Camara and D. L. G. Hill, "Generalized Overlap Measures for Evaluation and Validation in 
+%    W. R. Crum, O. Camara and D. L. G. Hill, "Generalized Overlap Measures for Evaluation and Validation in
 %        Medical Image Analysis," in IEEE Transactions on Medical Imaging, vol. 25, no. 11, pp. 1451-1461, Nov. 2006.
+%    David C. Anastasiu, George Karypis, "Efficient identification of Tanimoto nearest neighbors",
+%        International Journal of Data Science and Analytics, 2017.
 % __________________________________
 % Copyright © 2015-2019 ExploreASL
 %
@@ -30,17 +32,17 @@ function TC = xASL_stat_TanimotoCoeff(im1, im2, imMask, type)
 
     % Parameter admin
 	if nargin < 2
-		error('xASL_stat_TanimotoCoeff: Need to have at least two inputs.');
+		error('xASL_qc_TanimotoCoeff: Need to have at least two inputs.');
 	end
-	
+
 	if ~isequal(size(im1),size(im2))
-		error('xASL_stat_TanimotoCoeff: The two input images need to have the same size.');
+		error('xASL_qc_TanimotoCoeff: The two input images need to have the same size.');
 	end
-	
+
 	if nargin < 3 || isempty(imMask)
 		imMask = ones(size(im1));
 	end
-	
+
 	if nargin < 4 || isempty(type)
 		if islogical(im1) && islogical(im2)
 			% Binary input
@@ -55,29 +57,29 @@ function TC = xASL_stat_TanimotoCoeff(im1, im2, imMask, type)
 			end
 		end
 	end
-	
+
 	imMask = imMask.*(1-isnan(im1)).*(1-isnan(im2));
 	imMask = imMask > 0;
 	im1 = im1(imMask);
 	im2 = im2(imMask);
-	
+
 	switch (type)
 		case 1
 			% Needs to have a logical input
 			if ~islogical(im1)
 				im1 = im1>0;
 			end
-			
+
 			if ~islogical(im2)
 				im2 = im2>0;
 			end
 			TC = sum(im1&im2)/sum(im1|im2);
-			
+
 		case 2
 			TC = sum(min([im1,im2],[],2))/sum(max([im1,im2],[],2));
 		case 3
 			TC = sum(im1.*im2) / (sum(im1.^2) + sum(im2.^2) - sum(im1.*im2));
 		otherwise
-			error('xASL_stat_TanimotoCoeff: Unknown type.');
+			error('xASL_qc_TanimotoCoeff: Unknown type.');
 	end
 end
