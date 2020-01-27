d4ec3e545f4a0c0227b1d50a406145a0326c2f14
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-01-27 11:13:59 +0100

    Update xASL_adm_GzipAllFiles.m
    allow folder zipping

--- Functions/xASL_adm_GzipAllFiles.m
+++ Functions/xASL_adm_GzipAllFiles.m
@@ -1,4 +1,4 @@
-function xASL_adm_GzipAllFiles(ROOT)
+function xASL_adm_GzipAllFiles(ROOT, bFolder)
 %xASL_adm_GzipAllFiles This function zips all temporary
 % files.
 %
@@ -9,27 +9,47 @@ function xASL_adm_GzipAllFiles(ROOT)
 % visually
 %
 % High priority files are those that can be used for re-processing
+% bFolder - set to true for zipping separate folders in the ROOT folder
+% only (not recursive) (OPTIONAL, DEFAULT = false, zip files)
 
+    if nargin<2 || isempty(bFolder)
+        bFolder = false;
+    end
     
-    PathList    = xASL_adm_GetFileList(ROOT, '^.*\.(nii|wav|bmp)$', 'FPListRec', [0 Inf]);
+    if bFolder
+        PathList = xASL_adm_GetFileList(ROOT, '^.*$', 'FPList', [0 Inf], true);
+        fprintf('\n%s\n',['G-zZzZipping ' num2str(length(PathList)) ' folders']);
+    else
+        PathList = xASL_adm_GetFileList(ROOT, '^.*\.(nii|wav|bmp)$', 'FPListRec', [0 Inf], false);
+        fprintf('\n%s\n',['G-zZzZipping ' num2str(length(PathList)) ' files']);
+    end
+    
     fprintf('\n%s\n',['G-zZzZipping ' num2str(length(PathList)) ' files']);
     fprintf('Progress:   ');
 
-    for iP=1:length(PathList)
-        xASL_TrackProgress(iP,length(PathList));
-        Fname = PathList{iP};
-        [~, ~, Fext] = xASL_fileparts(Fname);
-        if strcmp(Fext,'.nii')
-            try
-                if exist([Fname '.gz'],'file')
-                    delete([Fname '.gz']); 
+    for iList=1:length(PathList)
+        xASL_TrackProgress(iList,length(PathList));
+        
+        try
+            if bFolder
+               zip([PathList{iList} '.zip'], PathList{iList});
+               xASL_adm_DeleteFileList(PathList{iList}, '^.*$', true, [0 Inf]);
+               xASL_delete(PathList{iList});
+            else           
+
+                Fname = PathList{iList};
+                [~, ~, Fext] = xASL_fileparts(Fname);
+                if strcmp(Fext,'.nii') || strcmp(Fext,'.wav') || strcmp(Fext,'.bmp')
+                    if exist([Fname '.gz'],'file')
+                        delete([Fname '.gz']); 
+                    end
+                    gzip(Fname);
+                    delete(Fname);
                 end
-                gzip(Fname);
-                delete(Fname);
-            catch ME
-                warning('Something went wrong, continuing with next one');
-                fprintf('%s\n',ME.message);
             end
+        catch ME
+            warning('Something went wrong, continuing with next one');
+            fprintf('%s\n',ME.message);
         end
     end
     fprintf('\n');
