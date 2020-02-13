8186cdb56a4288f563aa57d3a99be70ff6d527d9
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-02-13 08:41:30 +0100

    Minor fixes & customscripts

--- CustomScripts/StudySpecificScripts/NOVICE2/LongitudinalFLAIR/Figure2.m
+++ CustomScripts/StudySpecificScripts/NOVICE2/LongitudinalFLAIR/Figure2.m
@@ -27,8 +27,9 @@ figure(1);imshow([ImOut{1};ImOut{2};ImOut{3};ImOut{4}])
 
 %% Same as above, but with different FLAIR for each cohort
 %% Figure 2
-
-TemplateRoot = '/home/hjmutsaerts/lood_storage/divi/Projects/novice/MRI/ExploreASLoutput/LongitudinalAnalysis/Population/Templates/';
+x = ExploreASL_Master('',0);
+TemplateRoot = '/Users/henk/Google Drive/DataCopy/NoviceFigure2';
+% TemplateRoot = '/home/hjmutsaerts/lood_storage/divi/Projects/novice/MRI/ExploreASLoutput/LongitudinalAnalysis/Population/Templates/';
 % TemplateRoot = 'C:\BackupWork\ASL\Novice\LongitudinalAnalysis\Population\Templates';
 
 Path_FLAIR{1} = fullfile(TemplateRoot, 'FLAIR_HC1.nii');
@@ -47,31 +48,30 @@ MaskIM = xASL_io_Nifti2Im(fullfile(x.MyPath,'External','SPMmodified','MapsAdded'
 for iPath=1:4
     FLAIRim{iPath} = xASL_io_Nifti2Im(Path_FLAIR{iPath});
     WMHim{iPath} = xASL_io_Nifti2Im(Path_WMH{iPath});
-    WMHim{iPath}(WMHim{iPath}<0) = 0;
-    % let's dilate the regions a bit
-    ROIs = unique(WMHim{iPath});
-    ROIs = ROIs(2:end);
-    NewIm = zeros(size(WMHim{iPath}));
-    for iRoi=1:length(ROIs)
-        TempMask = WMHim{iPath}==ROIs(iRoi);
-        TempMask = xASL_im_DilateErodeFull(TempMask,'dilate',xASL_im_DilateErodeSphere(1));
-        NewIm = NewIm+(double(TempMask).*ROIs(iRoi));
-    end
-    WMHim{iPath} = NewIm;
+    WMHim{iPath} = WMHim{iPath}>0;
+    % let's dilate the regions a bit -> not required
+%     ROIs = unique(WMHim{iPath});
+%     ROIs = ROIs(2:end);
+%     NewIm = zeros(size(WMHim{iPath}));
+%     for iRoi=1:length(ROIs)
+%         TempMask = WMHim{iPath}==ROIs(iRoi);
+%         TempMask = xASL_im_DilateErodeFull(TempMask,'dilate',xASL_im_DilateErodeSphere(1));
+%         NewIm = NewIm+(double(TempMask).*ROIs(iRoi));
+%     end
+%     WMHim{iPath} = NewIm;
 end
 
 x.S.TraSlices = x.S.slicesLarge(5:8);
 x.S.Square = false;
 
-
-% WMHim{2} = WMHim{1};
-
 for iPath=1:4
     if max(WMHim{iPath}(:))==0
-        ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath}}, [], [1], [], {x.S.gray}, 0, {MaskIM}, 0, [], 5);
+%         ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath}}, [], [1], [], {x.S.gray}, 0, {MaskIM}, 0, [], 5);
+        ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath}}, [], [1], [], {x.S.gray}, 0, [], 0, [], 5);
     else
-        ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath} WMHim{iPath}>1}, [], [1 1], [], {x.S.gray x.S.jet256}, 0, {MaskIM MaskIM}, 0, [], 5);
+%         ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath} WMHim{iPath}}, [], [1 1], [], {x.S.gray x.S.jet256}, 0, {MaskIM MaskIM}, 0, [], 5);
+        ImOut{iPath} = xASL_im_CreateVisualFig(x, {FLAIRim{iPath} WMHim{iPath}}, [], [1 1], [], {x.S.gray x.S.jet256}, 0, [], 0, [], 5);
     end
 end
 
-figure(1);imshow([ImOut{1};ImOut{2};ImOut{3};ImOut{4}])
+figure(1);imshow([ImOut{1};ImOut{2};ImOut{3};ImOut{4}],'InitialMagnification',250)
