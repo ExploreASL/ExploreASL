38ca25fc285cbef15f9213aac36bf8ab162d6607
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-02-22 20:18:28 +0100

    CustomScripts

--- CustomScripts/StudySpecificScripts/SABRE/ParallelProcessingSABRE_MacBook.sh
+++ CustomScripts/StudySpecificScripts/SABRE/ParallelProcessingSABRE_MacBook.sh
@@ -5,11 +5,24 @@ let nParallel=$nParallel/8;
 let nParallel=3;
 
 MatlabPath=/Applications/MATLAB_R2016b.app/bin/matlab; # find Matlab path
-DataParPath=/Users/henk/surfdrive/SABRE/analysis/DataParameters_HiQ.json; # CHANGE DIR
+DataParPath=/Users/henk/surfdrive/HolidayPics/CICERO_Nolan/analysis/DataParameters_HiQ.json; # CHANGE DIR
 xASLdir=/Users/henk/ExploreASL/ExploreASL;
 cd $xASLdir
 
 for (( i=1; i<=$nParallel; i++ ));
-#do echo Running instance $i";
-do screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);" &
+do echo Running instance $i";
+#do screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);" &
 done
+
+let iModule=2; # run ASL module, skip DARTEL module
+
+i=1;
+screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "cd $xASLdir;ExploreASL_Master('$DataParPath', true, true, $i, $nParallel, $iModule);" &
+i=2;
+screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "cd $xASLdir;ExploreASL_Master('$DataParPath', true, true, $i, $nParallel, $iModule);" &
+i=3;
+screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "cd $xASLdir;ExploreASL_Master('$DataParPath', true, true, $i, $nParallel, $iModule);" &
+i=4;
+screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "cd $xASLdir;ExploreASL_Master('$DataParPath', true, true, $i, $nParallel, $iModule);" &
+i=5;
+screen -dmSL SABRE$i $MatlabPath -nodesktop -nosplash -r "cd $xASLdir;ExploreASL_Master('$DataParPath', true, true, $i, $nParallel, $iModule);" &
