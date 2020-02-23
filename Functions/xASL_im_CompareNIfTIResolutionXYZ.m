c6a8645c5626923d0d0704e80dd3b765ba449363
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-02-23 14:00:23 +0100

    Add headers

---
+++ Functions/xASL_im_CompareNIfTIResolutionXYZ.m
@@ -0,0 +1,31 @@
+function [IsEqualResolution] = xASL_im_CompareNIfTIResolutionXYZ(PathNIfTI1, PathNIfTI2)
+%xASL_im_CompareNIfTIResolutionXYZ Compare 3D resolution between 2 NIfTIs
+%
+% FORMAT: [IsEqualResolution] = xASL_im_CompareNIfTIResolutionXYZ(PathNIfTI1, PathNIfTI2)
+%
+% INPUT:
+%   PathNIfTI1  - Path to first NIfTI file (REQUIRED)
+%   PathNIfTI2  - Path to second NIfTI file (REQUIRED)%
+%
+% OUTPUT:
+%   IsEqualResolution  - true if the 3D resolution is identical
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This function checks whether the X, Y and Z resolution of a
+%              NIfTI with any number of dimensions is equal. It rounds for 2 floating
+%              points, for both NIfTIs, to ensure that the same precision is compared.
+%
+% EXAMPLE: IsEqualResolution = xASL_im_CompareNIfTIResolutionXYZ('/ASL/MyStudyName/Sub-001/c1T1.nii', '/ASL/MyStudyName/Sub-001/c1T1.nii');
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% Copyright 2015-2019 ExploreASL
+
+
+nii1 = xASL_io_ReadNifti(PathNIfTI1);
+nii2 = xASL_io_ReadNifti(PathNIfTI2);
+
+res1 = nii1.hdr.pixdim(2:4);
+res2 = nii2.hdr.pixdim(2:4);
+
+IsEqualResolution = isequal(xASL_round(res1,2), xASL_round(res2,2));
+
+end
+
--- Functions/xASL_wrp_CreateASLDeformationField.m
+++ Functions/xASL_wrp_CreateASLDeformationField.m
@@ -1,57 +1,86 @@
 function xASL_wrp_CreateASLDeformationField(x, bOverwrite, EstimatedResolution)
-%xASL_wrp_CreateASLDeformationField When we can use a T1w deformation field, smooth it to the ASL resolution
-% Smooth T1w deformation fields to the ASL resolution
-% First estimate ASL resolution
-% Note that if the resolution of ASL is not significantly (i.e. >0.5 mm in
-% any dimension) lower than T1w, the y_T1.nii is copied to y_ASL.nii
+%xASL_wrp_CreateASLDeformationField Adapt deformation field for lower resolution
+% FORMAT: xASL_wrp_CreateASLDeformationField(x, bOverwrite, EstimatedResolution)
+%
+% INPUT:
+%   x                   - structure containing fields with all information required to run this submodule (REQUIRED)
+%   bOverwrite          - true for overwriting any existing ASL deformation field (OPTIONAL, DEFAULT = false)
+%   EstimatedResolution - X Y Z scalar with estimated effective spatial
+%                         resolution of images that flowfield will be applied to (OPTIONAL,
+%                         DEFAULT = obtain this automaticall)
+% OUTPUT: n/a %
+% --------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This function smooths a transformation flow field to a lower
+%              resolution. Usually, we use a high resolution anatomical
+%              image (e.g. 3D T1w) to obtain the flowfields from native
+%              space to standard space, and apply these to the lower
+%              resolution ASL images. Because of the resolution
+%              differences, the flowfields need to be downsampled/smoothed,
+%              to avoid deformation effects that are crispier than the
+%              functional image that is investigated. This function
+%              performs the following steps:
+%              1) Obtain resolutions
+%              2) Fill NaNs at edges y_T1.nii flowfield to prevent interpolation artifact
+%              3) Smooth flowfield
+%              4) Fill NaNs at edges y_ASL.nii
+% 
+%              Note that if the resolution of ASL is not significantly (i.e. >0.5 mm in
+%              any dimension) lower than T1w, the y_T1.nii is copied to y_ASL.nii
+% --------------------------------------------------------------------------------------------------------------------
+% EXAMPLES: xASL_wrp_CreateASLDeformationField(x);
+%           xASL_wrp_CreateASLDeformationField(x, 1, [3 3 7]);
+% __________________________________
+% Copyright (C) 2015-2019 ExploreASL
 
+
+%% Admin
 if nargin<2 || isempty(bOverwrite)
     bOverwrite = false; % By default, this function will be skipped if the ASL deformation field already exists
 end
 
+if xASL_exist(x.P.Path_y_ASL,'file') && ~bOverwrite
+    return; % skip this function
+elseif ~xASL_exist(x.P.Path_y_T1,'file')
+    warning([x.P.Path_y_T1 ' didnt exist, skipping...']);
+    return;
+end
+
+%% 1) Obtain resolutions
 if nargin<3 || isempty(EstimatedResolution)
 	EstimatedResolution = xASL_init_DefaultEffectiveResolution(x.P.Path_ASL4D, x);
 end
 
-if ~xASL_exist(x.P.Path_y_ASL,'file') || bOverwrite
+TemplateResolution = [1.5 1.5 1.5];
+sKernel = (EstimatedResolution.^2 - TemplateResolution.^2).^0.5; % assuming the flow fields are in 1.5x1.5x1.5 mm
+sKernel(EstimatedResolution<TemplateResolution) = 0;
 
-    if ~xASL_exist(x.P.Path_y_T1,'file')
-        warning([x.P.Path_y_T1 ' didnt exist, skipping...']);
-        return;
-    end
-    
-    TemplateResolution = [1.5 1.5 1.5];
-    sKernel = (EstimatedResolution.^2 - TemplateResolution.^2).^0.5; % assuming the flow fields are in 1.5x1.5x1.5 mm
-    sKernel(EstimatedResolution<TemplateResolution) = 0;
+%% 2) Fill NaNs at edges y_T1.nii flowfield to prevent interpolation artifact
+xASL_im_FixEdgesFlowfield(x.P.Path_y_T1); % First fill NaNs, to prevent interpolation artifacts
 
-    xASL_im_FixEdgesFlowfield(x.P.Path_y_T1); % First fill NaNs, to prevent smoothing artifacts
+%% 3) Smooth flowfield
+% Note that we provide the T1w resolution, not the resolution of y_T1,
+% which is the standard space resolution (as it pulls not pushes).
+niiT1w = xASL_io_ReadNifti(x.P.Path_T1);
+resSrc = niiT1w.hdr.pixdim(2:4);
 
-    % Perform the smoothing
-    % xASL_spm_smooth(x.P.Path_y_T1, sKernel,x.P.Path_y_ASL);
-    
-    % Note that we provide the T1w resolution, not the resolution of y_T1,
-    % which is the standard space resolution (as it pulls not pushes).
-    niiT1w = xASL_io_ReadNifti(x.P.Path_T1);
-    resSrc = niiT1w.hdr.pixdim(2:4);
-    
-    xASL_im_PreSmooth(x.P.Path_ASL4D, x.P.Path_y_T1, x.P.Path_y_ASL, [], resSrc); % we need to add the effective resolution here still!
-    % sKernel, as calculated above, can be used for this. But the major
-    % rotations need to be taken into account, between the effective
-    % resolution as specified, and the one in the different NIfTIs
-    % (e.g. the ASL & T1w are usually acquired transversal & sagittally,
-    % respectively
+xASL_im_PreSmooth(x.P.Path_ASL4D, x.P.Path_y_T1, x.P.Path_y_ASL, [], resSrc); % we need to add the effective resolution here still!
+% sKernel, as calculated above, can be used for this. But the major
+% rotations need to be taken into account, between the effective
+% resolution as specified, and the one in the different NIfTIs
+% (e.g. the ASL & T1w are usually acquired transversal & sagittally,
+% respectively
 
-    % Solve edges
-    FieldT1 = xASL_io_Nifti2Im(x.P.Path_y_T1);
-    FieldASL = xASL_io_Nifti2Im(x.P.Path_y_ASL);
-    FieldASL([1:5 end-5:end],:,:,:,:) = FieldT1([1:5 end-5:end],:,:,:,:); % x
-    FieldASL(:,[1:5 end-5:end],:,:,:) = FieldT1(:,[1:5 end-5:end],:,:,:); % y
-    FieldASL(:,:,[1:5 end-5:end],:,:) = FieldT1(:,:,[1:5 end-5:end],:,:); % z
-    xASL_io_SaveNifti(x.P.Path_y_ASL, x.P.Path_y_ASL, FieldASL, [], 0);
+%% 4) Fill NaNs at edges y_ASL.nii
+% Remove this part, not required when flowfield edge fixing below is
+% repaired by Jan
+% FieldT1 = xASL_io_Nifti2Im(x.P.Path_y_T1);
+% FieldASL = xASL_io_Nifti2Im(x.P.Path_y_ASL);
+% FieldASL([1:5 end-5:end],:,:,:,:) = FieldT1([1:5 end-5:end],:,:,:,:); % x
+% FieldASL(:,[1:5 end-5:end],:,:,:) = FieldT1(:,[1:5 end-5:end],:,:,:); % y
+% FieldASL(:,:,[1:5 end-5:end],:,:) = FieldT1(:,:,[1:5 end-5:end],:,:); % z
+% xASL_io_SaveNifti(x.P.Path_y_ASL, x.P.Path_y_ASL, FieldASL, [], 0);
 
-    xASL_im_FixEdgesFlowfield(x.P.Path_y_ASL); % Again fill NaNs, if needed
-end
-
+xASL_im_FixEdgesFlowfield(x.P.Path_y_ASL); % Again fill NaNs, if needed
 
 
 end
