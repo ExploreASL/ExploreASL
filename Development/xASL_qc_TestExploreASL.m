e63369359fa21791b0a732459a9eee1c5a609881
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-03-08 14:04:02 +0100

    Update xASL_qc_TestExploreASL.m

--- Development/xASL_qc_TestExploreASL.m
+++ Development/xASL_qc_TestExploreASL.m
@@ -1,4 +1,4 @@
-function [ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, RunMethod)
+function [ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, RunMethod, bTestSPM)
 %xASL_qc_TestExploreASL Do a thorough test of the validity and reproducibility of ExploreASL
 %
 % FORMAT: [ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, RunMethod)
@@ -11,6 +11,7 @@ function [ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, RunMe
 %                 Option 2 = run ExploreASl parallel (start new MATLAB instances)
 %                 FUTURE Option 3 = run ExploreASL compilation serially
 %                 FUTURE Option 4 = run ExploreASL compilation parallel
+$   bTestSPM    - boolean for testing if SPM standalone with xASL modifications works (DEFAULT=true)
 % OUTPUT:
 %   ResultsTable - Table containing all results from the test runs
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -46,6 +47,7 @@ function [ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, RunMe
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE for Jan: [ResultsTable] = xASL_qc_TestExploreASL('/pet/projekte/asl/data/ExploreASL_TestCases', '/pet/projekte/asl/data/ExploreASL_TempRes', 1);
 % EXAMPLE for Henk on MacOS: [ResultsTable] = xASL_qc_TestExploreASL('/Users/henk/surfdrive/HolidayPics/ExploreASL_TestCases', '/Users/henk/ExploreASL/ExploreASL_TestCasesProcessed', 1);
+% EXAMPLE for VUmc server: [ResultsTable] = xASL_qc_TestExploreASL('/radshare/ExploreASL_Test/ExploreASL_TestCases', '/radshare/ExploreASL_Test/ExploreASL_TestCasesProcessed', 1);
 % __________________________________
 % Copyright 2015-2019 ExploreASL
 
@@ -57,6 +59,10 @@ if isempty(which('ExploreASL_Master'))
 else
     cd(fileparts(which('ExploreASL_Master')));
 end
+if nargin<4 || isempty(bTestSPM)
+    bTestSPM = true;
+end
+
 
 % ============================================================
 %% 1) Pull latest GitHub version
@@ -98,102 +104,105 @@ xASL_Copy(TestDirOrig, TestDirDest);
 
 % ============================================================
 %% 4) Test standalone SPM on low quality
-x.Quality = false;
-% Find the first directory and copy out the first T1 and ASL just for SPM testing
+if bTestSPM
 
-Dlist = xASL_adm_GetFileList(TestDirDest,'^.*$','List',[0 Inf], true);
+    x.Quality = false;
+    % Find the first directory and copy out the first T1 and ASL just for SPM testing
 
-xASL_Copy(fullfile(TestDirDest,Dlist{1},'001DM_1','T1.nii'),fullfile(TestDirDest,'T1.nii'));
-xASL_Copy(fullfile(TestDirDest,Dlist{1},'001DM_1','ASL_1','ASL4D.nii'),fullfile(TestDirDest,'ASL4D.nii'));
+    Dlist = xASL_adm_GetFileList(TestDirDest,'^.*$','List',[0 Inf], true);
 
-xASL_io_ReadNifti(fullfile(TestDirDest,'ASL4D.nii'));
-xASL_io_ReadNifti(fullfile(TestDirDest,'T1.nii'));
+    xASL_Copy(fullfile(TestDirDest,Dlist{1},'001DM_1','T1.nii'),fullfile(TestDirDest,'T1.nii'));
+    xASL_Copy(fullfile(TestDirDest,Dlist{1},'001DM_1','ASL_1','ASL4D.nii'),fullfile(TestDirDest,'ASL4D.nii'));
 
-% Test spm_realign on low quality
-matlabbatch = [];
-matlabbatch{1}.spm.spatial.realign.write.data               = {fullfile(TestDirDest,'ASL4D.nii')};
-matlabbatch{1}.spm.spatial.realign.write.roptions.which     = [2 0];
-matlabbatch{1}.spm.spatial.realign.write.roptions.interp    = 0; % low quality
-matlabbatch{1}.spm.spatial.realign.write.roptions.wrap      = [0 0 0];
-matlabbatch{1}.spm.spatial.realign.write.roptions.mask      = 1;
-matlabbatch{1}.spm.spatial.realign.write.roptions.prefix    = 'r';
-spm_jobman('run',matlabbatch);
+    xASL_io_ReadNifti(fullfile(TestDirDest,'ASL4D.nii'));
+    xASL_io_ReadNifti(fullfile(TestDirDest,'T1.nii'));
+
+    % Test spm_realign on low quality
+    matlabbatch = [];
+    matlabbatch{1}.spm.spatial.realign.write.data               = {fullfile(TestDirDest,'ASL4D.nii')};
+    matlabbatch{1}.spm.spatial.realign.write.roptions.which     = [2 0];
+    matlabbatch{1}.spm.spatial.realign.write.roptions.interp    = 0; % low quality
+    matlabbatch{1}.spm.spatial.realign.write.roptions.wrap      = [0 0 0];
+    matlabbatch{1}.spm.spatial.realign.write.roptions.mask      = 1;
+    matlabbatch{1}.spm.spatial.realign.write.roptions.prefix    = 'r';
+    spm_jobman('run',matlabbatch);
 
 
-% Test spm_reslice on low quality
-xASL_spm_reslice(fullfile(TestDirDest,'ASL4D.nii'), fullfile(TestDirDest,'T1.nii'), [], [], 0);
+    % Test spm_reslice on low quality
+    xASL_spm_reslice(fullfile(TestDirDest,'ASL4D.nii'), fullfile(TestDirDest,'T1.nii'), [], [], 0);
 
-% Test spm_smooth on low quality (small kernel)
-xASL_spm_smooth(fullfile(TestDirDest,'rT1.nii'), [2 2 2], fullfile(TestDirDest,'rsT1.nii'));
+    % Test spm_smooth on low quality (small kernel)
+    xASL_spm_smooth(fullfile(TestDirDest,'rT1.nii'), [2 2 2], fullfile(TestDirDest,'rsT1.nii'));
 
-% Test coreg on low quality
-matlabbatch = [];
-matlabbatch{1}.spm.spatial.coreg.estimate.ref = {fullfile(TestDirDest,'ASL4D.nii,1')};
-matlabbatch{1}.spm.spatial.coreg.estimate.source = {fullfile(TestDirDest,'rT1.nii,1')};
-matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'mi';
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = 9;
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
-spm_jobman('run',matlabbatch);
+    % Test coreg on low quality
+    matlabbatch = [];
+    matlabbatch{1}.spm.spatial.coreg.estimate.ref = {fullfile(TestDirDest,'ASL4D.nii,1')};
+    matlabbatch{1}.spm.spatial.coreg.estimate.source = {fullfile(TestDirDest,'rT1.nii,1')};
+    matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'mi';
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = 9;
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
+    spm_jobman('run',matlabbatch);
 
-% Test CAT12
-matlabbatch = [];
-SPMTemplateNII    = fullfile(x.MyPath,'External','SPMmodified', 'tpm', 'TPM.nii');
-DartelTemplateNII = fullfile(x.MyPath,'External','SPMmodified', 'toolbox', 'cat12', 'templates_1.50mm', 'Template_1_IXI555_MNI152.nii');
-GSTemplateNII     = fullfile(x.MyPath,'External','SPMmodified', 'toolbox', 'cat12', 'templates_1.50mm', 'Template_0_IXI555_MNI152_GS.nii');
-matlabbatch{1}.spm.tools.cat.estwrite.opts.tpm                         = {SPMTemplateNII};
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.darteltpm   = {DartelTemplateNII}; % Runs DARTEL to this n=555 subjects template
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.shootingtpm = {GSTemplateNII}; % Runs Geodesic Shooting to this n=555 subjects template
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.regstr = 1;
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.APP           = 0; % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.LASstr        = 0; % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.gcutstr       = 0; % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.vox           = 6; % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.opts.biasstr          = eps; % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.opts.samp             = 9;   % low quality
-matlabbatch{1}.spm.tools.cat.estwrite.data                  = {fullfile(TestDirDest,'T1.nii')}; % T1.nii
-matlabbatch{1}.spm.tools.cat.estwrite.nproc                 = 0; 
-matlabbatch{1}.spm.tools.cat.estwrite.opts.affreg           = 'mni'; % regularize affine registration for MNI European brains
-matlabbatch{1}.spm.tools.cat.estwrite.output.surface        = 0;   % don't do surface modeling
-matlabbatch{1}.spm.tools.cat.estwrite.output.ROImenu.noROI  = struct([]); % don't do ROI estimations
-matlabbatch{1}.spm.tools.cat.estwrite.output.GM.native      = 0;   % save c1T1 in native space
-matlabbatch{1}.spm.tools.cat.estwrite.output.GM.mod         = 0;   % don't save modulation
-matlabbatch{1}.spm.tools.cat.estwrite.output.GM.dartel      = 0;   % don't save DARTEL space c1T1, this happens below in the reslice part
-matlabbatch{1}.spm.tools.cat.estwrite.output.WM.native      = 0;   % save c2T1 in native space
-matlabbatch{1}.spm.tools.cat.estwrite.output.WM.mod         = 0;   % don't save modulation
-matlabbatch{1}.spm.tools.cat.estwrite.output.WM.dartel      = 0;   % don't save DARTEL space c2T1, this happens below in the reslice part
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.native      = 0;   % save c3T1 in native space
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.mod         = 0;   % don't save modulation
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.dartel      = 0;   % don't save DARTEL space c2T1, this happens below in the reslice part
-matlabbatch{1}.spm.tools.cat.estwrite.output.jacobian.warped= 0;   % don't save Jacobians
-matlabbatch{1}.spm.tools.cat.estwrite.output.warps          = [1 0]; % save warp to MNI
-matlabbatch{1}.spm.tools.cat.estwrite.output.bias.warped    = 0;   % don't save bias-corrected T1.nii
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.native      = 0;   % save c3T1 in native space
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.mod         = 0;   % don't save modulation
-matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.dartel      = 0;
-matlabbatch{1}.spm.tools.cat.estwrite.extopts.restypes.fixed= [1 0.1]; % process everything on 1 mm fixed resolution (default)
-spm_jobman('run',matlabbatch);
+    % Test CAT12
+    matlabbatch = [];
+    SPMTemplateNII    = fullfile(x.MyPath,'External','SPMmodified', 'tpm', 'TPM.nii');
+    DartelTemplateNII = fullfile(x.MyPath,'External','SPMmodified', 'toolbox', 'cat12', 'templates_1.50mm', 'Template_1_IXI555_MNI152.nii');
+    GSTemplateNII     = fullfile(x.MyPath,'External','SPMmodified', 'toolbox', 'cat12', 'templates_1.50mm', 'Template_0_IXI555_MNI152_GS.nii');
+    matlabbatch{1}.spm.tools.cat.estwrite.opts.tpm                         = {SPMTemplateNII};
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.darteltpm   = {DartelTemplateNII}; % Runs DARTEL to this n=555 subjects template
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.shootingtpm = {GSTemplateNII}; % Runs Geodesic Shooting to this n=555 subjects template
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.registration.regstr = 1;
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.APP           = 0; % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.LASstr        = 0; % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.gcutstr       = 0; % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.vox           = 6; % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.opts.biasstr          = eps; % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.opts.samp             = 9;   % low quality
+    matlabbatch{1}.spm.tools.cat.estwrite.data                  = {fullfile(TestDirDest,'T1.nii')}; % T1.nii
+    matlabbatch{1}.spm.tools.cat.estwrite.nproc                 = 0; 
+    matlabbatch{1}.spm.tools.cat.estwrite.opts.affreg           = 'mni'; % regularize affine registration for MNI European brains
+    matlabbatch{1}.spm.tools.cat.estwrite.output.surface        = 0;   % don't do surface modeling
+    matlabbatch{1}.spm.tools.cat.estwrite.output.ROImenu.noROI  = struct([]); % don't do ROI estimations
+    matlabbatch{1}.spm.tools.cat.estwrite.output.GM.native      = 0;   % save c1T1 in native space
+    matlabbatch{1}.spm.tools.cat.estwrite.output.GM.mod         = 0;   % don't save modulation
+    matlabbatch{1}.spm.tools.cat.estwrite.output.GM.dartel      = 0;   % don't save DARTEL space c1T1, this happens below in the reslice part
+    matlabbatch{1}.spm.tools.cat.estwrite.output.WM.native      = 0;   % save c2T1 in native space
+    matlabbatch{1}.spm.tools.cat.estwrite.output.WM.mod         = 0;   % don't save modulation
+    matlabbatch{1}.spm.tools.cat.estwrite.output.WM.dartel      = 0;   % don't save DARTEL space c2T1, this happens below in the reslice part
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.native      = 0;   % save c3T1 in native space
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.mod         = 0;   % don't save modulation
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.dartel      = 0;   % don't save DARTEL space c2T1, this happens below in the reslice part
+    matlabbatch{1}.spm.tools.cat.estwrite.output.jacobian.warped= 0;   % don't save Jacobians
+    matlabbatch{1}.spm.tools.cat.estwrite.output.warps          = [1 0]; % save warp to MNI
+    matlabbatch{1}.spm.tools.cat.estwrite.output.bias.warped    = 0;   % don't save bias-corrected T1.nii
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.native      = 0;   % save c3T1 in native space
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.mod         = 0;   % don't save modulation
+    matlabbatch{1}.spm.tools.cat.estwrite.output.CSF.dartel      = 0;
+    matlabbatch{1}.spm.tools.cat.estwrite.extopts.restypes.fixed= [1 0.1]; % process everything on 1 mm fixed resolution (default)
+    spm_jobman('run',matlabbatch);
 
-% Test deformations after CAT12
-xASL_Copy(fullfile(TestDirDest,'mri','y_T1.nii'), fullfile(TestDirDest,'y_T1.nii'));
-matlabbatch = [];
-matlabbatch{1}.spm.util.defs.comp{1}.def = {fullfile(TestDirDest,'y_T1.nii')};
-matlabbatch{1}.spm.util.defs.out{1}.pull.fnames = {fullfile(TestDirDest,'rT1.nii')};
-matlabbatch{1}.spm.util.defs.out{1}.pull.savedir.savesrc = 1;
-matlabbatch{1}.spm.util.defs.out{1}.pull.interp = 0;
-matlabbatch{1}.spm.util.defs.out{1}.pull.mask = 1;
-matlabbatch{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
-matlabbatch{1}.spm.util.defs.out{1}.pull.prefix = '';
-spm_jobman('run',matlabbatch);
+    % Test deformations after CAT12
+    xASL_Copy(fullfile(TestDirDest,'mri','y_T1.nii'), fullfile(TestDirDest,'y_T1.nii'));
+    matlabbatch = [];
+    matlabbatch{1}.spm.util.defs.comp{1}.def = {fullfile(TestDirDest,'y_T1.nii')};
+    matlabbatch{1}.spm.util.defs.out{1}.pull.fnames = {fullfile(TestDirDest,'rT1.nii')};
+    matlabbatch{1}.spm.util.defs.out{1}.pull.savedir.savesrc = 1;
+    matlabbatch{1}.spm.util.defs.out{1}.pull.interp = 0;
+    matlabbatch{1}.spm.util.defs.out{1}.pull.mask = 1;
+    matlabbatch{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
+    matlabbatch{1}.spm.util.defs.out{1}.pull.prefix = '';
+    spm_jobman('run',matlabbatch);
 
-% Remove temporary derivatives
-xASL_adm_DeleteFileList(TestDirDest, '.*', false, [0 Inf]);
-DirsAre = {'err' 'ASL_1' 'mri' 'report'};
-for iDir=1:length(DirsAre)
-    DirIs = fullfile(TestDirDest,DirsAre{iDir});
-    xASL_adm_DeleteFileList(DirIs, '.*', true, [0 Inf]);
-    xASL_delete(DirIs);
+    % Remove temporary derivatives
+    xASL_adm_DeleteFileList(TestDirDest, '.*', false, [0 Inf]);
+    DirsAre = {'err' 'ASL_1' 'mri' 'report'};
+    for iDir=1:length(DirsAre)
+        DirIs = fullfile(TestDirDest,DirsAre{iDir});
+        xASL_adm_DeleteFileList(DirIs, '.*', true, [0 Inf]);
+        xASL_delete(DirIs);
+    end
 end
 
 % ============================================================
@@ -215,7 +224,7 @@ for iList=1:length(Dlist)
 	        case 1 % run ExploreASL serially
 	            ExploreASL_Master(DataParFile{iList}{1}, true, true); % can we run screen from here? or run matlab in background, linux easy
 	        case 2 % run ExploreASl parallel (start new MATLAB instances)
-	            MatlabRunString = 'matlab -nodesktop -nosplash -r ';
+	            MatlabRunString = ['screen -dmSL ' TestxASL_' numstr(iList) ' -matlab -nodesktop -nosplash -r '];
 	            RunExploreASLString = ['"cd(''' x.MyPath ''');ExploreASL_Master(''' DataParFile{iList}{1} ''',true,true);exit"'];
 	            system([MatlabRunString RunExploreASLString ' &']);
 	        case 3 % run ExploreASL compilation serially
