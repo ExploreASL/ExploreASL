7338af6e365e48ca7fa799492a5ed0ba557c10f1
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-03-08 09:06:42 +0100

    Revamp xASL_spm_deface.m

--- Functions/xASL_spm_deface.m
+++ Functions/xASL_spm_deface.m
@@ -1,19 +1,61 @@
-function xASL_spm_deface( InPath )
-%xASL_spm_deface Defaces the anatomical images, to preserve anonimity
-% However, this makes it more difficult to check whether FLAIR & T1w images
-% belong to the same subject
+function xASL_spm_deface(PathIn, bReplace)
+%xASL_spm_deface Defaces an anatomical image to preserve anonimity
 
-	fprintf('%s\n','Defacing image for privacy reasons...  ');
+% FORMAT: xASL_spm_deface(PathIn, bReplace)
+% 
+% INPUT:
+%   PathIn   - path to anatomical NIfTI file that will be defaced. .nii & .nii.gz are both allowed (REQUIRED)
+%   bReplace - boolean to specify whether the original anatomical NIfTI will be
+%              overwritten (true) or whether the defaced result will be
+%              stored under 'anon_FileName' (false) (OPTIONAL,
+%              DEFAULT=false)
+% OUTPUT n/a
+% OUTPUT file:
+%   Defaced NIfTI (but always .nii, not .nii.gz format)
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This function removes the face from an anatomical NIfTI
+%              image, e.g. T1w or FLAIR, for disidentification/privacy purposes.
+%              When this script is run after the ExploreASL structural
+%              module, it does a pretty good job even for 2D images.
+%              However, note that this can always fail, strip part of
+%              the brain, or change the output of pipelines. So best not
+%              to compare results from defaced and non-defaced images.
+%              Also, note that defacing makes it difficult to ensure that
+%              the FLAIR and T1w are from the same subject.
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE: xASL_spm_deface('PathToMyStudy/T1.nii.gz');
+% __________________________________
+% Copyright 2015-2019 ExploreASL
 
-    [Fpath Ffile Fext]      = fileparts( InPath );
-    anonName                = fullfile( Fpath, ['anon_' Ffile Fext]);
-    xASL_delete(anonName);
-
-    xASL_io_ReadNifti(InPath); % make sure to unzip
-    matlabbatch{1}.spm.util.deface.images   = {InPath};
+    % ===========================================================
+    %% Admin
+    if nargin<2 || isempty(bReplace)
+        bReplace = 0;
+    end
+    
+    % get .nii (without .gz) name
+    [Fpath, Ffile, Fext] = fileparts(PathIn);
+    PathIn = fullfile(Fpath, [Ffile '.nii']);
+    
+    % Get the path to SPM defaced output file
+    PathAnon = fullfile( Fpath, ['anon_' Ffile Fext]);
+    xASL_delete(PathAnon);
+    
+    fprintf('%s\n','Defacing image for privacy reasons...');
+    
+    % ===========================================================
+    %% Unzipping if needed
+    xASL_io_ReadNifti(PathIn);
+    
+    % ===========================================================
+    %% Run spm_deface
+    matlabbatch{1}.spm.util.deface.images = {PathIn};
     spm_jobman('run',matlabbatch); close all;
 
-    % Replace the original file with the defaced one
-%     xASL_Move( anonName, InPath, 1);
+    % ===========================================================
+    %% Replace the original file with the defaced one
+    if bReplace
+        xASL_Move(PathAnon , PathIn, 1);
+    end
 
 end
