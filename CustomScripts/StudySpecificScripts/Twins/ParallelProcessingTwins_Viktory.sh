e2ddb7de9f60e442a3fa8d827bb60868d86f776f
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-03-14 21:01:25 +0100

    Update ParallelProcessingTwins_Viktory.sh

--- CustomScripts/StudySpecificScripts/Twins/ParallelProcessingTwins_Viktory.sh
+++ CustomScripts/StudySpecificScripts/Twins/ParallelProcessingTwins_Viktory.sh
@@ -1,8 +1,8 @@
 let nParallel=10;
 MatlabPath=/usr/local/apps/matlab/R2018b/bin/matlab;
-DataParPath=/radshare/Twins/Twins_ExploreASL/DATA_PAR_TwinStudy.m;
+DataParPath=/radshare/Twins/Twins_ExploreASL/DATA_PAR_TwinStudy.json;
 xASLdir=/radshare/EPAD/scripts/ExploreASL;
 cd $xASLdir
 for (( i=1; i<=$nParallel; i++ ));
-do screen -dmSL TWINS$i nice -n 10 $MatlabPath -nodesktop -nosplash -r "cd('$xASLdir');ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);system('screen -SX SABRE$i kill');" &
+do screen -dmSL TWINS$i nice -n 10 $MatlabPath -nodesktop -nosplash -r "cd('$xASLdir');ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);system('screen -SX TWINS$i kill');" &
 done
