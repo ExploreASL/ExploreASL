7e694c2530fdb3033b0695636d2f1a03e04e744a
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-05-03 16:15:03 +0200

    Update ParallelProcessingTwins_Viktory.sh

--- CustomScripts/StudySpecificScripts/Twins/ParallelProcessingTwins_Viktory.sh
+++ CustomScripts/StudySpecificScripts/Twins/ParallelProcessingTwins_Viktory.sh
@@ -2,9 +2,7 @@ let nParallel=10;
 MatlabPath=/usr/local/apps/matlab/R2018b/bin/matlab;
 DataFolder=/radshare/Twins/Twins_ExploreASL; 
 DataParPath=$DataFolder/DATA_PAR_TwinStudy.json;
-rm $DataFolder/lock/*/*/*/locked -d; # remove locked folders
-rm -rf $DataFolder/lock/xASL_module_Population; # Population lock folder
-xASLdir=/radshare/EPAD/scripts/ExploreASL;
+xASLdir=/radshare/ExploreASL_Test/ExploreASL;
 cd $xASLdir
 for (( i=1; i<=$nParallel; i++ ));
 do screen -dmSL TWINS$i nice -n 10 $MatlabPath -nodesktop -nosplash -r "cd('$xASLdir');ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);system('screen -SX TWINS$i kill');" &
