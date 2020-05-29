6d60fdde504d0256626034472b0c60ede16ef1dc
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-05-29 17:13:57 +0200

    Create xASL_qc_TestExploreASL_HM.m

---
+++ Development/xASL_qc_TestExploreASL_HM.m
@@ -0,0 +1,5 @@
+%% Test ExploreASL on Mac Mutsaerts
+
+TestDirOrig='/Users/henk/surfdrive/HolidayPics/ExploreASL_TestCases';
+TestDirDest='/Users/henk/ExploreASL/ASL/TestCasesProcessed';
+[ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, 1, 1)
