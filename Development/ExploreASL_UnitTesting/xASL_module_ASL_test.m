f43ad41442d38a45d23cce899f7d17e8f4836189
Michael Stritt <stritt.michael@gmail.com>
2020-05-31 12:54:39 +0200

    dummy function for xASL_module_ASL unit test

---
+++ Development/ExploreASL_UnitTesting/xASL_module_ASL_test.m
@@ -0,0 +1,60 @@
+% xASL_module_ASL_test Script to test the xASL_module_ASL function
+%
+% FORMAT:           None, script based
+%
+% INPUT:            None, script based
+%
+% OUTPUT:           Console window log
+%
+% OUTPUT FILES:     Log file of the unit tests of the xASL_module_ASL function
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This script is supposed to run various unit tests of xASL_module_ASL:
+%
+%           1) Run a test using the default TestDataSet inputs with high quality setting
+%           2) Run a test with ...
+%
+% EXAMPLE: xASL_module_ASL_test;
+% __________________________________
+% Copyright (C) 2015-2020 ExploreASL
+
+% DEFAULTS
+
+
+
+
+
+
+
+
+% PRECONDITIONS
+
+
+
+
+
+
+
+
+
+%% Test 1: Default TestDataSet with high quality setting
+
+% Load test inputs
+
+% Run test
+
+% Use assert for outputs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
