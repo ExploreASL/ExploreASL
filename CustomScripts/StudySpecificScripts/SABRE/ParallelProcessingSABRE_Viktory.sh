77f9d631a90dc7bba428eef503e29f92ebbf2650
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-06-02 08:05:27 +0200

    CustomScripts SABRE

--- CustomScripts/StudySpecificScripts/SABRE/ParallelProcessingSABRE_Viktory.sh
+++ CustomScripts/StudySpecificScripts/SABRE/ParallelProcessingSABRE_Viktory.sh
@@ -4,9 +4,12 @@ MatlabPath=/usr/local/apps/matlab/R2018b/bin/matlab;
 DataFolder=/radshare/SABRE/Analysis2; 
 DataParPath=$DataFolder/DataParameters_HiQ.json;
 rm $DataFolder/lock/*/*/*/locked -d; # remove locked folders
-rm -rf $DataFolder/lock/xASL_module_Population; # Population lock folder
+# srrm -rf $DataFolder/lock/xASL_module_Population; # Population lock folder
 xASLdir=/radshare/ExploreASL_Test/ExploreASL;
 cd $xASLdir;
 for (( i=1; i<=$nParallel; i++ ));
 do screen -dmSL SABRE$i nice -n 10 $MatlabPath -nodesktop -nosplash -r "cd('$xASLdir');ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);system('screen -SX SABRE$i kill');" &
 done
+
+
+# note to remove locks
