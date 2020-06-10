8b100c7db20f4a1988da5040f5309c40d504507c
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-06-10 15:51:06 +0200

    Update xASL_qc_TestExploreASL_HM.m

--- Development/xASL_qc_TestExploreASL_HM.m
+++ Development/xASL_qc_TestExploreASL_HM.m
@@ -1,5 +1,25 @@
-%% Test ExploreASL on Mac Mutsaerts
+function [ResultsTable] = xASL_qc_TestExploreASL_HM
+%xASL_qc_TestExploreASL_HM Run ExploreASL QC for test datasets
 
-TestDirOrig='/Users/henk/surfdrive/HolidayPics/ExploreASL_TestCases';
-TestDirDest='/Users/henk/ExploreASL/ASL/TestCasesProcessed';
-[ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, 1, 1)
+
+if ismac
+    TestDirOrig='/Users/henk/surfdrive/HolidayPics/ExploreASL_TestCases';
+    TestDirDest='/Users/henk/ExploreASL/ASL/TestCasesProcessed';
+elseif ispc
+    TestDirOrig='/Users/henk/surfdrive/HolidayPics/ExploreASL_TestCases';
+    TestDirDest='/Users/henk/ExploreASL/ASL/TestCasesProcessed';
+else
+    % linux
+    [StatusIs, UserName] = system('whoami','-echo');
+
+     TestDirOrig='/home/henk/ownCloud/HolidayPics/ExploreASL_TestCases';
+     TestDirDest='/home/henk/ExploreASL/ASL/ExploreASL_TestCasesProcessed';
+end
+    
+    
+    
+[ResultsTable] = xASL_qc_TestExploreASL(TestDirOrig, TestDirDest, 1, 1);
+
+
+end
+
