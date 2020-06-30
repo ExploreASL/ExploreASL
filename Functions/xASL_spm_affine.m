7a4fed78c1556bd1583ccc6aeb806b0a875d71c3
Petr <j.petr@hzdr.de>
2020-06-30 19:02:24 +0200

    #3 Creating the DCT option in spm_affine

--- Functions/xASL_spm_affine.m
+++ Functions/xASL_spm_affine.m
@@ -1,43 +1,52 @@
-function xASL_spm_affine(srcPath, refPath, fwhmSrc, fwhmRef, OtherList)
-% ExploreASL wrapper for SPM affine registration function (a.k.a. 'old normalize' but without DCT)
+function xASL_spm_affine(srcPath, refPath, fwhmSrc, fwhmRef, otherList, bDCT)
+% ExploreASL wrapper for SPM affine registration function (a.k.a. 'old normalize'). On default run without DCT.
 %
-% FORMAT: xASL_spm_affine(srcPath, refPath, fwhmSrc, fwhmRef)
+% FORMAT: xASL_spm_affine(srcPath, refPath, fwhmSrc, fwhmRef[,otherList, bDCT])
 % 
 % INPUT:
-%   refPath - path to reference space (NifTI image) you want to register the source image to (REQUIRED)
-%   srcPath - path to source image (NifTI image) you want to register (REQUIRED)
-%   fwhmSrc - Gaussian smoothing to be applied to the source image before estimating the affine registration, in FWHM (mm) (REQUIRED)
-%   fwhmRef - Gaussian smoothing to be applied to the reference image before estimating the affine registration, in FWHM (mm) (REQUIRED)
+%   refPath   - path to reference space (NifTI image) you want to register the source image to (REQUIRED)
+%   srcPath   - path to source image (NifTI image) you want to register (REQUIRED)
+%   fwhmSrc   - Gaussian smoothing to be applied to the source image before estimating the affine registration, in FWHM (mm) (REQUIRED)
+%   fwhmRef   - Gaussian smoothing to be applied to the reference image before estimating the affine registration, in FWHM (mm) (REQUIRED)
+%   otherList - a list of NIFTIs to which should this registration be applied (OPTIONAL, default EMPTY)
+%   bDCT      - perfroms the low-degree Discrete Cosine Transform (DCT) (OTIONAL, default FALSE)
 %
 % OUTPUT: n/a
 %                         
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION: This SPM wrapper runs SPM's old normalize-estimate function, which calculates the affine transformation (i.e. linear + zooming and shearing) that is required to
-% align the source image with the reference image. Note that the old normalize SPM function by default also estimates a low-degree Discrete
-% Cosine Transform (DCT) but this is disabled in this wrapper to have a simple affine transformation. Also note that this affine transformation uses a
-% correlation cost function, hence it requires the source and reference images to have similar contrasts.
-% As opposed to other SPM estimation functions (e.g. xASL_spm_coreg) this
-% function does not change the orientation header, not does it allow to change those of others (e.g. the OtherList in xASL_spm_coreg). Instead,
+% align the source image with the reference image. By default it does not estimate the low-degree Discrete Cosine Transform (DCT) to have a simple affine transformation 
+% but this can be enabled in this wrapper. Also note that this affine transformation uses a correlation cost function, hence it requires the source and reference images 
+% to have similar contrasts and resolution - or provide the resolution estimates so the smoothing can be done.
+% This function does not change the orientation header by default, but it allows to change those of others through the otherList. If bDCT is used or no otherList given,
 % it stores its estimated affine transformation as orientation difference matrix in a file with the same path but _sn.mat extension.
-% For the provided smoothing FWHM, note that smoothnesses combine with Pythagoras' rule (i.e. square summing)
+% For the provided smoothing FWHM, note that smoothnesses combine with Pythagoras' rule (i.e. square summing).
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE: xASL_spm_affine('/MyStudy/Subject1/T1.nii.gz', '/MyStudy/Subject1/mean_control.nii', 5, 5);
 % __________________________________
-% Copyright 2015-2019 ExploreASL
-% 2015-2019 HJ
+% Copyright 2015-2020 ExploreASL
+% 2015-2020 HJ, JP
    
 % Check parameters
 if nargin<5
-    OtherList = '';
+    otherList = '';
 else
-    OtherList = xASL_adm_OtherListSPM(OtherList);
+    otherList = xASL_adm_OtherListSPM(otherList);
 end
 if nargin < 4
 	error('xASL_spm_affine: Requires 4 input parameters.');
 end
 if ~xASL_exist(srcPath) || ~xASL_exist(refPath)
 	error('xASL_spm_affine: Cannot find input images.');
+end
+
+if nargin < 6
+	bDCT = false;
+end
+
+if ~isempty(otherList) && bDCT
+	warning('xASL_spm_affine: Cannot modify the headers in otherList if bDCT is true');
 end
 
 % Unzip the input files 
@@ -53,7 +62,11 @@ matlabbatch{1}.spm.tools.oldnorm.est.eoptions.smosrc        = fwhmSrc;
 matlabbatch{1}.spm.tools.oldnorm.est.eoptions.smoref        = fwhmRef;
 matlabbatch{1}.spm.tools.oldnorm.est.eoptions.regtype       = 'subj';
 matlabbatch{1}.spm.tools.oldnorm.est.eoptions.cutoff        = 25; % biasfield correction
-matlabbatch{1}.spm.tools.oldnorm.est.eoptions.nits          = 0; % 16 default (includes DCT)
+if bDCT
+	matlabbatch{1}.spm.tools.oldnorm.est.eoptions.nits          = 16; % 16 default (includes DCT)
+else
+	matlabbatch{1}.spm.tools.oldnorm.est.eoptions.nits          = 0; % 16 default (includes DCT)
+end
 matlabbatch{1}.spm.tools.oldnorm.est.eoptions.reg           = 1;
 
 %  ------------------------------------------------------------------------------------------
@@ -66,33 +79,33 @@ spm_jobman('run',matlabbatch);
 
 %% Apply to NIfTIs
 % If we apply this to NIfTIs, we remove the transformation,
-% to keep this transparant. This is done by providing OtherList,
+% to keep this transparant. This is done by providing otherList,
 % even if we want to apply it to the srcPath
-% If we don't provide an otherlist, this estimated affine transformation
+% If we don't provide an otherList, this estimated affine transformation
 % is only saved as _sn.mat, not applied
 
-if ~isempty(OtherList)
+if ~isempty(otherList) && ~bDCT
     [Fpath, Ffile] = xASL_fileparts(srcPath);
     PathMat = fullfile(Fpath, [Ffile '_sn.mat']);
     SnMat = load(PathMat);
     Affine = (SnMat.VG.mat/SnMat.Affine)/SnMat.VF.mat;
 
-    for iList=1:length(OtherList)
+    for iList=1:length(otherList)
         nii = [];
         
         % find comma
-        StartComma = regexp(OtherList{iList},',');
+        StartComma = regexp(otherList{iList},',');
         if ~isempty(StartComma)
-            Index4D = xASL_str2num(OtherList{iList}(StartComma+1:end));
-            OtherList{iList} = OtherList{iList}(1:StartComma-1);
+            Index4D = xASL_str2num(otherList{iList}(StartComma+1:end));
+            otherList{iList} = otherList{iList}(1:StartComma-1);
             if Index4D==1
                 % apply affine transformation to NIfTI file
-                nii = xASL_io_ReadNifti(OtherList{iList});
+                nii = xASL_io_ReadNifti(otherList{iList});
                 nii.mat = Affine*nii.mat;
                 create(nii);
             else
                 % apply affine transformation to motion orientation sidecar
-                [Fpath, Ffile] = xASL_fileparts(OtherList{iList});
+                [Fpath, Ffile] = xASL_fileparts(otherList{iList});
                 PathMotion = fullfile(Fpath, [Ffile '.mat']);
                 Mat = load(PathMotion,'-mat');
                 mat = Mat.mat;
@@ -100,7 +113,7 @@ if ~isempty(OtherList)
                 save(PathMotion,'mat');
             end
         else % apply affine transformation to NIfTI file
-            nii = xASL_io_ReadNifti(OtherList{iList});
+            nii = xASL_io_ReadNifti(otherList{iList});
             nii.mat = Affine*nii.mat;
             create(nii);
         end
