5afe51850cf1be0fc3ddea739a488dacfb30fad8
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-07-01 16:20:35 +0200

    #7 Improve documentation & speeding up xASL_exist

--- External/SPMmodified/xASL/xASL_exist.m
+++ External/SPMmodified/xASL/xASL_exist.m
@@ -1,55 +1,54 @@
 function OutputArg = xASL_exist(PathIn, Type)
-% ExploreASL wrapper of Matlab exist function. For NIFTII file, checks also the GZ version
+%xASL_exist ExploreASL wrapper of Matlab exist function, checking for
+%.nii & .nii.gz at same time
 %
 % FORMAT: xASL_exist(PathIn[,Type])
 %
 % INPUT:
 %   PathIn 	- path to be checked if it exists (REQUIRED)
-%   Type    - optional type of if .nii or .nii.gz should be checked. 
-%             If input does not have a .nii or .nii.gz extension, this argument is ignored (OPTIONAL, DEFAULT = check both)
+%   Type    - type of checking (e.g. 'var', 'file', 'dir', as with Matlab's built in exist function (OPTIONAL, DEFAULT = [])
 % OUTPUT:
 %   OutputArg - true or false depending on if exists
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION: Check if the given path exists, wrapper around the Matlab
 %              exist function, to allow checking for either .nii or .nii.gz
 %              Otherwise, exist is used normally.
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE: xASL_exist('/path/file.nii');
 %          xASL_exist('/path/file.nii.gz','file');
 %          xASL_exist('image','var');
-% -----------------------------------------------------------------------------------------------------------------------------------------------------
-%
 % __________________________________
-% Copyright (C) 2015-2019 ExploreASL
+% Copyright (C) 2015-2020 ExploreASL
 
 % It can have one or two input parameters
 if nargin>2
-	error('xASL_exist:Two input parameters is maximum.');
+	error('Two input parameters is maximum');
 end
 
-if nargin<2 
+if nargin<2 || isempty(Type)
 	Type = [];
+elseif ~strcmp(Type, 'file')
+    OutputArg = exist(PathIn, Type);
+    return; % bypass rest if not a file
 end
 
-% It only accepts strings on input
+% Function only accepts strings on input
 if ~ischar(PathIn)
     OutputArg = false; %%%% >>>>> difficult to debug
 %     warning('xASL_exist: Cannot check PathIn');
     return;
 end
 
-% Checks for both NII and NII.GZ if a file is given
+% Check for both NII and NII.GZ if a file is given
 if strcmp(PathIn(end-3:end),'.nii')
 	PathInGZ = [PathIn '.gz'];
 	
-	OutputArg = exist(PathIn,'file') || exist(PathInGZ,'file');
+	OutputArg = exist(PathIn, 'file') || exist(PathInGZ, 'file');
 elseif  strcmp(PathIn(end-2:end),'.gz')
 	PathInNII = PathIn(1:end-3);
-	OutputArg = exist(PathIn,'file') || exist(PathInNII,'file');
-elseif isempty(Type)
-	% In case another input is given from NII and NII.GZ, just check for the given type
-	OutputArg = exist(PathIn);
+	OutputArg = exist(PathIn, 'file') || exist(PathInNII, 'file');
 else
-	OutputArg = exist(PathIn,Type);
+	OutputArg = exist(PathIn, Type);
 end
         
 end
