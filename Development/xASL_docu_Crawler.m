ba69e2b98ff1d072ce0b70a2b4b1139c70631372
Michael Stritt <stritt.michael@gmail.com>
2020-07-06 18:33:20 +0200

    #7 Major docu crawler update

--- Development/xASL_docu_Crawler.m
+++ Development/xASL_docu_Crawler.m
@@ -14,6 +14,10 @@ function xASL_docu_Crawler(folder,mdoutput)
 %               extracts the information. The results is saved as a
 %               markdown file.
 %
+%               If you want to use star symbols (*testFile.m e.g.) we
+%               recommend not to use them in the same line with bold text
+%               (which is written like this: {{bold text}}).
+%
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLES:     xASL_docu_Crawler('M:\...\Functions', 'M:\...\Output.md')
 % __________________________________
@@ -56,52 +60,58 @@ for i = 1:numel(listing)
     fileName = listing(i).name;
     filePath = fullfile(folder, fileName);
     
-    % Extract header
-    try
-        header = help(fileName);
-        
-        % Convert header to cell array
-        headerList = strsplit(header,'\n');
-        headerList = headerList';
-        
-        % Get line with FORMAT description
-        [stringsFORMAT, ~, ~]= regexpi(headerList, 'FORMAT:', 'match');
-        
-        % Get line with FORMAT description
-        [stringsINPUT, ~, ~]= regexpi(headerList, 'INPUT:', 'match');
-        
-        % Get line with DESCRIPTION
-        [stringsDESCRIPTION, ~, ~]= regexpi(headerList, 'DESCRIPTION:', 'match');
-        
-        % Get line with EXAMPLE
-        [stringsEXAMPLE, ~, ~]= regexpi(headerList, 'EXAMPLE:', 'match');
-        
-        % Find indices
-        indexFORMAT = find(~cellfun(@isempty,stringsFORMAT));
-        indexINPUT = find(~cellfun(@isempty,stringsINPUT));
-        indexDESCRIPTION = find(~cellfun(@isempty,stringsDESCRIPTION));
-        indexEXAMPLE = find(~cellfun(@isempty,stringsEXAMPLE));
-        
-        % Check if all descriptions are there
-        if isempty(indexFORMAT) || isempty(indexINPUT) || isempty(indexDESCRIPTION) || isempty(indexEXAMPLE)
+    if ~strcmp(fileName,'README.md')
+    
+        % Extract header
+        try
+            header = help(fileName);
+
+            % Convert header to cell array
+            headerList = strsplit(header,'\n');
+            headerList = headerList';
+
+            % Get line with FORMAT description
+            [stringsFORMAT, ~, ~]= regexpi(headerList, 'FORMAT:', 'match');
+
+            % Get line with FORMAT description
+            [stringsINPUT, ~, ~]= regexpi(headerList, 'INPUT:', 'match');
+
+            % Get line with DESCRIPTION
+            [stringsDESCRIPTION, ~, ~]= regexpi(headerList, 'DESCRIPTION:', 'match');
+
+            % Get line with EXAMPLE
+            [stringsEXAMPLE, ~, ~]= regexpi(headerList, 'EXAMPLE:', 'match');
+
+            % Find indices
+            indexFORMAT = find(~cellfun(@isempty,stringsFORMAT));
+            indexINPUT = find(~cellfun(@isempty,stringsINPUT));
+            indexDESCRIPTION = find(~cellfun(@isempty,stringsDESCRIPTION));
+            indexEXAMPLE = find(~cellfun(@isempty,stringsEXAMPLE));
+
+            % Check if all descriptions are there
+            if isempty(indexFORMAT) || isempty(indexINPUT) || isempty(indexDESCRIPTION) || isempty(indexEXAMPLE)
+                extractHeaderInfo = false;
+                continue
+            end
+
+            % FORMAT text
+            formatText = headerList(indexFORMAT:indexINPUT-1);
+            formatText = strrep(formatText,'FORMAT:','');
+            formatText = char(strtrim(formatText));
+
+            % DESCRIPTION text
+            descriptionText = headerList(indexDESCRIPTION:indexEXAMPLE-1);
+            descriptionText = strrep(descriptionText,'DESCRIPTION:','');
+            descriptionText = char(strtrim(descriptionText));
+
+            % Give the okay to extract the information of the current file
+            extractHeaderInfo = true;
+
+        catch
+            fprintf('File %s does not fulfill the documentation requirements...', fileName);
             extractHeaderInfo = false;
-            continue
         end
-        
-        % FORMAT text
-        formatText = headerList(indexFORMAT:indexINPUT-1);
-        formatText = strrep(formatText,'FORMAT:','');
-        formatText = char(strtrim(formatText));
-        
-        % DESCRIPTION text
-        descriptionText = headerList(indexDESCRIPTION:indexEXAMPLE-1);
-        descriptionText = strrep(descriptionText,'DESCRIPTION:','');
-        descriptionText = char(strtrim(descriptionText));
-        
-        % Give the okay to extract the information of the current file
-        extractHeaderInfo = true;
-        
-    catch
+    else
         extractHeaderInfo = false;
     end
     
@@ -143,9 +153,23 @@ for i = 1:numel(listing)
         it = it+1;
         for i=1:lD
             TEXT{it,1} = descriptionText(i,:);
+            
+            % WITH THIS SYNTAX WE CANT USE STAR SYMBOLS AND BOLD TEXT IN ONE LINE
+            
+            % fprintf([TEXT{it,:},'\n']);
+            
+            % OUR NEW BOLD TEXT SYMBOLS ARE {{ AND }} (curly brackets)
+            if contains(TEXT{it,1},' {') || contains(TEXT{it,1},'} ')
+                TEXT{it,:} = strrep(TEXT{it,:},'{{','**');
+                TEXT{it,:} = strrep(TEXT{it,:},'}}','**');
+            elseif contains(TEXT{it,1},'*')
+                % Escape stars in the description
+                TEXT{it,:} = strrep(TEXT{it,:},'*','\*'); 
+            end
+            
             if contains(TEXT{it,1},'_')
-                TEXT{it,:} = strrep(TEXT{it,:},'_','\_'); % Escape underscores in the description
-                TEXT{it,:} = strrep(TEXT{it,:},'*','\*'); % Escape stars in the description
+                % Escape underscores in the description
+                TEXT{it,:} = strrep(TEXT{it,:},'_','\_'); 
             end
             it = it+1;
         end
@@ -167,7 +191,6 @@ for i=1:numel(TEXT)
         TEXT{i,:} = strrep(TEXT{i,:},SeparatorLine,'');
     end
 end
-
 
 % Print information to markdown file
 fileID = fopen(mdoutput,'w');
