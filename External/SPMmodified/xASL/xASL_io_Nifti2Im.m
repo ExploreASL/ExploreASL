078a97eb76e4f89e3dcf8bec6a5b7fa92a3f3173
Petr <j.petr@hzdr.de>
2020-07-29 22:56:32 +0200

    #48 Minor edits

--- External/SPMmodified/xASL/xASL_io_Nifti2Im.m
+++ External/SPMmodified/xASL/xASL_io_Nifti2Im.m
@@ -135,20 +135,20 @@ end
 % warning & try to correct this
 
 if exist('imOut', 'var') && exist('nii', 'var')
-    if nii.dat.scl_slope>16
-        % this if-clause speeds up in all other cases that don't have this potential Philips import issue
-        MaxIm = max(imOut(isfinite(imOut)));
-        if MaxIm>1e9
+	if nii.dat.scl_slope>16
+		% this if-clause speeds up in all other cases that don't have this potential Philips import issue
+		MaxIm = max(imOut(isfinite(imOut)));
+		if MaxIm>1e9
 			if exist('Fpath', 'var') && exist('Ffile', 'var')
 				if ~isempty(regexp(Ffile,'^.*(T1|FLAIR).*$'))
-					warning(sprintf('%s\n%s','Found a structural image with unusually large NIfTI scale slope and image data, resetting the maximum to 1e6 upon reading:',niftiIn));
-					imOut = imOut.*1e6./MaxIm;
+					warning('%s\n%s','Found a structural image with unusually large NIfTI scale slope and image data, resetting the maximum to 4096 upon reading:',niftiIn);
+					imOut = imOut.*4096./MaxIm;
 				else
-					warning(sprintf('%s\n%s','Found unusually large NIfTI scale slope and image data, check if all processing and quantification went correctly:',niftiIn));
+					warning('%s\n%s','Found unusually large NIfTI scale slope and image data, check if all processing and quantification went correctly:',niftiIn);
 				end
 			end
-        end
-    end
+		end
+	end
 end
 
 
