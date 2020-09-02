7df5da965d22ff97f620f1321a4c2c238e0c070a
Michael Stritt <stritt.michael@gmail.com>
2020-09-02 19:36:30 +0200

    #114 Switched statements

--- Functions/xASL_qc_CollectParameters.m
+++ Functions/xASL_qc_CollectParameters.m
@@ -94,10 +94,10 @@ switch ScanType
         % General software versions (put this in structural only, to avoid redundant output
         x.Output.(ScanType).Version_ExploreASL = x.Output.SoftwareVersion.ExploreASL;
         if isdeployed
-            x.Output.(ScanType).Version_Matlab = x.Output.SoftwareVersion.Matlab;
-        else
             x.Output.(ScanType).Version_Matlab = version;
             x.Output.(ScanType).Version_Matlab = x.Output.(ScanType).Version_Matlab(1:3);
+        else
+            x.Output.(ScanType).Version_Matlab = x.Output.SoftwareVersion.Matlab;
         end
         x.Output.(ScanType).Version_SPM12 = x.Output.SoftwareVersion.SPM12;        
     case {'ASL', 'dwi', 'func'}
