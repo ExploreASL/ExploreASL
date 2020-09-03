0be81d9daa569ac8038f28d467f109a0d1d3f1fe
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-09-03 20:36:07 +0200

    Fixes #118
    * fix bug that defaulted to bUseLinux=true for margin<3 && ispc. This should now default to bUseLinux=false\n* Improve header

--- Modules/SubModule_Population/xASL_adm_GzipAllFiles.m
+++ Modules/SubModule_Population/xASL_adm_GzipAllFiles.m
@@ -1,25 +1,48 @@
 function xASL_adm_GzipAllFiles(ROOT, bFolder, bUseLinux)
-%xASL_adm_GzipAllFiles This function zips all .nii
-% files recursively into nii.gz.
-% Note that the linux option cannot deal with spaces in filenames or
-% directories
+%xASL_adm_GzipAllFiles Zip files or folders
+%
+% FORMAT: xASL_adm_GzipAllFiles(ROOT, bFolder, bUseLinux)
+%
+% INPUT:
+%   x           - x structure containing all input parameters (REQUIRED)
+%   bFolder     - boolean, true for zipping complete folders rather than
+%                 individual files (Unix only, OPTIONAL, DEFAULT=false)
+%   bUseLinux   - boolean, true for using Unix's own filesystem and Gzip functionality.
+%                 Has a much faster i/o than Matlab's i/o. Assumes that
+%                 gzip is installed on Linux/MacOS (which is a fair
+%                 assumption). (OPTIONAL, DEFAULT = true for Unix (Linux/Mac) and false
+%                 otherwise (e.g. pc/Windows)
+% OUTPUT: n/a
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This function zips NIfTI files or folders recursively and deletes
+% the original file/folder after zipping.
+%
+% EXAMPLE: xASL_adm_GzipAllFiles('/MyStudy');
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% Copyright 2015-2020 ExploreASL
 
+
+    %% ----------------------------------------------------
+    %% 0) Admin
     if nargin<2 || isempty(bFolder)
         bFolder = false;
     end
-    if nargin<3 || isempty(bUseLinux) && (isunix || ismac)
+    if (nargin<3 || isempty(bUseLinux)) && (isunix || ismac)
         bUseLinux = true;
     elseif nargin<3 || isempty(bUseLinux)
         bUseLinux = false;
     end
 
+    %% ----------------------------------------------------
+    %% 1) Faster unix version, using OS file system
     if bUseLinux
         PathToSearch = xASL_adm_UnixPath(ROOT);
         system(['for i in `find ' PathToSearch '/* | grep -E \.nii$`; do gzip -1 -f -q -v "$i"; done'], '-echo');
         return;
     end
-            
-    
+
+    %% ----------------------------------------------------
+    %% 2) Slower Matlab version
     if bFolder
         PathList = xASL_adm_GetFileList(ROOT, '^.*$', 'FPList', [0 Inf], true);
         fprintf('\n%s\n',['G-zZzZipping ' num2str(length(PathList)) ' folders']);
@@ -58,6 +81,5 @@ function xASL_adm_GzipAllFiles(ROOT, bFolder, bUseLinux)
     end
     fprintf('\n');
 
+    
 end
-
-
