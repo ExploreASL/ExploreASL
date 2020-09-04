2cde880e61a4e8135bdc164e07aecc7b9f7b751f
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-09-04 16:23:29 +0200

    #112 Specify bOverwrite in xASL_adm_SaveX.m

--- Functions/xASL_adm_SaveX.m
+++ Functions/xASL_adm_SaveX.m
@@ -1,19 +1,20 @@
-function xASL_adm_SaveX(x, Path_xASL)
+function xASL_adm_SaveX(x, Path_xASL, bOverwrite)
 %xASL_adm_SaveX Saves x.mat file that keeps track of QC output
 % FORMAT: xASL_adm_SaveX(x[, Path_xASL])
 %
 % INPUT:
 %   x           - structure containing fields with all information required to run this submodule (REQUIRED)
 %   Path_xASL   - path to the x.mat that contains the QC output (OPTIONAL, DEFAULT = x.SUBJECTDIR/x.mat)
+%   bOverwite   - boolean specifying if we overwrite
 %
 % OUTPUT:
 %   n/a
-%
+
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION: This function saves the x.mat either to the predefined path or the the subject x.mat
 %
-% EXAMPLE: xASL_adm_SaveX(x, fullfile(x.D.ROOT,'x.mat'));
-%
+% EXAMPLE outside ExploreASL: xASL_adm_SaveX(x, fullfile(x.D.ROOT,'x.mat'));
+% EXAMPLE inside ExploreASL: xASL_adm_SaveX(x);
 % __________________________________
 % Copyright (C) 2015-2020 ExploreASL
 
@@ -27,8 +28,16 @@ if nargin<2 || isempty(Path_xASL)
 	end
 	Path_xASL = fullfile(x.SUBJECTDIR,'x.mat');
 end
+if nargin<3 || isempty(bOverwrite)
+    bOverwrite = true;
+end
 
-xASL_delete(Path_xASL);
-save(Path_xASL,'x'); 
+if exist(Path_xASL, 'file') && ~bOverwrite
+    error('x.mat already existed, skipping...');
+else
+    xASL_delete(Path_xASL);
+    save(Path_xASL,'x');
+end
+
 
 end
