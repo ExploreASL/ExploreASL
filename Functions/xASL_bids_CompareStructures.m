55379e4982681a27cfba03c299d1f61f72980e51
Petr <j.petr@hzdr.de>
2020-11-11 14:37:55 +0100

    #193 Final fixes and code cleaning.

--- Functions/xASL_bids_CompareStructures.m
+++ Functions/xASL_bids_CompareStructures.m
@@ -1,13 +1,13 @@
-function [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathDatasetB,printReport,thresh)
+function [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathDatasetB,bPrintReport,threshRmseNii)
 %xASL_bids_CompareStructures Function that compares two BIDS folders with several subfolders and studies and prints the differences.
 %
-% FORMAT: [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathDatasetB,[printReport,thresh]);
+% FORMAT: [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathDatasetB,[bPrintReport,threshRmseNii]);
 %
 % INPUT:
 %        pathDatasetA       - path to first BIDS structure (REQUIRED)
 %        pathDatasetB       - path to second BIDS structure (REQUIRED)
-%        printReport        - true or false to print console report (OPTIONAL, DEFAULT = true)
-%        thresh             - RMSE threshold for comparing NIFTI content (OPTIONAL, DEFAULT = 0.1)
+%        bPrintReport       - true or false to print console report (OPTIONAL, DEFAULT = true)
+%        threshRmseNii      - RMSE threshold for comparing NIFTI content (OPTIONAL, DEFAULT = 0.1)
 %
 % OUTPUT:
 %        identical          - Returns 1 if both folder structures are identical and 0 if not
@@ -15,7 +15,7 @@ function [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathData
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION:      Function that compares two BIDS folders with several subfolders and studies and prints the differences.
-%                   We recommend to set printReport to true, because you otherwise can't see significant file content differences.
+%                   We recommend to set bPrintReport to true, because you otherwise can't see significant file content differences.
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 %
@@ -46,14 +46,14 @@ function [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathData
         error('The root folder of structure B does not exist...');
     end
 	
-    % Default value for printReport
-	if nargin < 3 || isempty(printReport)
-		printReport = true;
-    end
+    % Default value for bPrintReport
+	if nargin < 3 || isempty(bPrintReport)
+		bPrintReport = true;
+	end
     
     % Default value for RMSE threshold
     if nargin < 4
-       thresh = 0.1; 
+       threshRmseNii = 0.1; 
     end
 
 
@@ -124,23 +124,31 @@ function [identical,results] = xASL_bids_CompareStructures(pathDatasetA,pathData
     end
     
     % Full Report
-    if printReport
+    if bPrintReport
         fprintf(strcat(repmat('=',100,1)','\n'));
         fprintf('Dataset:\t\t%s\n',datasetA)
         printList(results.(datasetA).missingFolders)
         printList(results.(datasetA).missingFiles)
+		
+		if isempty(results.(datasetA).missingFolders) && isempty(results.(datasetA).missingFiles)
+			fprintf('\t\t\t%s\n','No missing files');
+		end
 
         fprintf(strcat(repmat('=',100,1)','\n'));
         fprintf('Dataset:\t\t%s\n',datasetB)
         printList(results.(datasetB).missingFolders)
         printList(results.(datasetB).missingFiles)
+		
+		if isempty(results.(datasetB).missingFolders) && isempty(results.(datasetB).missingFiles)
+			fprintf('\t\t\t%s\n','No missing files');
+		end
 
         % End of report
         fprintf(strcat(repmat('=',100,1)','\n'));
     end
     
     % Compare file content
-    identical = checkFileContents(fileListA,fileListB,pathDatasetA,pathDatasetB,identical,printReport,thresh);
+    identical = checkFileContents(fileListA,fileListB,pathDatasetA,pathDatasetB,identical,bPrintReport,threshRmseNii);
 
     
     
@@ -150,96 +158,94 @@ end
 %% Modify file lists
 function fileList = modifyFileList(fileList,root)
     % Iterate over file list: change folder names
-    for it=1:numel(fileList)
-        fileList(it).folder = strrep(fileList(it).folder,root,'');
+    for iFile=1:numel(fileList)
+        fileList(iFile).folder = strrep(fileList(iFile).folder,root,'');
     end
     % Iterate over file list: change file names
-    for it=1:numel(fileList)
+    for iFile=1:numel(fileList)
         % Check that the current element is not a folder
-        if ~strcmp(fileList(it).name,'.') && ~strcmp(fileList(it).name,'..')
-            fileList(it).name = fullfile(fileList(it).folder,fileList(it).name);
+        if ~strcmp(fileList(iFile).name,'.') && ~strcmp(fileList(iFile).name,'..')
+            fileList(iFile).name = fullfile(fileList(iFile).folder,fileList(iFile).name);
         end
     end
 end
 
-% Print list functions
+%% Print list functions
 function printList(currentList)
     % Iterate over list
     if ~isempty(currentList)
-        for it=1:length(currentList)
-            fprintf('Missing:\t\t%s\n',currentList(it))
+        for iFile=1:length(currentList)
+            fprintf('Missing:\t\t%s\n',currentList(iFile))
         end
     end
 end
 
 %% Check file contents
-function identical = checkFileContents(filesDatasetA,filesDatasetB,pathDatasetA,pathDatasetB,identical,printReportFunction,thresh)
+function identical = checkFileContents(filesDatasetA,filesDatasetB,pathDatasetA,pathDatasetB,identical,bPrintReport,threshRmseNii)
     
     % All files
     allFiles = unique([filesDatasetA',filesDatasetB']');
     
     % Iterate over list
-    for it=1:length(allFiles)
+    for iFile=1:length(allFiles)
         % Assign root directory of dataset A
-        currentFileA = fullfile(pathDatasetA,allFiles(it));
-        currentFileB = fullfile(pathDatasetB,allFiles(it));
+        currentFileA = fullfile(pathDatasetA,allFiles(iFile));
+        currentFileB = fullfile(pathDatasetB,allFiles(iFile));
         % Get extension
-        [~,~,extension] = fileparts(allFiles(it));
+        [~,~,extension] = fileparts(allFiles(iFile));
         % Check extension
         if strcmp(extension,'.json')
-            if printReportFunction
-                fprintf('Checking:\t\t%s\n',allFiles(it))
-            end
-            % Compare JSON files on field basis
-            if (isfile(currentFileA) && isfile(currentFileB)) % xASL_exist somehow didn't work here (again)
-                % Import JSON files
-                jsonA = xASL_import_json(char(currentFileA));
-                jsonB = xASL_import_json(char(currentFileB));
-                % Get JSON field names
-                fieldNamesA = fieldnames(jsonA);
-                fieldNamesB = fieldnames(jsonB);
-                % Check if both JSON files have the same fields
-                differenceA = setdiff(fieldNamesA,fieldNamesB);
-                differenceB = setdiff(fieldNamesB,fieldNamesA);
-                if isempty(differenceA) && isempty(differenceB)
-                    % Both JSON files have the same fields, so we can compare the content
-                    compareFieldLists(jsonA,jsonB,fieldNamesA,fieldNamesB,printReportFunction);
-                else
-                    % The JSON files have different fields
-                    sharedFieldsAB = intersect(fieldNamesB,fieldNamesA);
-                    allFields = union(fieldNamesA,fieldNamesB);
-                    missingFields = setdiff(allFields,sharedFieldsAB);
-                    % Print out and compare shared fields 
-                    for sharedField=1:numel(sharedFieldsAB)
-                        if printReportFunction
-                            % fprintf('\t\t\t\tShared field %s ...\n',string(sharedFieldsAB{sharedField}));
-                        end
-                        % Now we can compare these fields like in the part above
-                        compareFieldLists(jsonA,jsonB,sharedFieldsAB,sharedFieldsAB,printReportFunction);
-                    end
-                    % Print out missing fields 
-                    for missingField=1:numel(missingFields)
-                        if printReportFunction
-                            fprintf('\t\t\t\tMissing field %s ...\n',string(missingFields{missingField}));
-                        end
-                    end
-                end
-            end
+			jsonErrorReport='';
+
+			% Compare JSON files on field basis
+			if (isfile(currentFileA) && isfile(currentFileB)) % xASL_exist somehow didn't work here (again)
+				% Import JSON files
+				jsonA = xASL_import_json(char(currentFileA));
+				jsonB = xASL_import_json(char(currentFileB));
+				
+				% Get JSON field names
+				fieldNamesA = fieldnames(jsonA);
+				fieldNamesB = fieldnames(jsonB);
+				
+				% Check which fields are shared and which different
+				sharedFieldsAB = intersect(fieldNamesB,fieldNamesA);
+
+				% Fields that are in B, but missing in A
+				missingFields = setdiff(fieldNamesB,fieldNamesA);
+				% Print out missing fields
+				for iField=1:numel(missingFields)
+					jsonErrorReport = sprintf('%s\t\t\t\tMissing field: %s\n',jsonErrorReport,string(missingFields{iField}));
+				end
+				
+				extraFields = setdiff(fieldNamesA,fieldNamesB);
+				% Print out missing fields
+				for iField=1:numel(extraFields)
+					jsonErrorReport = sprintf('%s\t\t\t\tExtra field: %s\n',jsonErrorReport,string(extraFields{iField}));
+				end
+				
+				% Now we can compare these fields like in the part above
+				jsonErrorReport = [jsonErrorReport, compareFieldLists(jsonA,jsonB,sharedFieldsAB)];
+				
+				if bPrintReport && ~isempty(jsonErrorReport)
+					fprintf('%s:\n',allFiles(iFile));
+					fprintf('%s',jsonErrorReport);
+				end
+			end
         elseif strcmp(extension,'.tsv') || strcmp(extension,'.txt') || strcmp(extension,'.csv')
             % Read files if they exist
             if (isfile(currentFileA) && isfile(currentFileB)) % xASL_exist somehow didn't work here (again)
                 % Compare text files content directly
                 currentFileTextA = fileread(currentFileA);
                 currentFileTextB = fileread(currentFileB);
-                if printReportFunction
-                    fprintf('Checking:\t\t%s\n',allFiles(it))
+                if bPrintReport
                     if ~strcmp(currentFileTextA,currentFileTextB)
-                        fprintf('\t\t\t\tDifferent file content!\n')
+						fprintf('%s:\t\t\n',allFiles(iFile));
+                        fprintf('\t\t\t\tDifferent file content.\n');
                         identical = false;
                     end
                 end
             end
-        elseif strcmp(extension,'.nii') || contains(allFiles(it),'.nii.gz') % Warning: This unzips your NIFTIs right now!
+        elseif strcmp(extension,'.nii') || contains(allFiles(iFile),'.nii.gz') % Warning: This unzips your NIFTIs right now!
             % Read files if they exist
             if (isfile(currentFileA) && isfile(currentFileB)) % xASL_exist somehow didn't work here (again)
                 % Check file size (there were some 0KB images in the bids-examples)
@@ -247,12 +253,10 @@ function identical = checkFileContents(filesDatasetA,filesDatasetB,pathDatasetA,
                 tmpFileB = dir(currentFileB);
                 sizeA = tmpFileA.bytes;
                 sizeB = tmpFileB.bytes;
-                if printReportFunction
-                    fprintf('Checking:\t\t%s\n',allFiles(it));
-                end
+
                 % Check file size (images with a file size lower than 1 byte are corrupt)
                 if (sizeA>1 && sizeB>1)
-                    if contains(allFiles(it),'.nii.gz')
+                    if contains(allFiles(iFile),'.nii.gz')
                         % Unzip first
                         tmpPathImageA = xASL_adm_UnzipNifti(char(currentFileA));
                         tmpPathImageB = xASL_adm_UnzipNifti(char(currentFileB));
@@ -260,24 +264,26 @@ function identical = checkFileContents(filesDatasetA,filesDatasetB,pathDatasetA,
                         imageA = xASL_io_Nifti2Im(tmpPathImageA);
                         imageB = xASL_io_Nifti2Im(tmpPathImageB);
                         % GZIP Niftis afterwards
-                        pathImageA = xASL_adm_GzipNifti(tmpPathImageA);
-                        pathImageB = xASL_adm_GzipNifti(tmpPathImageB);
+                        xASL_adm_GzipNifti(tmpPathImageA);
+                        xASL_adm_GzipNifti(tmpPathImageB);
                     else
                         imageA = xASL_io_Nifti2Im(char(currentFileA));
                         imageB = xASL_io_Nifti2Im(char(currentFileB));
                     end
                     % Report function which prints to the console
-                    if printReportFunction
+                    if bPrintReport
                         % differenceAB = sum(imageA-imageB,'all');
                         RMSE = sqrt(mean(imageA(:).^2 - imageB(:).^2)) ;
-                        if (RMSE>thresh)
-                            fprintf('RMSE of NIFTIs above threshold!\n')
+                        if (RMSE>threshRmseNii)
+							fprintf('%s:\t\t\n',allFiles(iFile));
+                            fprintf('\t\t\t\tRMSE of NIFTIs above threshold.\n');
                             identical = false;
                         end
                     end
                 else
-                    if printReportFunction
-                        fprintf('\t\t\t\tFile is too small to be a real image!\n');
+                    if bPrintReport
+						fprintf('%s:\t\t\n',allFiles(iFile));
+                        fprintf('\t\t\t\tFile is too small to be a real image.\n');
                     end
                 end
             end
@@ -286,51 +292,57 @@ function identical = checkFileContents(filesDatasetA,filesDatasetB,pathDatasetA,
 end
 
 %% Compare field lists
-function compareFieldLists(jsonStructA,jsonStructB,fieldListA,fieldListB,printReportNow)
+function strError = compareFieldLists(jsonStructA,jsonStructB,fieldList)
+    strError = '';
 
     % Iterate over fields
-    for curField=1:numel(fieldListA)
-        curFieldNameA = string(fieldListA(curField));
-        curFieldNameB = string(fieldListB(curField));
-        fieldContentA = jsonStructA.(fieldListA{curField});
-        fieldContentB = jsonStructB.(fieldListB{curField});
+    for iField=1:numel(fieldList)
+        curFieldName = string(fieldList(iField));
+        fieldContentA = jsonStructA.(fieldList{iField});
+        fieldContentB = jsonStructB.(fieldList{iField});
         if isnumeric(fieldContentA) && isnumeric(fieldContentB)
             % Compare numbers
             if ~(fieldContentA==fieldContentB)
-                if printReportNow
-                    fprintf('\t\t\t\tMismatch of JSON field %s ...\n', curFieldNameA);
-                end
+                strError = sprintf('%s\t\t\t\tDifferent value: %s (%f vs %f)\n', strError,curFieldName,fieldContentA,fieldContentB);
             end
         elseif (ischar(fieldContentA) || isstring(fieldContentA)) && (ischar(fieldContentB) || isstring(fieldContentB))
             % Compare char arrays and strings
             if ~(strcmp(fieldContentA,fieldContentB))
-                if printReportNow
-                    fprintf('\t\t\t\tMismatch of JSON field %s ...\n', curFieldNameA);
-                end
+                strError = sprintf('%s\t\t\t\tDifferent value: %s (%s vs %s)\n', strError,curFieldName,fieldContentA,fieldContentB);
             end
         elseif iscell(fieldContentA) && iscell(fieldContentB)
             % Compare cell arrays
             if ~(isempty(setdiff(fieldContentA,fieldContentB)) && isempty(setdiff(fieldContentB,fieldContentA)))
-                if printReportNow
-                    fprintf('\t\t\t\tMismatch of JSON field %s ...\n', curFieldNameA);
-                end
+                strError = sprintf('%s\t\t\t\tDifferent value: %s (array)\n', strError,curFieldName);
             end
         elseif isstruct(fieldContentA) && isstruct(fieldContentB)
-            % Compare cell arrays
-            if ~(isequal(fieldContentA,fieldContentB))
-                if printReportNow
-                    fprintf('\t\t\t\tMismatch of JSON field %s ...\n', curFieldNameA);
-                end
-            end
+			% Compare cell arrays
+			if ~(isequal(fieldContentA,fieldContentB))
+				strError = sprintf('%s\t\t\t\tDifferent value: %s (array)\n', strError,curFieldName);
+			end
+		elseif islogical(fieldContentA) && islogical(fieldContentB)
+            % Compare numbers
+            if ~(fieldContentA==fieldContentB)
+				if fieldContentA
+					fieldContentA = 'true';
+				else
+					fieldContentA = 'false';
+				end
+				
+				if fieldContentB
+					fieldContentB = 'true';
+				else
+					fieldContentB = 'false';
+				end
+				
+                strError = sprintf('%s\t\t\t\tDifferent value: %s (%s vs %s)\n', strError,curFieldName,fieldContentA,fieldContentB);
+            end			
         else
             % Neither number nor text
-            if printReportNow
-                fprintf('\t\t\t\tIt seems we are comparing neither numbers nor text ...\n');
-            end
+			if ~isequal(fieldContentA,fieldContentB)
+				strError = sprintf('%s\t\t\t\tDifferent value: %s (%s vs %s) - unknown or differing types\n', strError,curFieldName,fieldContentA,fieldContentB);
+			end
         end
     end
     
 end
-
-
-
