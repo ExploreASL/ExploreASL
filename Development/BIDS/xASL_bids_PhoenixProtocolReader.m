4f5be512caa7eda16fdcea95242aab773c668a66
Michael Stritt <stritt.michael@gmail.com>
2020-11-12 13:19:20 +0100

    #223 Read phoenix protocol based on pydicom

--- Development/BIDS/xASL_bids_PhoenixProtocolReader.m
+++ Development/BIDS/xASL_bids_PhoenixProtocolReader.m
@@ -1,21 +1,23 @@
-function phoenixProtocol = xASL_bids_PhoenixProtocolReader(pathDataset)
+function [parameterList,phoenixProtocol] = xASL_bids_PhoenixProtocolReader(pathData)
 %xASL_bids_PhoenixProtocolReader Function that reads raw DICOM data and extracts the phoenix protocol parameters.
 %
-% FORMAT: phoenixProtocol = xASL_bids_PhoenixProtocolReader(pathDataset);
+% FORMAT: [parameterList,phoenixProtocol] = xASL_bids_PhoenixProtocolReader(pathData);
 %
 % INPUT:
-%        pathDataset        - path to DICOM dataset (REQUIRED)
+%        pathData           - path to DICOM dataset (REQUIRED)
 %
 % OUTPUT:
-%        phoenixProtocol    - structure containing the phoenix protocol parameters
+%        parameterList      - list of parameters from the reduced phoenix protocol
+%        phoenixProtocol    - cell array containing the phoenix protocol
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION:      Function that reads raw DICOM data and extracts the phoenix protocol parameters.
+%                   Only works for Siemens DICOM data with phoenix protocol (tag = [0x29,0x1020]).
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 %
-% EXAMPLE:          pathDataset = '...\test-data';
-%                   phoenixProtocol = xASL_bids_PhoenixProtocolReader(pathDataset);
+% EXAMPLE:          pathData = '...\test-data';
+%                   [parameterList,phoenixProtocol] = xASL_bids_PhoenixProtocolReader(pathData);
 %
 % REFERENCES:       ...
 % __________________________________
@@ -23,30 +25,88 @@ function phoenixProtocol = xASL_bids_PhoenixProtocolReader(pathDataset)
 
 
     %% Input Check
-
-    % Check if both root folders are valid char arrays or strings
-    if ~(ischar(pathDataset) || isstring(pathDataset))
-        error('Input path is neither a char array not a string...');
-    end
-
-    % Default value for bPrintReport
+    
+    % Check number of input parameters
     if nargin < 1
         error('Missing input parameters...');
     end
 
+    % Check if the input path is a character array or a string
+    if ~(ischar(pathData) || isstring(pathData))
+        error('Input path is neither a char array not a string...');
+    end
+    
+    % Get file parts
+    [filePath,fileName,fileExtension] = fileparts(pathData);
+    
+    % Check extension
+    if ~strcmp(fileExtension,'.dcm')
+        error('Input path is not a DICOM file...');
+    end
 
     %% Defaults
+    
+    debug = false;
+    parameterList = {'Name','Value'};
+    startOfProtocol = '### ASCCONV BEGIN';
+    endOfProtocol = '### ASCCONV END';
 
-    % Initialize phoenix protocol
-    phoenixProtocol = struct;
-
-
-    %% Read DICOM file
-
+    %% Read Phoenix Protocol
+    py.importlib.import_module('pydicom');
+    ds = py.pydicom.dcmread(pathData,false,true);
+    phoenixProtocol = char(ds{2691104}.value); % 2691104 = (0x29,0x1020)
+    phoenixProtocol = [strsplit(phoenixProtocol,'\\n')]';
+    
+    % Remove tabs
+    for line=1:numel(phoenixProtocol)
+        phoenixProtocol{line,1} = strrep(phoenixProtocol{line,1},'\t','');
+    end
+    
 
     %% Extract parameters
-
-    % function hdr = spm_dicom_headers(P, essentials)
+    
+    parIterator = 2;
+    protocolStarted = false;
+    protocolEnded = false;
+    
+    % Iterate over phoenix protocol
+    for line=1:numel(phoenixProtocol)
+        
+        % Current line
+        curLine = string(phoenixProtocol{line,1});
+        
+        % Print current line in debug mode
+        if debug
+            fprintf('%s\n',string(phoenixProtocol(line,1)));
+        end
+        
+        % Check if protocol ended
+        if curLine.contains(endOfProtocol)
+            protocolEnded = true;
+        end
+        
+        % Export parameter
+        if (protocolStarted && ~protocolEnded)
+            % Extraction
+            extrLine = char(curLine);
+            % Search for equals sign
+            posInLine = strfind(curLine, ' = ');
+            % Get key and value
+            keyLine = extrLine(1:posInLine-1);
+            valLine = extrLine(posInLine+3:end);
+            % Assign them to the parameter list
+            parameterList{parIterator,1} = keyLine;
+            parameterList{parIterator,2} = valLine;
+            parIterator = parIterator+1;
+        end
+        
+        % Check if protocol started
+        if curLine.contains(startOfProtocol)
+            protocolStarted = true;
+        end
+        
+    end
+    
 
 
 end
