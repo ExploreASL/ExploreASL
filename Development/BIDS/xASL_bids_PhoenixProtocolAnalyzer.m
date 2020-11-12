e74916b0d215231037072fdc88c35bf7a63e1829
Michael Stritt <stritt.michael@gmail.com>
2020-11-12 13:24:15 +0100

    #223 Phoenix protocol analyzer initial commit

---
+++ Development/BIDS/xASL_bids_PhoenixProtocolAnalyzer.m
@@ -0,0 +1,34 @@
+function xASL_bids_PhoenixProtocolAnalyzer(parameterList)
+%xASL_bids_PhoenixProtocolAnalyzer This function analyzes the parameter list of the phoenix protocol (tag = [0x29,0x1020]).
+%
+% FORMAT: xASL_bids_PhoenixProtocolAnalyzer(parameterList);
+%
+% INPUT:
+%        parameterList      - list of parameters from the reduced phoenix protocol (can be generated using xASL_bids_PhoenixProtocolReader) (REQUIRED)
+%
+% OUTPUT:
+%        n/a
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION:      This function analyzes the parameter list of the phoenix protocol (tag = [0x29,0x1020]).
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+%
+% EXAMPLE:          xASL_bids_PhoenixProtocolAnalyzer(parameterList);
+%
+% REFERENCES:       ...
+% __________________________________
+% Copyright @ 2015-2020 ExploreASL
+
+
+    
+    
+
+
+end
+
+
+
+
+
+
