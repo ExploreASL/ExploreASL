cde453491ed062dda602e76e58be6d1455e479c6
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2020-12-13 12:20:03 +0100

    #179 Revamp xASL_im_ComputeDice

--- Functions/xASL_im_ComputeDice.m
+++ Functions/xASL_im_ComputeDice.m
@@ -1,30 +1,53 @@
-function DiceCoeff = xASL_im_ComputeDice(imA, imB)
+function [DiceCoeff] = xASL_im_ComputeDice(imA, imB)
 %xASL_im_ComputeDice Calculate Dice coefficient of image overlap
 %
-% FORMAT:       DiceCoeff = xASL_im_ComputeDice(imA, imB)
+% FORMAT:       [DiceCoeff] = xASL_im_ComputeDice(imA, imB)
 % 
-% INPUT:        ...
+% INPUT:
+%   imA         - image matrix A (numerical or logical; REQUIRED)
+%   imB         - image matrix B (numerical or logical; REQUIRED)
 %
-% OUTPUT:       ...
+% OUTPUT:
+%    DiceCoeff  - Dice coefficient, 0 = no image overlap, 1 = perfect image
+%                 overlap.
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION:  This function calculates the Dice coefficient of image overlap.
 % 
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION:  Calculate Dice coefficient of image overlap.
-%
-% -----------------------------------------------------------------------------------------------------------------------------------------------------
-% EXAMPLE:      ...
+% EXAMPLE: DiceCoeff = xASL_im_ComputeDice([1 0 1], [1 1 0]); % mask has
+% two voxels, of which 1 voxel overlaps, so DiceCoeff=0.5
 % __________________________________
 % Copyright 2015-2020 ExploreASL
 
-if ~islogical(imA)
-    warning('Input image was not dichotomous, dichotomizing now');
+
+%% 0. Admin
+if nargin<2
+    error('Input image(s) missing');
+end    
+if ~islogical(imA) && isnumeric(imA)
+    warning('Input image A was not dichotomous, dichotomizing now');
     imA = imA>0;
+elseif ~islogical(imA) && ~isnumeric(imA)
+    error('Input image A has the wrong format');
 end
-if ~islogical(imB)
-    warning('Input image was not dichotomous, dichotomizing now');
+if ~islogical(imB)  && isnumeric(imB)
+    warning('Input image B was not dichotomous, dichotomizing now');
     imB = imB>0;
+elseif ~islogical(imB) && ~isnumeric(imB)
+    error('Input image B has the wrong format');
 end
 
-DiceCoeff(1)   = (2 .* sum(sum(sum(imA & imB)))) ./ (sum(imA(:)) + sum(imB(:)) );
+if ~isequal(size(imA), size(imB))
+    error('Input image matrices A & B must have the same size');
+end
+
+%% 1. Calculate Dice coefficient
+if sum(imA(:))==0 && sum(imB(:))==0
+    warning('Two empty maps, DICE overlap 1');
+    DiceCoeff = 1;
+else
+    DiceCoeff = (2 .* sum(sum(sum(imA & imB)))) ./ (sum(imA(:)) + sum(imB(:)) );
+end
 
 
 end
