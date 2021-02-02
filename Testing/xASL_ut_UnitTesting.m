620c9c0ca5c9e6e6a3d11678fe3618c67ce80e4b
MichaelStritt <stritt.michael@gmail.com>
2021-02-02 10:03:07 +0100

    #294 Move tests to subfolder, add input check

--- Testing/xASL_ut_UnitTesting.m
+++ Testing/xASL_ut_UnitTesting.m
@@ -17,54 +17,54 @@ function UnitTests = xASL_ut_UnitTesting
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % Copyright 2015-2021 ExploreASL
 
-%% Initialize Paths
+    %% Initialize Paths
 
-% Add testing directory
-TestingPath = mfilename('fullpath');
-filepath = fileparts(TestingPath);
-addpath(filepath)
+    % Add testing directory
+    TestingPath = mfilename('fullpath');    % Path of the current script
+    filepath = fileparts(TestingPath);      % Testing directory
+    xASL_dir = fileparts(filepath);         % All subfolders of xASL directory
+    warning('off','all')                    % Turn of warnings (only add path related)
+    addpath(genpath(xASL_dir))              % Add all scripts to path (including testing directory)
+    warning('on','all')
 
-% Add all subfolders of xASL directory
-xASL_dir = fileparts(filepath);
-warning('off','all')
-addpath(genpath(xASL_dir))
-warning('on','all')
+    %% Clean Up
+    clc
+    clear
 
-%% Clean Up
-clc
-clear
-
-%% Get Test Repository
-if usejava('desktop')
-    TestRepository = uigetdir([],'Select test repository...');
-else
-    TestRepository = input('Insert test repository: ');
-end
-
-%% Test Workflow
-
-% Unit test: xASL_tsvRead
-UnitTests(1) = xASL_ut_UnitTest_function_tsvRead(TestRepository);
-
-% Unit test: xASL_tsvWrite
-UnitTests(2) = xASL_ut_UnitTest_function_tsvWrite(TestRepository);
-
-%% Print test results
-clc
-fprintf('================================= TEST RESULTS =================================\n')
-for it = 1:numel(UnitTests)
-    fprintf('TEST:\t\t%s\n',UnitTests(it).name)
-    if UnitTests(it).passed
-        fprintf('RESULTS:\t%s\n','Passed')
+    %% Get Test Repository
+    if usejava('desktop')
+        TestRepository = uigetdir([],'Select test repository...');
     else
-        fprintf('RESULTS:\t%s\n','Failed')
+        TestRepository = input('Insert test repository: ');
     end
+    if ~ischar(TestRepository)
+        error('Test repository path required...');
+    end
+
+    %% Test Workflow
+
+    % Unit test: xASL_tsvRead
+    UnitTests(1) = xASL_ut_UnitTest_function_tsvRead(TestRepository);
+
+    % Unit test: xASL_tsvWrite
+    UnitTests(2) = xASL_ut_UnitTest_function_tsvWrite(TestRepository);
+
+    %% Print test results
+    clc
+    fprintf('================================= TEST RESULTS =================================\n')
+    for it = 1:numel(UnitTests)
+        fprintf('TEST:\t\t%s\n',UnitTests(it).name)
+        if UnitTests(it).passed
+            fprintf('RESULTS:\t%s\n','Passed')
+        else
+            fprintf('RESULTS:\t%s\n','Failed')
+        end
+    end
+    fprintf('================================================================================\n')
+
+
+
 end
-fprintf('================================================================================\n')
-
-
-
-
 
 
 
