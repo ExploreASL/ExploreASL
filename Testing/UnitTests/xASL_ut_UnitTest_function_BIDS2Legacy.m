72f87b172652337db0216df7d93b3150698f8071
MichaelStritt <stritt.michael@gmail.com>
2021-02-26 23:55:05 +0100

    #361 Dataset configuration

--- Testing/UnitTests/xASL_ut_UnitTest_function_BIDS2Legacy.m
+++ Testing/UnitTests/xASL_ut_UnitTest_function_BIDS2Legacy.m
@@ -36,16 +36,56 @@ testTime = tic;
 droTestPatientSource = fullfile(TestRepository,'UnitTesting','dro_files','test_patient');
 droTestPatient = fullfile(TestRepository,'UnitTesting','working_directory','test_patient');
 
+% DRO subject
+droSubject = 'sub-Sub1';
+
 % Copy test data to working directory
-copyfile(droTestPatientSource,fullfile(droTestPatient,'rawdata'));
-    
+xASL_Copy(droTestPatientSource,fullfile(droTestPatient,'rawdata',droSubject));
+
+% Rename asl to perf
+perfDirectory = fullfile(droTestPatient,'rawdata',droSubject,'perf');
+xASL_Move(fullfile(droTestPatient,'rawdata',droSubject,'asl'),perfDirectory);
+xASL_Move(fullfile(perfDirectory,'001_asl.json'),fullfile(perfDirectory,[droSubject,'_asl.json']));
+xASL_Move(fullfile(perfDirectory,'001_asl.nii.gz'),fullfile(perfDirectory,[droSubject,'_asl.nii.gz']));
+xASL_Move(fullfile(perfDirectory,'001_aslcontext.tsv'),fullfile(perfDirectory,[droSubject,'_aslcontext.tsv']));
+
 % Run BIDS2Legacy
-warning('We need to add the dataset_description.json to the rawdata folder first...');
-% xASL_bids_BIDS2Legacy(droTestPatient);
+
+% Create dummy dataset_description.json
+json = struct;
+json.Name = "DRO_Digital_Reference_Object";
+json.BIDSVersion = "1.5.0";
+json.DatasetType = "raw";
+json.License = "RandomText";
+json.Authors = "RandomText";
+json.Acknowledgements = "RandomText";
+json.HowToAcknowledge = "Please cite this paper: https://www.ncbi.nlm.nih.gov/pubmed/001012092119281";
+json.Funding = "RandomText";
+json.EthicsApprovals = "RandomText";
+json.ReferencesAndLinks = "RandomText";
+json.DatasetDOI = "RandomText";
+
+% Write file
+spm_jsonwrite(fullfile(droTestPatient,'rawdata','dataset_description.json'),json);
+
+% Run BIDS2Legacy
+xASL_bids_BIDS2Legacy(droTestPatient);
 
 % Define one or multiple test conditions here
 testCondition = true; % Fallback
-if ~exist(fullfile(droTestPatient,'sourcedata'),'dir')
+if ~exist(fullfile(droTestPatient,'derivatives'),'dir')
+    testCondition = false; % Test failed
+end
+if ~exist(fullfile(droTestPatient,'derivatives','ExploreASL','DataPar.json'),'file')
+    testCondition = false; % Test failed
+end
+if ~exist(fullfile(droTestPatient,'derivatives','ExploreASL',droSubject,'ASL_1','ASL4D.json'),'file')
+    testCondition = false; % Test failed
+end
+if ~exist(fullfile(droTestPatient,'derivatives','ExploreASL',droSubject,'ASL_1','ASL4D.nii.gz'),'file')
+    testCondition = false; % Test failed
+end
+if ~exist(fullfile(droTestPatient,'derivatives','ExploreASL',droSubject,'ASL_1','ASL4D_aslcontext.tsv'),'file')
     testCondition = false; % Test failed
 end
 
