7faa945e5b5ddbe10d2a6671d863293115d27c2c
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-03-07 15:37:20 +0100

    #398 xASL_bids_TestBidsConversion: Code shortening: repetitive DirASL definition

--- Development/xASL_bids_TestBidsConversion.m
+++ Development/xASL_bids_TestBidsConversion.m
@@ -41,10 +41,10 @@ end
 
 %% Initialization
 % Initialize ExploreASL 
-ExploreASL_Initialize([],false);
+ExploreASL_Initialize([], false);
 
 % Load the list of the directories
-fList = xASL_adm_GetFileList(baseDirImport,[],false,[],true);
+fList = xASL_adm_GetFileList(baseDirImport, [], false, [], true);
 
 %% Go through all studies and import them
 if bImport
@@ -55,69 +55,71 @@ if bImport
 			% Afterwards we can continue with the NII+JSON->ASL-BIDS
 			case 'Siemens_PCASL_3DGRASE_vascular'
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [1 0 0], false, true, false, false);
-                xASL_adm_DeleteFileList(fullfile(baseDirImport, 'Siemens_PCASL_3DGRASE_vascular', 'analysis', 'Sub1', 'ASL_1'),'^ASL4D_7.*$',1);
-                xASL_adm_DeleteFileList(fullfile(baseDirImport, 'Siemens_PCASL_3DGRASE_vascular', 'analysis', 'Sub1', 'ASL_1'),'^ASL4D_8.*$',1);
-                xASL_adm_DeleteFileList(fullfile(baseDirImport, 'Siemens_PCASL_3DGRASE_vascular', 'analysis', 'Sub1', 'ASL_1'),'^ASL4D_9.*$',1);
-                xASL_adm_DeleteFileList(fullfile(baseDirImport, 'Siemens_PCASL_3DGRASE_vascular', 'analysis', 'Sub1', 'ASL_1'),'^ASL4D_10.*$',1);
-				nii_files = xASL_adm_GetFileList([baseDirImport 'Siemens_PCASL_3DGRASE_vascular', 'analysis', 'Sub1', 'ASL_1'],'^*.nii$','FPList',[],false);
+                DirASL = fullfile(baseDirImport, fList{ii}, 'analysis', 'Sub1', 'ASL_1');
+                
+                xASL_adm_DeleteFileList(DirASL, '^ASL4D_7.*$', 1);
+                xASL_adm_DeleteFileList(DirASL, '^ASL4D_8.*$', 1);
+                xASL_adm_DeleteFileList(DirASL, '^ASL4D_9.*$', 1);
+                xASL_adm_DeleteFileList(DirASL, '^ASL4D_10.*$', 1);
+				nii_files = xASL_adm_GetFileList(DirASL, '^*.nii$', 'FPList', [], false);
 				nii_files = xASL_bids_MergeNifti(nii_files, 'ASL');
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [0 1 0], false, true, false, false);
 				
 			case 'Philips_PCASL_3DGRASE_R5.4_TopUp'
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [1 0 0], false, true, false, false);
-				xASL_Move(fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0_1.nii'),...
-                          fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0.nii'), 1);
-				xASL_Move(fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0_1.json'),...
-                          fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0.json'), 1);
-				xASL_Move(fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0_2.nii'),...
-                          fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0PERev.nii'), 1);
-				xASL_Move(fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0_2.json'),...
-                          fullfile(baseDirImport, 'Philips_PCASL_3DGRASE_R5.4_TopUp', 'analysis', 'Sub1', 'ASL_1', 'M0PERev.json'), 1);
+                DirASL = fullfile(baseDirImport, fList{ii}, 'analysis', 'Sub1', 'ASL_1');
+                
+				xASL_Move(fullfile(DirASL, 'M0_1.nii'), fullfile(DirASL, 'M0.nii'), 1);
+				xASL_Move(fullfile(DirASL, 'M0_1.json'), fullfile(DirASL, 'M0.json'), 1);
+				xASL_Move(fullfile(DirASL, 'M0_2.nii'), fullfile(DirASL, 'M0PERev.nii'), 1);
+				xASL_Move(fullfile(DirASL, 'M0_2.json'), fullfile(DirASL, 'M0PERev.json'), 1);
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [0 1 0], false, true, false, false);
 				
 			case 'Siemens_PCASL_volunteer'
 				ExploreASL_ImportBIDS(fullfile(baseDirImport,fList{ii}), [],[], [1 0 0], false, true, false, false);
-				if xASL_exist(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', 'ASL4D_NS.nii'))
-					xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_NS.json'));
-					xASL_Move(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_SS.json'),...
-                              fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D.json'), 1);
-					imNS = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_NS.nii'));
-					imSS = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_SS.nii'));
+                DirASL = fullfile(baseDirImport, fList{ii}, 'analysis', 'Sub1', 'ASL_1');
+                
+				if xASL_exist(fullfile(DirASL, 'ASL4D_NS.nii'))
+					xASL_delete(fullfile(DirASL, ASL4D_NS.json'));
+					xASL_Move(fullfile(DirASL, ASL4D_SS.json'), fullfile(DirASL, ASL4D.json'), 1);
+                              
+					imNS = xASL_io_Nifti2Im(fullfile(DirASL, ASL4D_NS.nii'));
+					imSS = xASL_io_Nifti2Im(fullfile(DirASL, ASL4D_SS.nii'));
 					imNS(:,:,:,2) = imSS;
-					xASL_io_SaveNifti(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_NS.nii'),...
-                        fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D.nii'), imNS/10, [], 1, []);
-					xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_NS.nii'));
-					xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', ASL4D_SS.nii'));
-					xASL_Move(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', M0_2.json'),...
-                              fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', M0PERev.json'), 1);
-					xASL_Move(fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', M0_2.nii'),...
-                              fullfile(baseDirImport, 'Siemens_PCASL_volunteer', 'analysis', 'Sub1', 'ASL_1', M0PERev.nii'), 1);
+					xASL_io_SaveNifti(fullfile(DirASL, ASL4D_NS.nii'), fullfile(DirASL, ASL4D.nii'), imNS/10, [], 1);
+                        
+					xASL_delete(fullfile(DirASL, ASL4D_NS.nii'));
+					xASL_delete(fullfile(DirASL, ASL4D_SS.nii'));
+                    
+					xASL_Move(fullfile(DirASL, M0_2.json'), fullfile(DirASL, M0PERev.json'), 1);
+					xASL_Move(fullfile(DirASL, M0_2.nii'), fullfile(DirASL, M0PERev.nii'), 1);
 				end
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [0 1 0], false, true, false, false);
 				
 			case 'Siemens_PCASL_multiTI'
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [1 0 0], false, true, false, false);
-				if xASL_exist(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', 'ASL4D_NS_300.nii'))
+                DirASL = fullfile(baseDirImport, fList{ii}, 'analysis', 'Sub1', 'ASL_1');
+                
+				if xASL_exist(fullfile(DirASL, 'ASL4D_NS_300.nii'))
 					mTIvec = [300,600,900,1200,1500,1800,2100,2400,2700,3000];
 					for jj = 1:length(mTIvec)
 						if jj>1
-                            xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.json']));
-                            xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.json']));
-							imNSSS(:,:,:,2*(jj-1)+1) = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
-							imNSSS(:,:,:,2*(jj-1)+2) = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
+                            xASL_delete(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.json']));
+                            xASL_delete(fullfile(DirASL, ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.json']));
+							imNSSS(:,:,:,2*(jj-1)+1) = xASL_io_Nifti2Im(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
+							imNSSS(:,:,:,2*(jj-1)+2) = xASL_io_Nifti2Im(fullfile(DirASL, ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
                         else
-                            xASL_Move(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.json']),...
-                                      fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', 'ASL4D.json'));
-                            xASL_delete(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.json']));
-							imNSSS = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
-							imNSSS(:,:,:,2) = xASL_io_Nifti2Im(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
+                            xASL_Move(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.json']), fullfile(DirASL, 'ASL4D.json'));
+                            xASL_delete(fullfile(DirASL, ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.json']));
+							imNSSS = xASL_io_Nifti2Im(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
+							imNSSS(:,:,:,2) = xASL_io_Nifti2Im(fullfile(DirASL, ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
 						end
 					end
-					xASL_io_SaveNifti(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(1)) '.nii']),...
-                        fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', 'ASL4D.nii'), imNSSS/10, [], 1, []);
+					xASL_io_SaveNifti(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(1)) '.nii']),...
+                        fullfile(DirASL, 'ASL4D.nii'), imNSSS/10, [], 1, []);
 					for jj = 1:length(mTIvec)
-						xASL_delete(fullfile(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
-						xASL_delete(fullfile(fullfile(baseDirImport, 'Siemens_PCASL_multiTI', 'analysis', 'Sub1', 'ASL_1', ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
+						xASL_delete(fullfile(DirASL, ['ASL4D_NS_' xASL_num2str(mTIvec(jj)) '.nii']));
+						xASL_delete(fullfile(DirASL, ['ASL4D_SS_' xASL_num2str(mTIvec(jj)) '.nii']));
 					end
 				end
 				ExploreASL_ImportBIDS(fullfile(baseDirImport, fList{ii}), [],[], [0 1 0], false, true, false, false);
