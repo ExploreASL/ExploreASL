347942a4f8f662a8d083674674cba0e8e5dc1f96
Petr <j.petr@hzdr.de>
2021-03-18 12:13:56 +0100

    #423 xASL_im_ResampleIM: Improved header

--- External/SPMmodified/xASL/xASL_im_ResampleIM.m
+++ External/SPMmodified/xASL/xASL_im_ResampleIM.m
@@ -1,31 +1,41 @@
-function [imOut] = xASL_im_ResampleIM(imIn,matIn,matOut,dimOut,intMethod)
-% xASL_im_ResampleIM Resamples an image using Matlab interp3 function
+function [imOut] = xASL_im_ResampleIM(imIn, matIn, matOut, dimOut, interpolationType)
+%xASL_im_ResampleIM Resamples an image with provided input and output orientation matrices
 %
-% IntMethod can be one of 'linear' 'cubic' 'or 'nearest'
-%
-% FORMAT: [imOut] = xASL_im_ResampleIM(imIn,matIn,matOut,dimOut,intMethod)
+% FORMAT: [imOut] = xASL_im_ResampleIM(imIn, matIn, matOut, dimOut[, interpolationType])
 %
 % INPUT:
-%   imIn	    - ...
-%   matIn	    - ...
-%   matOut	    - ...
-%   dimOut	    - ...
-%   intMethod   - ...
+%   imIn	          - input image for resampling - an image matrix (REQUIRED)
+%   matIn	          - 4x4 orientation matrix of the input image (REQUIRED)
+%   matOut	          - 4x4 orientation matrix of the output image (REQUIRED)
+%   dimOut	          - 4D vector of output image dimensions (REQUIRED)
+%   interpolationType - Type of interpolation used by interp3 - 'linear' 'cubic' 'or 'nearest'
+%                       (OPTIONAL, DEFAULT='linear')
 %
 % OUTPUT:
-%  imOut        - ...
+%  imOut        - Resampled image matrix
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION: Resamples an image using Matlab interp3 function.
+% DESCRIPTION: Resamples an input image imIn oriented according to the homogeneous matrix matIn to and output
+%              image imOut that has dimension dimOut and matrix matOut. This allows to resample images between
+%              two spaces with different orientation and matrix sizes. It uses the Matlab interp3 function and
+%              the interpolation method used by this method can be chosen. Note that the recommended use of this
+%              function is resampling between space with similar resolution or upsampling. For downsampling, simple
+%              interpolation does not delivery correct results respecting the point-spread-function and a combination of
+%              xASL_im_PreSmooth and xASL_spm_reslice should be used instead.
 %
-% EXAMPLE:     n/a
+% EXAMPLE:     imOut = xASL_im_ResampleIM(imIn,[1 0 0 10;0 1 0 2;0 0 1 3; 0 0 0 1],[31 0 0 0;0 2 0 2;0 0 2 3; 0 0 0 1],[50 50 50], 'linear')
 %
 % __________________________________
 % Copyright 2015-2021 ExploreASL
 
-if ~exist('intMethod','var')
-    % default, CAT12 also uses linear interpolation & single precision:
-    intMethod  = 'linear';
+% Input parameter admin
+if nargin < 4
+	error('Need to specify input image, dimensions and input and output orientation matrix');
+end
+
+if nargin < 5 || isempty('interpolationType')
+    % Default, CAT12 also uses linear interpolation & single precision:
+    interpolationType  = 'linear';
 end
 
 % Dimension of the input
@@ -47,9 +57,8 @@ Zout = Zout(:);
 XYZresampled = (matIn\matOut) * [Xout';Yout';Zout';ones(size(Zout'))];
 
 % Run the interpolation
-imOut = interp3(Xin',Yin',Zin',double(imIn),XYZresampled(1,:),XYZresampled(2,:),XYZresampled(3,:),intMethod);
+imOut = interp3(Xin',Yin',Zin',double(imIn),XYZresampled(1,:),XYZresampled(2,:),XYZresampled(3,:),interpolationType);
 imOut(isnan(imOut)) = 0; % remove nans
 imOut = reshape(imOut,dimOut); % reshape back
 
-return
-
+end
