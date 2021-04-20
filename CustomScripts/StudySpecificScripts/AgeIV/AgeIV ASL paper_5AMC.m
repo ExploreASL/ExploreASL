d09b30b0a4a755c693a204be75377130cb390f84
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-04-20 13:06:30 +0200

    Cleaning up for EPAD release

--- CustomScripts/StudySpecificScripts/AgeIV/AgeIV ASL paper_5AMC.m
+++
@@ -1,5 +0,0 @@
-%slides chosen:
-
-    % 49 -> subcortex, anterior cingulate
-    % 63 -> insula & posterior cingulate
-    % 84 -> top of brain
--- CustomScripts/StudySpecificScripts/Twins/ParallelProcessingTwins_Viktory.sh
+++
@@ -1,9 +0,0 @@
-let nParallel=10;
-MatlabPath=/usr/local/apps/matlab/R2018b/bin/matlab;
-DataFolder=/radshare/Twins/Twins_ExploreASL; 
-DataParPath=$DataFolder/DATA_PAR_TwinStudy.json;
-xASLdir=/radshare/ExploreASL_Test/ExploreASL;
-cd $xASLdir
-for (( i=1; i<=$nParallel; i++ ));
-do screen -dmSL TWINS$i nice -n 10 $MatlabPath -nodesktop -nosplash -r "cd('$xASLdir');ExploreASL_Master('$DataParPath', true, true, $i, $nParallel);system('screen -SX TWINS$i kill');" &
-done
