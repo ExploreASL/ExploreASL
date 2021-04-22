7d56e5bfaad2c6556302c4909647555ac2cbe0d6
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-04-22 21:26:43 +0200

    #502 xASL_spm_smooth: Fix for smoothing a 4D NIfTI (as multiple 3D NIfTIs)

--- External/SPMmodified/xASL/xASL_spm_smooth.m
+++ External/SPMmodified/xASL/xASL_spm_smooth.m
@@ -26,11 +26,19 @@ function xASL_spm_smooth(pathIn, fwhmSmooth, pathNew)
 % Admin
 
 if nargin < 2
-	error('xASL_spm_smooth: Needs at least two input arguments.');
+	error('Needs at least two input arguments');
+elseif isempty(pathIn)
+    error('pathIn should be defined');
+elseif isempty(fwhmSmooth)
+    error('fwhmSmooth should be defined');
+elseif ~ischar(pathIn)
+    error('pathIn should be a char array');    
+elseif ~isnumeric(fwhmSmooth)
+    error('fwhmSmooth should be numerical');    
 end
 
 if length(fwhmSmooth) ~= 3
-	error('xASL_spm_smooth: The smoothing kernel must have size 3.');
+	error('The smoothing kernel must have size 3');
 end
 
 if nargin<3 || isempty(pathNew)
@@ -46,23 +54,24 @@ Dims5 = size(nii.dat,5);
 % ------------------------------------------------------------------------------------------
 % Save multiple volumes as individual NIfTIs
 % ------------------------------------------------------------------------------------------
-for i4=1:Dims4
-	for i5=1:Dims5
-		% Save to 3D volumes and keep all the names
-		if i4*i5 == 1
-			Path2Smooth{1} = pathIn;
-			Dim4(1) = 1;
-			Dim5(1) = 1;
-			NN = 2;
-		else
-			Path2Smooth{NN,1} = fullfile(Fpath,[Ffile '_' num2str(NN) '.nii']);
-			xASL_io_SaveNifti(pathIn,Path2Smooth{NN},nii.dat(:,:,:,i4,i5),[], false);
+bMultipleVolumes = Dims4>1 || Dims5>1;
 
-			Dim4(NN) = i4;
-			Dim5(NN) = i5;
-			NN  = NN+1;
-		end
-	end
+if bMultipleVolumes
+    fprintf('%s\n','Preparing smoothing:   ');
+    for i4=1:Dims4
+        for i5=1:Dims5
+            % Save to 3D volumes and keep all the names
+            NN = (i4-1)*i5+i5;
+            xASL_TrackProgress(NN, Dims4*Dims5);
+            Path2Smooth{NN,1} = fullfile(Fpath,[Ffile '_' num2str(NN) '.nii']); % enforce .nii instead of .nii.gz
+            xASL_io_SaveNifti(pathIn,Path2Smooth{NN},nii.dat(:,:,:,i4,i5),[], false);
+
+            Dim4(NN) = i4;
+            Dim5(NN) = i5;
+        end
+    end
+else
+    Path2Smooth = xASL_spm_admin(pathIn, 0);
 end
 
 % ------------------------------------------------------------------------------------------
@@ -81,21 +90,24 @@ fprintf('%s\n',['Smoothed ' Ffile Fext ' with [' num2str(fwhmSmooth(1),3) ' ' nu
 % ------------------------------------------------------------------------------------------
 % Reconstruct image from multiple volumes
 % ------------------------------------------------------------------------------------------
-if length(Path2Smooth)>1
+if bMultipleVolumes
 	% Allocated the smoothed image
-	imSmoothed = zeros(size(nii.dat));
+    fprintf('\n%s', 'Putting volumes back together:   ');
+    
+	imSmoothed = zeros(size(nii.dat), 'single');
     for iVolume=1:length(Path2Smooth)
+        xASL_TrackProgress(iVolume, length(Path2Smooth));
+        
 		% Load the smoothed image
         [Fpath2, Ffile2, Fext2] = xASL_fileparts(Path2Smooth{iVolume});
         SmoothedPath = fullfile(Fpath2,['s' Ffile2 Fext2]);
         imSmoothed(:,:,:,Dim4(iVolume),Dim5(iVolume)) = xASL_io_Nifti2Im(SmoothedPath);
 
-		% Delete the (two) temporary images
+		% Delete the two temporary images
         xASL_delete(SmoothedPath);
-        if iVolume>1
-            xASL_delete(Path2Smooth{iVolume});
-        end
-	end
+        xASL_delete(Path2Smooth{iVolume});
+    end
+    fprintf('\n');
 
 	% Save the completed image
     TempName = fullfile(Fpath, ['s' Ffile Fext]);
@@ -113,5 +125,6 @@ if ~isempty(pathNew) % otherwise don't rename, keep prefix "s" for smoothed
         xASL_Move(TempName,pathNew,1);
     end
 end
+
 
 end
