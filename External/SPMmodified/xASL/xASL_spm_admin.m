96d332ac36b7c310c19942aba987370f5a524270
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-04-22 21:28:06 +0200

    #502 xASL_spm_admin: Revamp header & comments

--- External/SPMmodified/xASL/xASL_spm_admin.m
+++ External/SPMmodified/xASL/xASL_spm_admin.m
@@ -1,61 +1,73 @@
-function [InPath] = xASL_spm_admin(InPath, bPadComma1)
-%xASL_spm_admin Force ,1 at end of IMname.
-% This is useful for refIM/srcIM in CoregInit, OldNormalizeWrapper etc
-% IMname should be a cell
-% bPadComma1 is to add the ,1 to the end of the pathstring, which SPM uses
-% to assign the first image of a 4D image array (OPTIONAL, DEFAULT = true)
+function [pathOut] = xASL_spm_admin(pathIn, bPadComma1)
+%xASL_spm_admin Manage SPM input path format
 %
-% FORMAT: [InPath] = xASL_spm_admin(InPath, bPadComma1)
+% FORMAT: xASL_spm_admin(pathIn[, bPadComma1])
 %
 % INPUT:
-%
-% InPath      - ...
-% bPadComma1  - ...
+%   pathIn     - path to NifTI image, including full foldername and extension (REQUIRED)
+%   bPadComma1 - boolean specifying if the ',1' is added, 
+%                to assign the first image of an SPM 4D array (OPTIONAL, DEFAULT = FALSE)
 %
 % OUTPUT:
-% InPath      - ...
-%
-% DESCRIPTION: Force ,1 at end of IMname. This is useful for refIM/srcIM in CoregInit, OldNormalizeWrapper etc.
-%
-% EXAMPLE: n/a
+%   pathOut    - corrected path to NifTI image
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This SPM admin function takes a NIfTI path and does a few
+% checks to make this valid to SPM. It accepts both .nii and .nii.gz.
+% It runs the following steps:
+% 1. Unzip .nii.gz
+% 2. Convert char to cell
+% 3. Add ',1' suffix
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE: xASL_spm_admin('/MyStudy/Subject1/T1.nii.gz');
 % __________________________________
 % Copyright 2015-2021 ExploreASL
 
-if nargin<2 || isempty(bPadComma1)
-    bPadComma1 = true; % default
-end
 
+    if nargin<1 || isempty(pathIn)
+        error('Too few input arguments');
+    elseif ~ischar(pathIn)
+        error('pathIn should be a char array');
+    end    
 
-    %% Manage unzipping
-    [~, InPath]  = xASL_io_ReadNifti(InPath);
-
-    %% -----------------------------------------------------
-    %% Manage iscell
-	if ~iscell(InPath)
-		tempL = InPath;
-		IMnameOut{1} = tempL;
-	else
-		IMnameOut = InPath;
+    if nargin<2 || isempty(bPadComma1)
+        bPadComma1 = true; % default
     end
 
-    InPath = IMnameOut;
-
     %% -----------------------------------------------------
-    %% Manage ',1' suffix
+    %% 1. Unzip .nii.gz
+    [~, pathIn] = xASL_io_ReadNifti(pathIn);
+
+    
+    %% -----------------------------------------------------
+    %% 2. Convert char to cell
+	if ~iscell(pathIn)
+		tempL = pathIn;
+		IMnameOut{1} = tempL;
+	else
+		IMnameOut = pathIn;
+    end
+
+    pathIn = IMnameOut;
+
+    
+    %% -----------------------------------------------------
+    %% 3. Add ',1' suffix
     if bPadComma1
         String2Pad = ',1';
     else
         String2Pad = '';
     end
     
-    FoundStr = strfind(InPath{1}, ',1');
+    FoundStr = strfind(pathIn{1}, ',1');
     if isempty(FoundStr)
-        InPath{1} = [InPath{1} String2Pad];
+        pathIn{1} = [pathIn{1} String2Pad];
     else
         FoundStr = FoundStr(length(FoundStr));
-        if FoundStr+1~=length(InPath{1})
-            InPath{1} = [InPath{1} String2Pad];
+        if FoundStr+1~=length(pathIn{1})
+            pathIn{1} = [pathIn{1} String2Pad];
         end
     end
 
+    pathOut = pathIn;
+    
 end
