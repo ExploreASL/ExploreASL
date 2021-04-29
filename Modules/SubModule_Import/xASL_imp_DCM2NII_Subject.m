5cd885512473a2adcc781c9887879907e0fa0e52
MichaelStritt <stritt.michael@gmail.com>
2021-04-29 22:21:02 +0200

    #514 xASL_imp_DCM2NII_Subject: Remove realASL context function

--- Modules/SubModule_Import/xASL_imp_DCM2NII_Subject.m
+++ Modules/SubModule_Import/xASL_imp_DCM2NII_Subject.m
@@ -419,41 +419,6 @@ function [nii_files, summary_line, globalCounts, ASLContext] = xASL_imp_DCM2NII_
 end
 
 
-%% Get real ASL Context
-function realASLContext = xASL_imp_DCM2NII_Subject_RealASLContext(parms)
 
-    % Fallback
-    realASLContext = '';
-
-    try
-        if isfield(parms,'InstanceNumber') && isfield(parms,'ImageTypes')
-            if size(parms.InstanceNumber,2)==size(parms.ImageTypes,2)
-                % Get table
-                for iNum = 1:size(parms.InstanceNumber,2)
-                    tableInNumImType{iNum,1} = parms.InstanceNumber(1,iNum);
-                    if ~isempty(parms.ImageTypes{1,iNum})
-                        tableInNumImType{iNum,2} = parms.ImageTypes(1,iNum);
-                    else
-                        tableInNumImType{iNum,2} = '';
-                    end
-                end
-                % Sort rows
-                tableInNumImType = sortrows(tableInNumImType,1);
-                % Get ASL context
-                realASLContextCell = unique([tableInNumImType{:,2}]);
-                for iContext = 1:size(realASLContextCell,2)
-                    parStruct.ImageType = realASLContextCell{1,iContext};
-                    thisContext = xASL_bids_determineImageTypeGE(parStruct);
-                    realASLContext = [realASLContext ',' thisContext];
-                end
-                realASLContext = realASLContext(2:end);
-            end
-        end
-    catch ME
-        warning('Extraction of real ASL context failed: %s', ME.message);
-    end
-
-
-end
 
 
