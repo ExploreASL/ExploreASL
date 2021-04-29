9d40f2adbc02bfd48419614819d1b8a4d8f093c1
MichaelStritt <stritt.michael@gmail.com>
2021-04-29 17:16:20 +0200

    #467 Add version check to DRO2BIDS script

--- Functions/xASL_bids_DRO2BIDS.m
+++ Functions/xASL_bids_DRO2BIDS.m
@@ -55,72 +55,80 @@ function xASL_bids_DRO2BIDS(droTestPatient,droSubject)
     % Remove the ground truth files
     xASL_delete(groundTruthDirectory,true);
     
-    %% sub-Sub1_asl.json
-
     % Read ASL JSON file
     jsonASL = spm_jsonread(fullfile(perfDirectory,[droSubject,'_asl.json']));
     
-    % Change field name from LabelingType to ArterialSpinLabelingType
-    jsonASL.ArterialSpinLabelingType = jsonASL.LabelingType;
-    jsonASL = rmfield(jsonASL,'LabelingType');
+    %% Switch software versions
+    softwareVersion = jsonASL.DROSoftwareVersion;
     
-    % Change field name from RepetitionTime to RepetitionTimePreparation
-    jsonASL.RepetitionTimePreparation = jsonASL.RepetitionTime;
-    jsonASL = rmfield(jsonASL,'RepetitionTime');
-    
-    % Change field name from MrAcquisitionType to MRAcquisitionType
-    jsonASL.MRAcquisitionType = jsonASL.MrAcquisitionType;
-    jsonASL = rmfield(jsonASL,'MrAcquisitionType');
-    
-    % Add necessary fields
-    jsonASL.M0Type = 'Separate';
-    jsonASL.BackgroundSuppression = false;
-    jsonASL.TotalAcquiredPairs = 1;
-    
-    % Write JSON file
-    spm_jsonwrite(fullfile(perfDirectory,[droSubject,'_asl.json']),jsonASL);
-    
-    %% dataset_description.json
+    switch softwareVersion
+        case '2.2.0'
+            %% sub-Sub1_asl.json
+            
+            % Change field name from LabelingType to ArterialSpinLabelingType
+            jsonASL.ArterialSpinLabelingType = jsonASL.LabelingType;
+            jsonASL = rmfield(jsonASL,'LabelingType');
 
-    % Define required fields
-    jsonTemplate.Name = 'DRO_Digital_Reference_Object';
-    
-    % Call script to fix missing fields
-    [jsonDescription] = xASL_bids_CreateDatasetDescriptionTemplate(jsonTemplate);
-    
-    % Write dataset_description file
-    spm_jsonwrite(fullfile(droTestPatient,'rawdata','dataset_description.json'),jsonDescription);
-    
-    %% sourceStructure.json
-    
-    % Define sourceStructure
-    sourceStructure.folderHierarchy = {'^(.)+$','^(ASL|T1w|M0|T2|FLAIR)$'};
-    sourceStructure.tokenOrdering = [1,0,2];
-    sourceStructure.tokenSessionAliases = {'',''};
-    sourceStructure.tokenScanAliases = {'^ASL$','ASL4D','^T1w$','T1w','^M0$','M0','^T2$','T2w','^FLAIR$','FLAIR'};
-    sourceStructure.bMatchDirectories = true;
-    
-    % Write sourceStructure file
-    spm_jsonwrite(fullfile(droTestPatient,'sourceStructure.json'),sourceStructure);
-    
-    %% studyPar.json
-    
-    % Define studyPar
-    studyPar.DatasetType = 'raw';
-    studyPar.License = 'license';
-    studyPar.Authors = {'ASPIRE Project: Gold Standard Phantoms'};
-    studyPar.Acknowledgements = 'acknowledgements';
-    studyPar.HowToAcknowledge = 'Gold Standard Phantoms';
-    studyPar.Funding = {'ASPIRE Project'};
-    studyPar.EthicsApprovals = {'ASPIRE Project'};
-    studyPar.ReferencesAndLinks = {'https://github.com/gold-standard-phantoms/asldro', 'https://pypi.org/project/asldro/', 'https://asldro.readthedocs.io/'};
-    studyPar.DatasetDOI = 'https://pypi.org/project/asldro/';
-    studyPar.LabelingType = jsonASL.ArterialSpinLabelingType;
-    studyPar.ASLContext = 'm0scan,control,label';
-    
-    % Write sourceStructure file
-    spm_jsonwrite(fullfile(droTestPatient,'studyPar.json'),studyPar);
+            % Change field name from RepetitionTime to RepetitionTimePreparation
+            jsonASL.RepetitionTimePreparation = jsonASL.RepetitionTime;
+            jsonASL = rmfield(jsonASL,'RepetitionTime');
 
+            % Change field name from MrAcquisitionType to MRAcquisitionType
+            jsonASL.MRAcquisitionType = jsonASL.MrAcquisitionType;
+            jsonASL = rmfield(jsonASL,'MrAcquisitionType');
+
+            % Add necessary fields
+            jsonASL.M0Type = 'Separate';
+            jsonASL.BackgroundSuppression = false;
+            jsonASL.TotalAcquiredPairs = 1;
+
+            % Write JSON file
+            spm_jsonwrite(fullfile(perfDirectory,[droSubject,'_asl.json']),jsonASL);
+
+            %% dataset_description.json
+
+            % Define required fields
+            jsonTemplate.Name = 'DRO_Digital_Reference_Object';
+
+            % Call script to fix missing fields
+            [jsonDescription] = xASL_bids_CreateDatasetDescriptionTemplate(jsonTemplate);
+
+            % Write dataset_description file
+            spm_jsonwrite(fullfile(droTestPatient,'rawdata','dataset_description.json'),jsonDescription);
+
+            %% sourceStructure.json
+
+            % Define sourceStructure
+            sourceStructure.folderHierarchy = {'^(.)+$','^(ASL|T1w|M0|T2|FLAIR)$'};
+            sourceStructure.tokenOrdering = [1,0,2];
+            sourceStructure.tokenSessionAliases = {'',''};
+            sourceStructure.tokenScanAliases = {'^ASL$','ASL4D','^T1w$','T1w','^M0$','M0','^T2$','T2w','^FLAIR$','FLAIR'};
+            sourceStructure.bMatchDirectories = true;
+
+            % Write sourceStructure file
+            spm_jsonwrite(fullfile(droTestPatient,'sourceStructure.json'),sourceStructure);
+
+            %% studyPar.json
+
+            % Define studyPar
+            studyPar.DatasetType = 'raw';
+            studyPar.License = 'license';
+            studyPar.Authors = {'ASPIRE Project: Gold Standard Phantoms'};
+            studyPar.Acknowledgements = 'acknowledgements';
+            studyPar.HowToAcknowledge = 'Gold Standard Phantoms';
+            studyPar.Funding = {'ASPIRE Project'};
+            studyPar.EthicsApprovals = {'ASPIRE Project'};
+            studyPar.ReferencesAndLinks = {'https://github.com/gold-standard-phantoms/asldro', 'https://pypi.org/project/asldro/', 'https://asldro.readthedocs.io/'};
+            studyPar.DatasetDOI = 'https://pypi.org/project/asldro/';
+            studyPar.LabelingType = jsonASL.ArterialSpinLabelingType;
+            studyPar.ASLContext = 'm0scan,control,label';
+
+            % Write sourceStructure file
+            spm_jsonwrite(fullfile(droTestPatient,'studyPar.json'),studyPar);
+            
+        otherwise
+            warning('Unknown DRO version...');
+    end
 
 end
 
