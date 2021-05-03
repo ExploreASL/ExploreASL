1bbf10af74e27d73084ac8cc4bbe5c105f625756
Beatriz Padrela <beatrizpadrela@hotmail.com>
2021-05-03 09:27:46 +0200

    #349 xASL_Iteration Some adjustments

--- Functions/xASL_Iteration.m
+++ Functions/xASL_Iteration.m
@@ -332,8 +332,8 @@ function [bAborted, x] = runIteration(db)
         if ~AlreadyProcessed
             fprintf('\n%s\n',repmat('+',1,72)); % just draw a separator line
             [StartI, EndI] = regexp(diaryFileEx, 'ASL_\d','ONCE'); %to find the name of the session inside diaryFileEx: ASL_with any digit after
-            session= diaryFileEx(StartI:EndI); %isolate "ASL_1 or ASL_2 etc
-             if ~isempty (StartI) %writes the session only for ASL module
+             if ~isempty(StartI) %writes the session only for ASL module
+                session = diaryFileEx(StartI:EndI); %isolate "ASL_1 or ASL_2 etc
                 fprintf('%s\n',['=== Subject: ' x.SUBJECT ', Session: ' session ', Module: ' x.MUTEXID ',  ' datestr(now) ' ===']);
             else % Structural or Population module
                 fprintf('%s\n',['=== Subject: ' x.SUBJECT ', Module: ' x.MUTEXID ',  ' datestr(now) ' ===']);
