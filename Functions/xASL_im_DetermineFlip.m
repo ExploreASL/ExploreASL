75db15c81885393173bbffb034fdb020d2bd6b3d
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-05-13 14:31:31 +0200

    xASL_im_DetermineFlip: Generalization & revamping

--- Functions/xASL_im_DetermineFlip.m
+++ Functions/xASL_im_DetermineFlip.m
@@ -1,11 +1,11 @@
-function [QCstruct] = xASL_im_DetermineFlip(x,iS,PathOrientationResults,QCstruct)
+function [QCstruct] = xASL_im_DetermineFlip(x, iSubject, PathOrientationResults, QCstruct)
 %xASL_im_DetermineFlip Check determinants, should be the same
 % before & after registration, otherwise a left-right flip is applied
 % This is not visible, but detrimental for image analysis/stats
 %
 % FORMAT:       [QCstruct] = xASL_im_DetermineFlip(x,iS,PathOrientationResults,QCstruct)
 % 
-% INPUT:        ...
+% INPUT:        iSubject -> OPTIONAL, DEFAULT = check all
 %
 % OUTPUT:       ...
 % 
@@ -19,33 +19,66 @@ function [QCstruct] = xASL_im_DetermineFlip(x,iS,PathOrientationResults,QCstruct
 % __________________________________
 % Copyright 2015-2020 ExploreASL
 
+
+if nargin<4 || isempty(QCstruct)
+    QCstruct = struct;
+end
+if nargin<3 || isempty(PathOrientationResults)
+    error('PathOrientationResults missing');
+end
+if nargin<2 || isempty(iSubject)
+    bSingleNifti = false;
+else
+    bSingleNifti = true;
+end
+
     QCstruct.LR_flip_YesNo = NaN; % default
 
-    if exist(PathOrientationResults,'file')
-        [~, CellTSV] = xASL_bids_csv2tsvReadWrite(PathOrientationResults);
-
-        % Determine correct row, differs between Matlab versions
-        
-        if size(CellTSV,1)<3 || size(CellTSV,2)<26
-                warning(['Missing data in: ' PathOrientationResults]);
-                return;
-        elseif ~isempty(CellTSV{2,13})
-                iRow    = 2;
-        elseif ~isempty(CellTSV{3,13})
-                iRow    = 3;
-        end
-    else
+    if ~exist(PathOrientationResults, 'file')
         warning(['File missing: ' PathOrientationResults]);
         return;
+    end        
+        
+    [~, CellTSV] = xASL_bids_csv2tsvReadWrite(PathOrientationResults);
+
+    % Determine correct row, differs between Matlab versions
+    if size(CellTSV,1)<3 || size(CellTSV,2)<26
+            warning(['Missing data in: ' PathOrientationResults]);
+            return;
+    end
+    
+    % if units are printed as second row, the data starts on the third row
+    if isempty(CellTSV{2,13})
+        firstRow = 3;
+    else
+        firstRow = 2;
     end
 
-    Det_Ori = xASL_str2num(CellTSV{iRow,13});
-    Det_New = xASL_str2num(CellTSV{iRow,26});
-    QCstruct.LR_flip_YesNo = uint8(~min((Det_Ori.*Det_New)>0));
-
-    if QCstruct.LR_flip_YesNo>0
-        fprintf(['LR flip found for ' x.SUBJECTS{iS}]);
-        QCstruct.LR_flip_YesNo = 1;
+    columnDetOri = find(strcmp(CellTSV(1,:), 'DetOrigin'));
+    columnDetNew = find(strcmp(CellTSV(1,:), 'DetCurrent'));
+    
+    if isempty(columnDetOri) || ~isnumeric(columnDetOri)
+        warning('Information missing for original determinant, skipping');
+        return;
+    elseif isempty(columnDetNew) || ~isnumeric(columnDetNew)
+        warning('Information missing for new determinant, skipping');
+        return;
+    end
+    
+    DeterminantOri = xASL_str2num(CellTSV(firstRow:end, columnDetOri));
+    DeterminantNew = xASL_str2num(CellTSV(firstRow:end, columnDetNew));
+    
+    if bSingleNifti
+        % standard ExploreASL behavior is to do this per subject in
+        % xASL_wrp_VisualQC* -> xASL_qc_CollectQC*        
+        QCstruct.LR_flip_YesNo = uint8(max((DeterminantOri.*DeterminantNew)<0));
+        
+        if QCstruct.LR_flip_YesNo>0
+            fprintf(['LR flip found for ' x.SUBJECTS{iSubject}]);
+            QCstruct.LR_flip_YesNo = 1;
+        end        
+    else
+        QCstruct.LR_flip_YesNo = find((DeterminantOri.*DeterminantNew)<0);
     end
     
 end
