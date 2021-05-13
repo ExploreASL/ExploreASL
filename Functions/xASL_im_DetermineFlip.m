e01485c85c448d0a4178cb2eb713f717a206e1ab
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-05-13 14:31:31 +0200

    Cherry-pick #575 xASL_im_DetermineFlip: Generalization & revamping

--- Functions/xASL_im_DetermineFlip.m
+++ Functions/xASL_im_DetermineFlip.m
@@ -46,6 +46,13 @@ function [LR_flip_YesNo] = xASL_im_DetermineFlip(PathOrientationResults)
     
     %% ============================================================    
     %% 2. If units are printed as second row, the data starts on the third row
+    % Determine correct row, differs between Matlab versions
+    if size(CellTSV,1)<3 || size(CellTSV,2)<26
+        warning(['Missing data in: ' PathOrientationResults]);
+        return;
+    end
+    
+    % if units are printed as second row, the data starts on the third row
     if isempty(CellTSV{2,13})
         firstRow = 3;
     else
@@ -77,5 +84,6 @@ function [LR_flip_YesNo] = xASL_im_DetermineFlip(PathOrientationResults)
     else
         LR_flip_YesNo = find((DeterminantOri.*DeterminantNew)<0);
     end
+    
     
 end
