6f0e5a39c884da4017c00d5e3a0a295d4e0334ab
MichaelStritt <stritt.michael@gmail.com>
2021-05-19 14:30:27 +0200

    #588 Integration testing (simplified workflow)

--- Testing/xASL_test_IntegrationTesting.m
+++ Testing/xASL_test_IntegrationTesting.m
@@ -32,51 +32,85 @@ function [TestResults] = xASL_test_IntegrationTesting
     addpath(genpath(xASL_dir))              % Add all scripts to path (including testing directory)
     warning('on','all')
 
-    %% Clean Up
-    clc
-    clear
-
     %% Get paths
-    if exist(fullfile(TestingPath,'testConfig.json'),'file')
-    	testConfig = spm_jsonread(fullfile(TestingPath,'testConfig.json'));
+    if exist(fullfile(filepath,'testConfig.json'),'file')
+        testConfig = spm_jsonread(fullfile(filepath,'testConfig.json'));
     else
-    	fprintf('Please add a testConfig.json to the Testing directory of ExploreASL...\n');
-    	return
+        fprintf('Please add a testConfig.json to the Testing directory of ExploreASL...\n');
+        return
     end
 
     % Check testConfig fields
     if ~isfield(testConfig, 'pathExploreASL')
-    	error('Add the pathExploreASL field to your testConfig.json...');
+        error('Add the pathExploreASL field to your testConfig.json...');
     end
     if ~isfield(testConfig, 'pathTest')
-    	error('Add the pathTest field to your testConfig.json...');
+        error('Add the pathTest field to your testConfig.json...');
     end
     if ~isfield(testConfig, 'cmdCloneFlavors')
-    	error('Add the cmdCloneFlavors field to your testConfig.json...');
+        error('Add the cmdCloneFlavors field to your testConfig.json...');
     end
 
     %% Set-up
-	cd(testConfig.pathExploreASL);
+    cd(testConfig.pathExploreASL);
 
-	% Initialize ExploreASL
-	[x] = ExploreASL_Initialize;
-	cd(pathTest);
-	
-	% Clone flavors if they do not exist already
-	if ~exist(fullfile(testConfig.pathTest,'FlavorDatabase'), 'dir')
-	    xASL_adm_CreateDir(testConfig.pathTest);
-	    system(testConfig.cmdCloneFlavors);
-	end
+    % Initialize ExploreASL
+    [x] = ExploreASL_Initialize;
+    cd(testConfig.pathTest);
+    
+    % Clean Up
+    clc
 
-	% Get the up-to-date flavor list
-	% ...
+    % Clone flavors if they do not exist already
+    if ~exist(fullfile(testConfig.pathTest,'FlavorDatabase'), 'dir')
+        xASL_adm_CreateDir(testConfig.pathTest);
+        system(testConfig.cmdCloneFlavors);
+    end
 
-	%% Iterate over flavors
-	% ...
+    % Get the up-to-date flavor list
+    flavorList = xASL_adm_GetFsList(fullfile(testConfig.pathTest,'FlavorDatabase'), '^.+$', true);
+    
+    % Delete existing results
+    if exist(fullfile(testConfig.pathTest,'FlavorDatabaseTest'), 'dir')
+        xASL_delete(fullfile(testConfig.pathTest,'FlavorDatabaseTest'), 1);
+    end
 
-	% Run individual flavor
-	% ...
+    %% Iterate over flavors
+    for iFlavor = 1:numel(flavorList)
+        % Run individual flavor
+        TestResults(iFlavor) = xASL_test_thisFlavor(testConfig.pathTest, ...
+                                                    fullfile(testConfig.pathTest,'FlavorDatabase'), ...
+                                                    flavorList{iFlavor});
+    end
 
+    % Fallback
+    if ~exist('TestResults','var')
+        TestResults = struct;
+    end
+    
+    % Go back to ExploreASL directory
+    cd(testConfig.pathExploreASL);
+
+end
+
+
+%% Run individual flavor dataset
+function xFlavor = xASL_test_thisFlavor(testingRoot, databaseRoot, flavorName)
+
+    % Copy flavor
+    fprintf('Copy %s...\n', flavorName);
+    xASL_Copy(fullfile(databaseRoot,flavorName),fullfile(testingRoot,'FlavorDatabaseTest',flavorName),1);
+    
+    % Remove rawdata folder from testing dataset
+    xASL_delete(fullfile(testingRoot,'FlavorDatabaseTest',flavorName,'rawdata'),1);
+
+    % Run test
+    [xFlavor] = ExploreASL_Master(fullfile(testingRoot,'FlavorDatabaseTest',flavorName),1,1);
+    
+    % Fallback
+    if ~exist('xFlavor','var')
+        xFlavor = struct;
+    end
     
 
 end
