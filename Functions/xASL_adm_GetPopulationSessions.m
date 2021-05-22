22b31e90af6ad56885e9766a0469ad548c2a47ec
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-05-23 00:08:56 +0200

    #586 xASL_adm_GetPopulationSessions: add SESSIONS output argument

--- Functions/xASL_adm_GetPopulationSessions.m
+++ Functions/xASL_adm_GetPopulationSessions.m
@@ -1,4 +1,4 @@
-function [nSessions, bSessionsMissing] = xASL_adm_GetPopulationSessions(x)
+function [nSessions, bSessionsMissing, SESSIONS] = xASL_adm_GetPopulationSessions(x)
 % xASL_adm_GetPopulationSessions(x) obtain number of Sessions by determining amount of input files present in the Population folder
 % FORMAT: [nSessions, bSessionsMissing] = xASL_adm_GetPopulationSessions(x)
 %
@@ -8,6 +8,7 @@ function [nSessions, bSessionsMissing] = xASL_adm_GetPopulationSessions(x)
 % OUTPUT:
 %   nSessions           - Maximum amount of sessions present in Population folder
 %   bSessionsMissing    - Boolean to show if no sessions can be found in the Population folder
+%   SESSIONS            - x.SESSIONS
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % DESCRIPTION: This function looks for the maximum amount of sessions that
 % are present in selected processed files present in the Population folder.
@@ -19,6 +20,12 @@ function [nSessions, bSessionsMissing] = xASL_adm_GetPopulationSessions(x)
 %   5. Check and provide warning of number of sesssions differs per subject
 
 %% 1. Administration
+if ~isfield(x, 'SESSIONS')
+    SESSIONS = 'ASL_1';
+else
+    SESSIONS = x.SESSIONS;
+end
+
 currentSubjectRegExp = x.subject_regexp;
 if strcmp(currentSubjectRegExp(1), '^')
     currentSubjectRegExp = currentSubjectRegExp(2:end);
@@ -31,10 +38,11 @@ SessionList = xASL_adm_GetFileList(x.D.PopDir,['^' x.S.InputDataStr '_' currentS
 
 %% 3. Obtain nSessions
 if isempty(SessionList) % If no files found, search for subject files instead of session files
+
     nSessions = 1;
     bSessionsMissing = 1;
-    fprintf('%s\n','No session files found');
-    return;
+    fprintf('%s\n','No session or subject files found');
+    return;    
 else % If files found, continue with defining sessions from SessionList
     IndexStart = cellfun(@(y) regexp(y,'ASL_\d+\.(nii|nii\.gz)'), SessionList);
     
@@ -56,6 +64,12 @@ else % If files found, continue with defining sessions from SessionList
     CompareSessions = ones(1,size(CountSessionNumbers,2)) .* nSessions; % create an array to check differences in sessions per subject with maximum amount of sessions
     if ~isequal(CountSessionNumbers,CompareSessions) % Check if amount of sessions is similar for each subject and provide warning if not
         warning('Amount of Sessions differs between Subjects');
+    end
+end
+
+if numel(SESSIONS)<nSessions
+    for iSession=1:nSessions
+        SESSIONS{iSession} = ['ASL_' num2str(iSession)];
     end
 end
 
