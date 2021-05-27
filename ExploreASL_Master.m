c8f0e17a9f83f045ccbeee35350cb3c0faf163bd
MichaelStritt <stritt.michael@gmail.com>
2021-05-27 10:44:19 +0200

    #588 Update file structure (ExploreASL_ProcessMaster)

--- ExploreASL_Master.m
+++ ExploreASL_Master.m
@@ -36,26 +36,22 @@ function [x] = ExploreASL_Master(varargin)
 % 
 % ExploreASL_Initialize    - This wrapper initializes ExploreASL: managing paths, deployment, etc.
 % ExploreASL_ImportMaster  - Multi-step import workflow for DCM2NII, NII2BIDS & BIDS2LEGACY.
-% xASL_Module_Structure    - Processes structural data, i.e. high-resolution T1w and FLAIR scans, 
-%                            type help xASL_module_Structural for more information.
-% xASL_Module_ASL          - Processes ASL data, i.e. ASL scans and M0, type help xASL_module_ASL for more information. 
-% xASL_Module_Population   - Processes data on population basis, mostly QC, but also template/parametric maps creation, 
-%                            etc. Type help xASL_module_Population for more information. 
+% ExploreASL_ProcessMaster - Multi-step processing workflow for the STRUCTURAL, ASL and POPULATION module.
 % 
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 %
 % EXAMPLES:
 %
-% [x] = ExploreASL_Master('/MyDisk/MyStudy/DataPar.json', 1, 1, 1, 1, 1);
-% [x] = ExploreASL_Master('/MyDisk/MyStudy/DataPar.json', [1 1 1 1], [1 1 1], 1, 1, 1);
-% [x] = ExploreASL_Master('/MyDisk/MyStudy/DataPar.json', '[1 1 1 1]', '[1 1 1]', '1', '1', '1');
+% [x] = ExploreASL_Master('/MyDisk/MyStudy', 1, 1, 1, 1, 1);
+% [x] = ExploreASL_Master('/MyDisk/MyStudy', [1 1 1 1], [1 1 1], 1, 1, 1);
+% [x] = ExploreASL_Master('/MyDisk/MyStudy', '[1 1 1 1]', '[1 1 1]', '1', '1', '1');
 %
 % For additional examples we recommend to check out the tutorial section: https://exploreasl.github.io/Documentation/site/Tutorials/
 % __________________________________
 % Copyright 2015-2021 ExploreASL
 
     % -----------------------------------------------------------------------------
-    %% 1 Initialization when calling this function
+    %% Initialization when calling this function
 
     % NB: *.mat files that contain statistics in data-root folder
     % (e.g.\analysis) should contain the following format:
@@ -66,11 +62,11 @@ function [x] = ExploreASL_Master(varargin)
     % 3rd column contains parameter values
     
     % -----------------------------------------------------------------------------
-    % Initialization
+    %% Initialization
     x = ExploreASL_Initialize(varargin{:});
     
     % -----------------------------------------------------------------------------
-    % Import Master
+    %% Import Master
     if x.opts.bImportData
         x = ExploreASL_ImportMaster(x);
     end
@@ -80,7 +76,7 @@ function [x] = ExploreASL_Master(varargin)
     end
     
     % -----------------------------------------------------------------------------
-    % Re-Initialize for potential data loading/processing
+    %% Re-Initialize for potential data loading/processing
     if x.opts.bReinitialize
         x = ExploreASL_Initialize(x.opts.StudyRoot, x.opts.ImportModules, x.opts.ProcessModules, x.opts.bPause, x.opts.iWorker, x.opts.nWorkers);
     end
@@ -90,7 +86,7 @@ function [x] = ExploreASL_Master(varargin)
     end
     
     % -----------------------------------------------------------------------------
-    % Print user feedback
+    %% Print user feedback
     if x.opts.bProcessData==0 || x.opts.bProcessData==2
         if x.opts.bProcessData==2 && nargout==0
             warning('Data loading requested but no output structure defined');
@@ -104,69 +100,9 @@ function [x] = ExploreASL_Master(varargin)
         pause;
     end
 
-    
-    %% -----------------------------------------------------------------------------
-    %% 1  xASL_module_Structural
-    %  1) Alignment T1w -> ACPC in MNI
-    %  2) Coregister FLAIR, T2, T1c -> T1w
-    %  3) FLAIR biasfield correction
-    %  4) Segment FLAIR
-    %  5) Lesion Filling
-    %  6) Segment T1w
-    %  7) CleanUpWMH_SEGM
-    %  8) Resample structural images -> common space
-    %  9) Get tissue volumes (incl WMH if available)
-    % 10) Visual & automatic QC
-
-    if x.opts.ProcessModules(1)==1
-        [~, x] = xASL_Iteration(x,'xASL_module_Structural');
-        % The following DARTEL module is an optional extension of the structural module
-        % to create population-specific templates
-        if isfield(x,'SegmentSPM12') && x.SegmentSPM12 && x.nSubjects>1
-            % in case we used SPM12 instead of CAT12 for segmentation,
-            % we have to run DARTEL separately
-            [~, x] = xASL_Iteration(x,'xASL_module_DARTEL');
-        end
-        % Now only check the availability of files when not running parallel
-        if x.opts.nWorkers==1; xASL_adm_CreateFileReport(x); end        
-    end
-
-    % Optional modules
-    % The following are optional extensions of the structural module and not required to run, normally they can be ignored:
-    if isfield(x, 'bRunModule_LongReg') && x.bRunModule_LongReg
-        [~, x] = xASL_Iteration(x,'xASL_module_LongReg'); % use this module for longitudinal registration
-    end
-    if isfield(x, 'bRunModule_DARTEL') && x.bRunModule_DARTEL
-        [~, x] = xASL_Iteration(x,'xASL_module_DARTEL'); % use this module for additional additional -subject registration/creating templates
-    end
-    
     % -----------------------------------------------------------------------------
-    %% 2    xASL_module_ASL  
-    %  1    TopUp geometric distortion correction
-    %  2    Motion correction
-    %  3    Registration ASL sessions to T1w
-    %  4    Reslice ASL data to high resolution standard space
-    %  5    Resolution estimation & prepare partial volume maps
-    %  6    Process M0
-    %  7    Quantification
-    %  8    Create analysis mask
-    %  9    Visual check
-    % 10    WAD-QC
-    
-    if x.opts.ProcessModules(2)==1
-        [~, x] = xASL_Iteration(x,'xASL_module_ASL');
-        % Now only check the availability of files when not running parallel
-        if x.opts.nWorkers==1; xASL_adm_CreateFileReport(x); end    
-    end
-
-
-    % -----------------------------------------------------------------------------
-    %% 3    xASL_module_Population
-    % Performs all group-level processing & QC
-
-    if x.opts.ProcessModules(3)==1
-        [~, x] = xASL_Iteration(x,'xASL_module_Population');
-    end
+    %% Processing Master
+    [x] = ExploreASL_ProcessMaster(x);
 
     % -----------------------------------------------------------------------------    
     %% Finishing touch
---
+++ Modules/Module_Master/ExploreASL_ProcessMaster.m
@@ -0,0 +1,92 @@
+function [x] = ExploreASL_ProcessMaster(x)
+%ExploreASL_ProcessMaster Multi-step processing workflow for the STRUCTURAL, ASL and POPULATION module.
+%
+% FORMAT: [x] = ExploreASL_ProcessMaster(x)
+%
+% INPUT:
+%   x             - Struct containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%
+% OUTPUT:
+%   x             - Struct containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION:    Multi-step processing workflow for the STRUCTURAL, ASL and POPULATION module.
+%
+% xASL_Module_Structure    - Processes structural data, i.e. high-resolution T1w and FLAIR scans, 
+%                            type help xASL_module_Structural for more information.
+% xASL_Module_ASL          - Processes ASL data, i.e. ASL scans and M0, type help xASL_module_ASL for more information. 
+% xASL_Module_Population   - Processes data on population basis, mostly QC, but also template/parametric maps creation, 
+%                            etc. Type help xASL_module_Population for more information. 
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE:        n/a
+% __________________________________
+% Copyright 2015-2021 ExploreASL
+
+
+    %% Processing Workflow
+
+    %% -----------------------------------------------------------------------------
+    %% 1  xASL_module_Structural
+    %  1) Alignment T1w -> ACPC in MNI
+    %  2) Coregister FLAIR, T2, T1c -> T1w
+    %  3) FLAIR biasfield correction
+    %  4) Segment FLAIR
+    %  5) Lesion Filling
+    %  6) Segment T1w
+    %  7) CleanUpWMH_SEGM
+    %  8) Resample structural images -> common space
+    %  9) Get tissue volumes (incl WMH if available)
+    % 10) Visual & automatic QC
+
+    if x.opts.ProcessModules(1)==1
+        [~, x] = xASL_Iteration(x,'xASL_module_Structural');
+        % The following DARTEL module is an optional extension of the structural module
+        % to create population-specific templates
+        if isfield(x,'SegmentSPM12') && x.SegmentSPM12 && x.nSubjects>1
+            % in case we used SPM12 instead of CAT12 for segmentation,
+            % we have to run DARTEL separately
+            [~, x] = xASL_Iteration(x,'xASL_module_DARTEL');
+        end
+        % Now only check the availability of files when not running parallel
+        if x.opts.nWorkers==1; xASL_adm_CreateFileReport(x); end        
+    end
+
+    % Optional modules
+    % The following are optional extensions of the structural module and not required to run, normally they can be ignored:
+    if isfield(x, 'bRunModule_LongReg') && x.bRunModule_LongReg
+        [~, x] = xASL_Iteration(x,'xASL_module_LongReg'); % use this module for longitudinal registration
+    end
+    if isfield(x, 'bRunModule_DARTEL') && x.bRunModule_DARTEL
+        [~, x] = xASL_Iteration(x,'xASL_module_DARTEL'); % use this module for additional additional -subject registration/creating templates
+    end
+    
+    % -----------------------------------------------------------------------------
+    %% 2    xASL_module_ASL  
+    %  1    TopUp geometric distortion correction
+    %  2    Motion correction
+    %  3    Registration ASL sessions to T1w
+    %  4    Reslice ASL data to high resolution standard space
+    %  5    Resolution estimation & prepare partial volume maps
+    %  6    Process M0
+    %  7    Quantification
+    %  8    Create analysis mask
+    %  9    Visual check
+    % 10    WAD-QC
+    
+    if x.opts.ProcessModules(2)==1
+        [~, x] = xASL_Iteration(x,'xASL_module_ASL');
+        % Now only check the availability of files when not running parallel
+        if x.opts.nWorkers==1; xASL_adm_CreateFileReport(x); end    
+    end
+
+
+    % -----------------------------------------------------------------------------
+    %% 3    xASL_module_Population
+    % Performs all group-level processing & QC
+
+    if x.opts.ProcessModules(3)==1
+        [~, x] = xASL_Iteration(x,'xASL_module_Population');
+    end
+
+end
