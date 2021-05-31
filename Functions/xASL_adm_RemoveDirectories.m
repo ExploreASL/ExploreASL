ed0a19cdc1d45dd891858ef793118ae644341871
MichaelStritt <stritt.michael@gmail.com>
2021-05-31 17:49:24 +0200

    #583 xASL_adm_RemoveDirectories: Initial commit

---
+++ Functions/xASL_adm_RemoveDirectories.m
@@ -0,0 +1,36 @@
+function xASL_adm_RemoveDirectories(root)
+%xASL_adm_RemoveDirectories Script to remove all ExploreASL related paths
+%
+% FORMAT:       xASL_adm_RemoveDirectories(root)
+% 
+% INPUT:        root - Root directory (REQUIRED, CHAR ARRAY)
+%
+% OUTPUT:       n/a
+% 
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION:  Script to remove all ExploreASL related paths.
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE:      xASL_adm_RemoveDirectories(x.MyPath);
+% __________________________________
+% Copyright 2015-2021 ExploreASL
+
+    %% Check input
+    if nargin < 1
+        error('Please provide a root directory...');
+    end
+    
+    % ExploreASL
+    try
+        warning('off', 'all');
+        rmpath(genpath(root));
+        warning('on', 'all');
+    catch ME
+        fprintf('%s\n', ME.message);
+    end
+
+end
+
+
+
+
