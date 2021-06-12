d824d79bcfa6e339998047a9bee8ff4db979b6db
HJMM Mutsaerts <henkjanmutsaerts@gmail.com>
2021-06-12 13:20:35 +0200

    #646 xASL_init_DetermineRequiredPaths: fix regular expressions

--- Functions/xASL_init_DetermineRequiredPaths.m
+++ Functions/xASL_init_DetermineRequiredPaths.m
@@ -21,22 +21,32 @@ function [x] = xASL_init_DetermineRequiredPaths(x)
 
     %% Check the BIDS dataset root for the metadata JSON files
 
+    % Regular expressions used below, explained:
+    % ^ = expression must start with this
+    % $ = expression must end with this
+    % (?i) expression becomes case insensitive
+    % \ = escaping
+    % . = any character
+    % * = previous repeated 0-Inf times
+    % .* = any character repeated 0-Inf times
+    % \. extension delimiter/period
+    
     % BIDS DatasetRoot directory
     x.dir.DatasetRoot = x.opts.DatasetRoot;
     x.opts.dataParType = 'directory';
     % Search for descriptive JSON files
-    fileListSourceStructure = xASL_adm_GetFileList(x.dir.DatasetRoot, 'sourceStructure.*.json');
-    fileListStudyPar = xASL_adm_GetFileList(x.dir.DatasetRoot, 'studyPar.*.json');
-    fileListDataDescription = xASL_adm_GetFileList(fullfile(x.dir.DatasetRoot, 'rawdata'), 'dataset_description.json');
+    fileListSourceStructure = xASL_adm_GetFileList(x.dir.DatasetRoot, '(?i)^sourcestructure.*\.json$');
+    fileListStudyPar = xASL_adm_GetFileList(x.dir.DatasetRoot, '(?i)^studypar.*\.json$');
+    fileListDataDescription = xASL_adm_GetFileList(fullfile(x.dir.DatasetRoot, 'rawdata'), '(?i)^dataset_description\.json$');
     % First try the derivatives folder
-    fileListDataPar = xASL_adm_GetFileList(fullfile(x.dir.DatasetRoot, 'derivatives', 'ExploreASL'), 'dataPar*.json');
+    fileListDataPar = xASL_adm_GetFileList(fullfile(x.dir.DatasetRoot, 'derivatives', 'ExploreASL'), '(?i)^datapar.*\.json$');
     % Check for x.D.ROOT
     if exist(fullfile(x.dir.DatasetRoot, 'derivatives', 'ExploreASL'),'dir')
         x.D.ROOT = fullfile(x.dir.DatasetRoot, 'derivatives', 'ExploreASL');
     end
     if isempty(fileListDataPar)
         % Derivatives maybe does not exist already, we'll try study root
-        fileListDataPar = xASL_adm_GetFileList(x.dir.DatasetRoot, 'dataPar.*.json');
+        fileListDataPar = xASL_adm_GetFileList(x.dir.DatasetRoot, '(?i)^datapar.*\.json$');
     end
     % Assign fields
     if ~isempty(fileListSourceStructure)
