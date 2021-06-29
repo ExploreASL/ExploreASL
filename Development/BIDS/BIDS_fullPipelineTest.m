6d6aa4eae698449bf47477da605b9b506210ec47
Petr <j.petr@hzdr.de>
2021-06-29 14:50:00 +0200

    #663 BIDS test fix macOS

--- Development/BIDS/BIDS_fullPipelineTest.m
+++ Development/BIDS/BIDS_fullPipelineTest.m
@@ -23,30 +23,36 @@ end
 
 %% Preparation for Jan
 if strcmp(username,'janpetr')
-    pathExploreASL = '/home/janpetr/ExploreASL/ExploreASL';
-    pathTest = '/pet/projekte/asl/data/BIDS';
-    cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
+	if ismac
+		pathExploreASL = '/Volumes/janpetr/ExploreASL/ExploreASL';
+		pathTest = '/Volumes/pet/projekte/asl/data/BIDS';
+	else
+		pathExploreASL = '/home/janpetr/ExploreASL/ExploreASL';
+		pathTest = '/pet/projekte/asl/data/BIDS';
+	end
+	cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
 end
+
 
 %% Preparation for Michael
 if strcmp(username,'matlab')
-    pathExploreASL = 'M:\SoftwareDevelopment\MATLAB\m.stritt\Server_xASL\ExploreASL';
-    pathTest = 'M:\SoftwareDevelopment\MATLAB\m.stritt\Server_xASL\FlavorTests';
-    cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
+	pathExploreASL = 'M:\SoftwareDevelopment\MATLAB\m.stritt\Server_xASL\ExploreASL';
+	pathTest = 'M:\SoftwareDevelopment\MATLAB\m.stritt\Server_xASL\FlavorTests';
+	cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
 end
 
 %% Preparation for Beatriz
 if strcmp(username, 'bestevespadrela')
-    pathExploreASL = '/home/bestevespadrela/ExploreASL/';
-    pathTest = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/TestBIDS';
-    cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
+	pathExploreASL = '/home/bestevespadrela/ExploreASL/';
+	pathTest = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/TestBIDS';
+	cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
 end
 
 %% Preparation for Mathijs
 if strcmp(username,'mathijs')
-    pathExploreASL = '/home/mdijsselhof/ExploreASL/';
-    pathTest = '/home/mdijsselhof/Test_BIDS';
-    cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
+	pathExploreASL = '/home/mdijsselhof/ExploreASL/';
+	pathTest = '/home/mdijsselhof/Test_BIDS';
+	cmdCloneFlavors = 'git clone git@github.com:ExploreASL/FlavorDatabase.git';
 end
 
 %% Clone the flavors database if necessary
@@ -54,8 +60,8 @@ cd(pathExploreASL);
 [x] = ExploreASL_Initialize; % Initialize ExploreASL
 cd(pathTest);
 if ~exist(fullfile(pathTest,'FlavorDatabase'), 'dir')
-    xASL_adm_CreateDir(pathTest);
-    system(cmdCloneFlavors);
+	xASL_adm_CreateDir(pathTest);
+	system(cmdCloneFlavors);
 end
 
 %% Test execution
@@ -64,6 +70,7 @@ end
 xASL_test_Flavors(pathExploreASL, pathTest, [1 0 0 0 0 0 0], x);
 
 % Convert to BIDS
+
 xASL_test_Flavors(pathExploreASL, pathTest, [0 1 0 0 0 0 0], x);
 
 % Check the BIDS conversion
@@ -87,4 +94,3 @@ xASL_test_Flavors(pathExploreASL, pathTest, [0 0 0 0 0 1 0], x);
 % TODO
 % - check legacy conversion
 % - check the pipeline results
-
