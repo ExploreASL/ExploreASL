052bc5f71ea3ac23dd3ae0a4a2177d9ba3a46d8b
MichaelStritt <stritt.michael@gmail.com>
2021-07-14 13:34:06 +0200

    #717 xASL_adm_DeleteManyTempFiles: Add try-catch because of instable behavior

--- Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
+++ Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
@@ -17,16 +17,19 @@ function xASL_adm_DeleteManyTempFiles(x)
 
 Files2Del   = {'ATT_BiasField.nii' 'Mask_Template.nii' 'Mean_CBF_Template.nii' 'PseudoCBF.nii' 'RawTemplate.nii' 'VascularArtifact_Template.nii' 'mean_PWI_Clipped.nii' 'SliceGradient_extrapolated.nii' 'FoV.nii'};
 
+try
+    for iP=1:length(Files2Del)
+        xASL_TrackProgress(iP,length(Files2Del));
+        [~, Ffile, Fext]    = xASL_fileparts(Files2Del{iP});
 
-for iP=1:length(Files2Del)
-    xASL_TrackProgress(iP,length(Files2Del));
-    [~, Ffile, Fext]    = xASL_fileparts(Files2Del{iP});
-    
-    PathList    = xASL_adm_GetFileList(x.D.ROOT,['^' File2Search Fext '$'],'FPListRec',[0 Inf]);
-    for iL=1:length(PathList)
-        xASL_delete(PathList{iL});
+        PathList    = xASL_adm_GetFileList(x.D.ROOT,['^' File2Search Fext '$'],'FPListRec',[0 Inf]);
+        for iL=1:length(PathList)
+            xASL_delete(PathList{iL});
+        end
+
     end
-
+catch ME
+    fprintf('Deleting temporary files failed because: %s', ME.message);
 end
 
 
