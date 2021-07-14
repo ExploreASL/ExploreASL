9dba511f9a7e78fd716ff2e311a1883a616a59e5
MichaelStritt <stritt.michael@gmail.com>
2021-07-14 11:44:53 +0200

    #717 Major fix for backwards compatibility
    The major problem was that xASL_adm_LoadParms just dumps all fields of the JSON into the x struct. This is not really helpful and maybe we should do this in a more controlled way in a future release. For now it should mostly be fixed. We use a standardized script called xASL_io_CheckDeprecatedFieldsX for this now. We use the same script for xASL_io_ReadDataPar and we could also integrate it in the LoadX function, we just have to use OldX.x for x there I think.

--- Functions/xASL_io_ReadDataPar.m
+++ Functions/xASL_io_ReadDataPar.m
@@ -166,58 +166,12 @@ elseif strcmpi(Fext, '.json')
 	x = xASL_io_ReadDataPar_FixFields(x);
 	
 	%% Check deprecated fields
-    x = xASL_io_ReadDataParCheckDeprecatedFields(x);
+    x = xASL_io_CheckDeprecatedFieldsX(x,true);
 	
 	
 else
 	error('Unknown file extension');
 end
-
-end
-
-
-%% Check deprecated fields
-function x = xASL_io_ReadDataParCheckDeprecatedFields(x)
-
-    % Get conversion table
-    conversionTable = xASL_adm_GetDeprecatedFields();
-    
-    nOutdatedParameter = 0;
-    bOldFieldsDetected = 0;
-    for iField = 1:size(conversionTable,1)
-        if isfield(x,conversionTable{iField,1})
-            bOldFieldsDetected = 1;
-            if ~isfield(x,conversionTable{iField,2}) || ...
-                    ~isfield(x.(conversionTable{iField,2}),conversionTable{iField,3}) || ...
-                    ~isfield(x.(conversionTable{iField,2}),conversionTable{iField,4})
-                % Deprecated field detected
-                nOutdatedParameter = nOutdatedParameter+1;
-                detectedFields{nOutdatedParameter,1} = conversionTable{iField,1};
-                % Basic field does not exist
-                if ~isfield(x,conversionTable{iField,2})
-                    x.(conversionTable{iField,2}) = struct;
-                end
-                % Actual deprecated field
-                if ~isfield(x.(conversionTable{iField,2}),conversionTable{iField,3})
-                    % Sub-Structure (x.FIELD -> x.STRUCT.FIELD)
-                    x.(conversionTable{iField,2}).(conversionTable{iField,3}) = x.(conversionTable{iField,1});
-                else
-                    % Sub-Sub-Structure (x.FIELD -> x.STRUCT.STRUCT.FIELD)
-                    x.(conversionTable{iField,2}).(conversionTable{iField,3}).(conversionTable{iField,4}) = x.(conversionTable{iField,1});
-                end
-            end
-            % Remove old field
-            x = rmfield(x,conversionTable{iField,1});
-        end
-    end
-    
-    if bOldFieldsDetected
-        % Print warning and individual fields
-        fprintf('Detected deprecated fields in dataPar.json ...\n');
-        for iField = 1:size(detectedFields,1)
-            fprintf('Deprecated field: %s\n', detectedFields{iField,1});
-        end
-    end
 
 end
 
@@ -235,6 +189,7 @@ end
 StructOut = xASL_io_ReadDataPar_ConvertCellArrays(StructOut);
 
 end
+
 
 %% ConvertNumericalFields (Convert strings which contain numbers to numbers, remove invalid fields)
 function [StructOut] = xASL_io_ReadDataPar_ConvertNumericalFields(StructIn)   
@@ -333,3 +288,5 @@ for iField=1:length(listFields)
 	end
 end
 end
+
+
