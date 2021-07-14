3631d0cca95598410cb3d3f918ca1b6cc1385b3d
Michael Stritt <stritt.michael@gmail.com>
2021-07-14 14:53:11 +0200

    #690 xASL_adm_stat_StdNan: Add multi dim tests

--- Testing/UnitTests/xASL_ut_function_xASL_stat_StdNan.m
+++ Testing/UnitTests/xASL_ut_function_xASL_stat_StdNan.m
@@ -20,7 +20,7 @@ function UnitTest = xASL_ut_function_xASL_stat_StdNan(TestRepository)
 %% Test run 1
 
 % Give your individual subtest a name
-UnitTest.tests(1).testname = 'Multiple test calls with different arrays';
+UnitTest.tests(1).testname = 'Multiple test calls with different arrays (1D)';
 
 % Start the test
 testTime = tic;
@@ -49,6 +49,52 @@ UnitTest.tests(1).duration = toc(testTime);
 
 % Evaluate your test
 UnitTest.tests(1).passed = testCondition;
+
+
+%% Test run 2
+
+% Give your individual subtest a name
+UnitTest.tests(2).testname = 'Multiple test calls with different arrays (2D, 3D, 4D)';
+
+% Start the test
+testTime = tic;
+
+% Run your test here
+A = randi([0 1],100,100,'double');
+B = randi([0 1],100,100,100,'double');
+C = randi([0 1],100,100,100,100,'double');
+stdA = std(A(:));
+stdB = std(B(:));
+stdC = std(C(:));
+nanA = NaN(100,100);
+nanB = NaN(100,100,100);
+nanC = NaN(100,100,100,100);
+concatA = [A, nanA];
+concatB = [B, nanB];
+concatC = [C, nanC];
+y1 = xASL_stat_StdNan(concatA);
+y2 = xASL_stat_StdNan(concatB);
+y3 = xASL_stat_StdNan(concatC);
+
+% Define one or multiple test conditions here
+testCondition = true;
+
+% Define one or multiple test conditions here
+if ~(y1==stdA)
+    testCondition = false;
+end
+if ~(y2==stdB)
+    testCondition = false;
+end
+if ~(y3==stdC)
+    testCondition = false;
+end
+
+% Get test duration
+UnitTest.tests(2).duration = toc(testTime);
+
+% Evaluate your test
+UnitTest.tests(2).passed = testCondition;
 
 
 %% End of testing
