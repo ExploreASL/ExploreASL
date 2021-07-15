24716fc86273ef3a92a8ae4e151ae42639781bae
MichaelStritt <stritt.michael@gmail.com>
2021-07-15 09:57:40 +0200

    #717 xASL_adm_DeleteManyTempFiles: Input check and fix undefined variable

--- Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
+++ Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
@@ -17,19 +17,21 @@ function xASL_adm_DeleteManyTempFiles(x)
 
 Files2Del   = {'ATT_BiasField.nii' 'Mask_Template.nii' 'Mean_CBF_Template.nii' 'PseudoCBF.nii' 'RawTemplate.nii' 'VascularArtifact_Template.nii' 'mean_PWI_Clipped.nii' 'SliceGradient_extrapolated.nii' 'FoV.nii'};
 
-try
+
+if isfield(x,'D') && isfield(x.D,'ROOT')
+    fprintf('Deleting temporary files:    ');
     for iP=1:length(Files2Del)
         xASL_TrackProgress(iP,length(Files2Del));
-        [~, Ffile, Fext]    = xASL_fileparts(Files2Del{iP});
-
+        [~, File2Search, Fext]    = xASL_fileparts(Files2Del{iP});
+        
         PathList    = xASL_adm_GetFileList(x.D.ROOT,['^' File2Search Fext '$'],'FPListRec',[0 Inf]);
         for iL=1:length(PathList)
             xASL_delete(PathList{iL});
         end
-
     end
-catch ME
-    fprintf('Deleting temporary files failed because: %s', ME.message);
+    fprintf('\n');
+else
+    fprintf('Missing root directory, deleting temporary files failed...\n');
 end
 
 
