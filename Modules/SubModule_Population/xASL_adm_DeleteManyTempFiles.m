1a988eaee4df6c4735279d949284cb46c7493260
MichaelStritt <stritt.michael@gmail.com>
2021-07-20 15:31:43 +0200

    #717 xASL_adm_DeleteManyTempFiles: Minor bugfix
    This commit was moved from the original #717 branch. The changes were already discussed and approved by Jan and Henk. Testing still required.

--- Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
+++ Modules/SubModule_Population/xASL_adm_DeleteManyTempFiles.m
@@ -13,21 +13,29 @@ function xASL_adm_DeleteManyTempFiles(x)
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE:      xASL_adm_DeleteManyTempFiles(x);
 % __________________________________
-% Copyright 2015-2020 ExploreASL
-
-Files2Del   = {'ATT_BiasField.nii' 'Mask_Template.nii' 'Mean_CBF_Template.nii' 'PseudoCBF.nii' 'RawTemplate.nii' 'VascularArtifact_Template.nii' 'mean_PWI_Clipped.nii' 'SliceGradient_extrapolated.nii' 'FoV.nii'};
+% Copyright (c) 2015-2021 ExploreASL
 
 
-for iP=1:length(Files2Del)
-    xASL_TrackProgress(iP,length(Files2Del));
-    [~, Ffile, Fext]    = xASL_fileparts(Files2Del{iP});
-    
-    PathList    = xASL_adm_GetFileList(x.D.ROOT,['^' File2Search Fext '$'],'FPListRec',[0 Inf]);
-    for iL=1:length(PathList)
-        xASL_delete(PathList{iL});
+    %% Delete temporary files
+    Files2Del   = {'ATT_BiasField.nii' 'Mask_Template.nii' 'Mean_CBF_Template.nii' 'PseudoCBF.nii' 'RawTemplate.nii' 'VascularArtifact_Template.nii' 'mean_PWI_Clipped.nii' 'SliceGradient_extrapolated.nii' 'FoV.nii'};
+
+    % Check if root directory is defined
+    if isfield(x,'D') && isfield(x.D,'ROOT')
+        fprintf('Deleting temporary files:    ');
+        for iP=1:length(Files2Del)
+            % Show progress
+            xASL_TrackProgress(iP,length(Files2Del));
+            [~, File2Search, Fext] = xASL_fileparts(Files2Del{iP});
+            % Path which files to delete
+            PathList = xASL_adm_GetFileList(x.D.ROOT,['^' File2Search Fext '$'],'FPListRec',[0 Inf]);
+            % Iterate over file list
+            for iL=1:length(PathList)
+                xASL_delete(PathList{iL});
+            end
+        end
+        fprintf('\n');
+    else
+        fprintf('Missing root directory, deleting temporary files failed...\n');
     end
-
-end
-
 
 end
