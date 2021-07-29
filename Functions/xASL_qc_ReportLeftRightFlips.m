63b1c52f6a1bfcf8a2bc6308c957d614c39772d1
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-07-29 13:03:25 +0200

    #575 xASL_qc_ReportLeftRightFlips: Fix header text

--- Functions/xASL_qc_ReportLeftRightFlips.m
+++ Functions/xASL_qc_ReportLeftRightFlips.m
@@ -12,12 +12,10 @@ function xASL_qc_ReportLeftRightFlips(dirRoot, bZip)
 % Prints a list on the screen with NIfTIs that contain left-right flips
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION: This submodule registers T1w linearly to the center of MNI space, a.k.a. ACPC alignment
-% The same transformation is applied to all other related scans (ASL4D, M0, FLAIR, etc.)
-% This facilitates MNI-based algorithms (e.g. SPM-based segmentation), and allows for visual QC with all images
-% roughly in the same space. This submodule first clips high values that can bias the registration algorithm, then performs
-% a center of mass-based ACPC alignment, and then several iterations of SPM coregistration.
-% Assuming that this submodule is run at the start of ExploreASL, all NIfTI orientation matrices are restored before running the registration.
+% DESCRIPTION: This function identifies and reports illegal left-right
+% flips for image matrices within a NIfTI. This can be useful as these are
+% not readily observed by the human eye, as the left and right hemispheres
+% are too symmetrical by default.
 %
 % All NifTIs are found recursively (i.e. in the folder and its subfolders),
 % irregardless of them being .nii or .nii.gz.
@@ -25,7 +23,7 @@ function xASL_qc_ReportLeftRightFlips(dirRoot, bZip)
 % EXAMPLE: xASL_qc_ReportLeftRightFlips('/Users/henk/surfdrive/HolidayPics/CICERO_Nolan/analysis');
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % __________________________________
-% Copyright 2015-2019 ExploreASL
+% Copyright 2015-2021 ExploreASL
 
 
     if nargin<1 || isempty(dirRoot)
