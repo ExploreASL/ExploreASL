ff07c4e8b1085a04a8e2208d8df12838a00061fb
MichaelStritt <stritt.michael@gmail.com>
2021-08-02 17:48:12 +0200

    #732 xASL_imp_NII2BIDS_(...): remaining required script changes for multi-session

---
+++ Modules/SubModule_Import/xASL_imp_NII2BIDS_Session.m
@@ -0,0 +1,71 @@
+function xASL_imp_NII2BIDS_Session(imPar, bidsPar, studyPar, listSessions, nameSubjectSession, bidsLabel, iSession)
+%xASL_imp_NII2BIDS_SubjectSession NII2BIDS conversion for a single sessions.
+%
+% FORMAT: xASL_imp_NII2BIDS_SubjectSession(imPar, bidsPar, studyPar, listSessions, nameSubjectSession, subjectLabel, iSession)
+% 
+% INPUT:
+%   imPar                 - JSON file with structure with import parameter (STRUCT, REQUIRED)
+%   bidsPar               - Output of xASL_imp_Config (STRUCT, REQUIRED)
+%   studyPar              - JSON file with the BIDS parameters relevant for the whole study (STRUCT, REQUIRED)
+%   listSessions          - list of sessions (CELL ARRAY, REQUIRED, e.g.: {'ASL_1'})
+%   nameSubjectSession    - name of the subject (CELL ARRAY, REQUIRED)
+%   subjectLabel          - subject label (CHAR ARRAY, REQUIRED)
+%   iSession              - Session number (INTEGER, REQUIRED)
+%
+% OUTPUT:
+%  n/a
+%                         
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: NII2BIDS conversion for a single sessions.
+% 
+% 1. Make a subject directory
+% 2. Iterate over runs
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE:     n/a
+% __________________________________
+% Copyright 2015-2021 ExploreASL
+
+    %% Unpack bidsLabel struct
+    subjectLabel = bidsLabel.subject;
+    visitLabel = bidsLabel.visit;
+
+    %% 1. Make a subject directory
+    if length(listSessions)>1 || ~isempty(visitLabel)
+        if ~isempty(visitLabel)
+            sessionLabel = ['ses-' visitLabel];
+        else
+            sessionLabel = ['ses-' listSessions{iSession}(5:end)];
+        end
+
+        if ~exist(fullfile(imPar.BidsRoot,['sub-' subjectLabel],sessionLabel),'dir')
+            xASL_adm_CreateDir(fullfile(imPar.BidsRoot,['sub-' subjectLabel],sessionLabel));
+            xASL_adm_CreateDir(fullfile(imPar.BidsRoot,['sub-' subjectLabel],sessionLabel,'perf'));
+        end
+        inSessionPath = fullfile(imPar.TempRoot,nameSubjectSession,listSessions{iSession});
+        outSessionPath = fullfile(imPar.BidsRoot,['sub-' subjectLabel],sessionLabel);
+
+        % Need to add the underscore so that it doesn't need to be added automatically and can be skipped for empty session
+        sessionLabel = ['_' sessionLabel];
+    else
+        % Session label is skipped
+        sessionLabel = '';
+
+        % Only one session - no session labeling
+        if ~exist(fullfile(imPar.BidsRoot,['sub-' subjectLabel]),'dir')
+            xASL_adm_CreateDir(fullfile(imPar.BidsRoot,['sub-' subjectLabel]));
+            xASL_adm_CreateDir(fullfile(imPar.BidsRoot,['sub-' subjectLabel],bidsPar.strPerfusion));
+        end
+        inSessionPath = fullfile(imPar.TempRoot,nameSubjectSession,listSessions{iSession});
+        outSessionPath = fullfile(imPar.BidsRoot,['sub-' subjectLabel]);
+    end
+
+    % Check if there are multiple runs per session
+    listRuns = xASL_adm_GetFileList(inSessionPath,'^ASL4D_\d.nii+$',false,[],false);
+
+    %% 2. Iterate over runs
+    for iRun = 1:(max(length(listRuns),1))
+        xASL_imp_NII2BIDS_Run(imPar, bidsPar, studyPar, [subjectLabel sessionLabel], inSessionPath, outSessionPath, listRuns, iRun, nameSubjectSession);
+    end
+
+end
--- Modules/SubModule_Import/xASL_imp_NII2BIDS_Subject.m
+++ Modules/SubModule_Import/xASL_imp_NII2BIDS_Subject.m
@@ -1,13 +1,13 @@
-function xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubject)
+function xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubjectSession)
 %xASL_imp_NII2BIDS_Subject Run NII to ASL-BIDS for one individual subject.
 %
 % FORMAT: xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubject)
 % 
 % INPUT:
-%   imPar           - JSON file with structure with import parameter (REQUIRED, STRUCT)
-%   bidsPar         - Output of xASL_imp_Config (REQUIRED, STRUCT)
-%   studyPar        - JSON file with the BIDS parameters relevant for the whole study (REQUIRED, STRUCT)
-%   nameSubject     - name of the subject (REQUIRED, LIST)
+%   imPar               - JSON file with structure with import parameter (REQUIRED, STRUCT)
+%   bidsPar             - Output of xASL_imp_Config (REQUIRED, STRUCT)
+%   studyPar            - JSON file with the BIDS parameters relevant for the whole study (REQUIRED, STRUCT)
+%   nameSubjectSession  - name of the subject (REQUIRED, LIST)
 %
 % OUTPUT:
 %   n/a
@@ -16,10 +16,10 @@ function xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubject)
 % DESCRIPTION: Run NII to ASL-BIDS for one individual subject.
 %
 % 1. Initialize
-% 2. Process all the anatomical files
-% 3. Process the perfusion files (iterate over sessions)
+% 2. Process the anat & perfusion files
 % - 1. Make a subject directory
-% - 2. Iterate over runs
+% - 2. Iterate over sessions
+% - 3. Iterate over runs
 % 
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE:     xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubject);
@@ -27,57 +27,51 @@ function xASL_imp_NII2BIDS_Subject(imPar, bidsPar, studyPar, nameSubject)
 % Copyright 2015-2021 ExploreASL
 
     %% 1. Initialize
+    bidsLabel = xASL_imp_CheckForAliasInVisit(imPar,nameSubjectSession);
     
-    subjectLabel = xASL_adm_CorrectName(nameSubject,2);
-
-	% Make a subject directory
-	if ~exist(fullfile(imPar.BidsRoot,['sub-' subjectLabel]),'dir')
-		mkdir(fullfile(imPar.BidsRoot,['sub-' subjectLabel]));
-	end
-
-    %% 2. Process all the anatomical files
-    % Go throught the list of anat files
-    for iAnatType = bidsPar.listAnatTypes
-
-        % Check if it exists
-        anatPath = '';
-        if xASL_exist(fullfile(imPar.TempRoot,nameSubject,[iAnatType{1},'.nii']),'file')
-            anatPath = fullfile(imPar.TempRoot,nameSubject,iAnatType{1});
-        end
-
-        if xASL_exist(fullfile(imPar.TempRoot,nameSubject,[iAnatType{1} '_1'],[iAnatType{1},'.nii']),'file')
-            anatPath = fullfile(imPar.TempRoot,nameSubject,[iAnatType{1} '_1'],iAnatType{1});
-        end
-
-        % If anatomical file of this type exist, then BIDSify its structure
-        if ~isempty(anatPath)
-
-            % Create the anatomical directory
-            if ~exist(fullfile(imPar.BidsRoot,['sub-' subjectLabel],'anat'),'dir')
-                mkdir(fullfile(imPar.BidsRoot,['sub-' subjectLabel],'anat'));
-            end
-
-            % Move the NiFTI file
-            xASL_Move([anatPath '.nii'],fullfile(imPar.BidsRoot,['sub-' subjectLabel],'anat',...
-                ['sub-' subjectLabel '_' iAnatType{1} '.nii.gz']),1);
-
-            % Load the JSON
-            jsonAnat = spm_jsonread([anatPath,'.json']);
-			
-            % Save the JSON
-			jsonAnat = xASL_bids_BIDSifyAnatJSON(jsonAnat,studyPar);
-            jsonAnat = xASL_bids_VendorFieldCheck(jsonAnat);
-            jsonAnat = xASL_bids_JsonCheck(jsonAnat,'');
-			
-            spm_jsonwrite(fullfile(imPar.BidsRoot,['sub-' subjectLabel],'anat',['sub-' subjectLabel '_' iAnatType{1} '.json']),jsonAnat);
-        end
+    % Make a subject directory
+    if ~exist(fullfile(imPar.BidsRoot,['sub-' bidsLabel.subject]),'dir')
+        xASL_adm_CreateDir(fullfile(imPar.BidsRoot,['sub-' bidsLabel.subject]));
     end
-
-    %% 3. Process the perfusion files
-    listSessions = xASL_adm_GetFileList(fullfile(imPar.TempRoot,nameSubject),'^ASL.+$',false,[],true);
-
+    
+    %% 2. Process the anat & perfusion files
+    listSessions = xASL_adm_GetFileList(fullfile(imPar.TempRoot,nameSubjectSession),'^ASL.+$',false,[],true);
+    
     % Go through all sessions
     for iSession = 1:length(listSessions)
-        xASL_imp_NII2BIDS_SubjectSession(imPar, bidsPar, studyPar, listSessions, nameSubject, subjectLabel, iSession);
+        xASL_imp_NII2BIDS_Session(imPar, bidsPar, studyPar, listSessions, nameSubjectSession, bidsLabel, iSession);
     end
+    
 end
+
+
+
+%% Check if there is a visit alias within the subject/visit name
+function bidsLabel = xASL_imp_CheckForAliasInVisit(imPar,nameSubjectSession)
+
+    % Get visitAliases from imPar
+    visitAliases = imPar.tokenVisitAliases(:,2);
+    
+    % Separator subject/visit
+    separator = '_';
+
+    % Default
+    subjectName = nameSubjectSession;
+    visitName = '';
+
+    % Iterate over aliases
+    for iAlias = 1:numel(visitAliases)
+        checkExpression = regexp(nameSubjectSession, [separator visitAliases{iAlias,1} '$'], 'once');
+        if ~isempty(checkExpression) % nameSubject should end in the visit alias
+            visitName = nameSubjectSession(checkExpression:end);
+            subjectName = nameSubjectSession(1:checkExpression-1);
+        end
+    end
+    
+    bidsLabel.subject = xASL_adm_CorrectName(subjectName,2);
+    bidsLabel.visit = xASL_adm_CorrectName(visitName,2);
+
+
+end
+
+
