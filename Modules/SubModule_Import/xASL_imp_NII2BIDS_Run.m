3381d7045d02a6b0587a36b6f3320cea0443089c
MichaelStritt <stritt.michael@gmail.com>
2021-08-02 17:45:44 +0200

    #732 xASL_imp_NII2BIDS_Run: rename from subjectsessionrun, simplify modality behavior

---
+++ Modules/SubModule_Import/xASL_imp_NII2BIDS_Run.m
@@ -0,0 +1,41 @@
+function xASL_imp_NII2BIDS_Run(imPar, bidsPar, studyPar, subjectSessionLabel, inSessionPath, outSessionPath, listRuns, iRun, nameSubjectSession)
+%xASL_imp_NII2BIDS_Run NII2BIDS conversion for a single sessions, single run.
+%
+% FORMAT: xASL_imp_NII2BIDS_Run(bidsPar, studyPar, subjectSessionLabel, inSessionPath, outSessionPath, listRuns, iRun)
+% 
+% INPUT:
+% imPar               - JSON file with structure with import parameter (STRUCT, REQUIRED)
+% bidsPar             - Output of xASL_imp_Config (STRUCT, REQUIRED)
+% studyPar            - JSON file with the BIDS parameters relevant for the whole study (STRUCT, REQUIRED)
+% subjectSessionLabel - subject-session label (CHAR ARRAY, REQUIRED)
+% inSessionPath       - input session path (CHAR ARRAY, PATH, REQUIRED)
+% outSessionPath      - output session path (CHAR ARRAY, PATH, REQUIRED)
+% listRuns            - list of runs (INTEGER, REQUIRED)
+% iRun                - run number (INTEGER, REQUIRED)
+% nameSubjectSession  - Name of subject & session (REQUIRED)
+%
+% OUTPUT:
+% n/a
+%                         
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: NII2BIDS conversion for a single sessions, single runs.
+%
+% 1. Convert anat files
+% 2. Convert perf files
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% EXAMPLE:     n/a
+% __________________________________
+% Copyright 2015-2021 ExploreASL
+
+
+    %% Anatomical files
+    xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel, inSessionPath, outSessionPath, listRuns, iRun, nameSubjectSession);
+
+    
+    %% Perfusion files
+    xASL_imp_NII2BIDS_RunPerf(imPar, bidsPar, studyPar, subjectSessionLabel, inSessionPath, outSessionPath, listRuns, iRun);
+
+
+end
+
