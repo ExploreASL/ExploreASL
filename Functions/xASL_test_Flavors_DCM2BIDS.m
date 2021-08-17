3f02ebe998023a561b35c4dcd1711e5ece91c5f7
Petr <j.petr@hzdr.de>
2021-08-17 18:16:08 +0200

    #178 xASL_test_Flavors_DSM2BIDS: Updated new flavors names

--- Functions/xASL_test_Flavors_DCM2BIDS.m
+++ Functions/xASL_test_Flavors_DCM2BIDS.m
@@ -19,10 +19,10 @@ function xASL_test_Flavors_DCM2BIDS(baseDirImport, x)
 % 1.  Initialization
 % 2. DICOM -> NII+JSON (i.e. dcm2niiX)
 % 3. Manual curation for certain flavors
-% 3a. Siemens_PCASL_3DGRASE_vascular
-% 3b. Philips_PCASL_3DGRASE_R5.4_TopUp
-% 3c. Siemens_PCASL_volunteer
-% 3d. Siemens_PCASL_multiTI
+% 3a. Siemens_PCASL_3DGRASE_VD13D_2
+% 3b. Philips_PCASL_3DGRASE_5.4.1.0_TopUp_1
+% 3c. Siemens_PCASL_3DGRASE_VB17A_TopUp_1
+% 3d. Siemens_PCASL_3DGRASE_VB17A_multiPLD_1
 % 4. Convert NII+JSON -> BIDS
 %
 % EXAMPLE: xASL_test_Flavors_DCM2BIDS('mydir/testImport');
@@ -48,16 +48,16 @@ for iFlavor = 1:length(flavorList)
 	%% 3. Manual curation for certain flavors
 	switch (flavorList{iFlavor})
 		
-		% 3a. 'Siemens_PCASL_3DGRASE_vascular'
-		case 'Siemens_PCASL_3DGRASE_vascular'
+		% 3a. 'Siemens_PCASL_3DGRASE_VD13D_2'
+		case 'Siemens_PCASL_3DGRASE_VD13D_2'
 			
 			xASL_adm_DeleteFileList(DirASL, '^ASL4D_(32|33|34|35)_00001.*$', 1);
 			
 			nii_files = xASL_adm_GetFileList(DirASL, '^.*\.nii$', 'FPList', [], false);
 			xASL_bids_MergeNifti(nii_files, 'ASL');
 			
-			% 3b. 'Philips_PCASL_3DGRASE_R5.4_TopUp'
-		case 'Philips_PCASL_3DGRASE_R5.4_TopUp'
+			% 3b. 'Philips_PCASL_3DGRASE_5.4.1.0_TopUp_1'
+		case 'Philips_PCASL_3DGRASE_5.4.1.0_TopUp_1'
 			DirASL = fullfile(baseDirImport, flavorList{iFlavor}, 'temp', 'Sub1', 'ASL_1');
 			
 			% xASL_Move(fullfile(DirASL, 'M0_601_00601.nii'), fullfile(DirASL, 'M0.nii'), 1);
@@ -69,8 +69,8 @@ for iFlavor = 1:length(flavorList)
 			xASL_Move(fullfile(DirASL, 'M0_701_00001.nii'), fullfile(DirASL, 'M0PERev.nii'), 1);
 			xASL_Move(fullfile(DirASL, 'M0_701_00001.json'), fullfile(DirASL, 'M0PERev.json'), 1);
 			
-			% 3c. 'Siemens_PCASL_volunteer'
-		case 'Siemens_PCASL_volunteer'
+			% 3c. 'Siemens_PCASL_3DGRASE_VB17A_TopUp_1'
+		case 'Siemens_PCASL_3DGRASE_VB17A_TopUp_1'
 			imNS = xASL_io_Nifti2Im(fullfile(DirASL, 'ASL4D_NS.nii'));
 			imSS = xASL_io_Nifti2Im(fullfile(DirASL, 'ASL4D_SS.nii'));
 			imNS(:,:,:,2) = imSS;
@@ -85,8 +85,8 @@ for iFlavor = 1:length(flavorList)
 			xASL_Move(fullfile(DirASL, 'M0_2.json'), fullfile(DirASL, 'M0PERev.json'), 1);
 			xASL_Move(fullfile(DirASL, 'M0_2.nii'), fullfile(DirASL, 'M0PERev.nii'), 1);
 			
-			% 3d. 'Siemens_PCASL_multiTI'
-		case 'Siemens_PCASL_multiTI'
+			% 3d. 'Siemens_PCASL_3DGRASE_VB17A_multiPLD_1'
+		case 'Siemens_PCASL_3DGRASE_VB17A_multiPLD_1'
 			
 			if xASL_exist(fullfile(DirASL, 'ASL4D_NS_300.nii'))
 				mTIvector = [300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000];
