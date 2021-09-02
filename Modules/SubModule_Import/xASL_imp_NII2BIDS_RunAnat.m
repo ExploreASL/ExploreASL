9170b02ff60decd2f476b0ad31bec1bccc70a1a5
Petr <j.petr@hzdr.de>
2021-09-02 19:10:24 +0200

    #810 xASL_imp_NII2BIDS_RunAnat: Clean code

--- Modules/SubModule_Import/xASL_imp_NII2BIDS_RunAnat.m
+++ Modules/SubModule_Import/xASL_imp_NII2BIDS_RunAnat.m
@@ -1,7 +1,7 @@
 function xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel, outSessionPath, listRuns, iRun, nameSubjectSession)
 %xASL_imp_NII2BIDS_RunAnat NII2BIDS conversion for a single sessions, single run.
 %
-% FORMAT: xASL_imp_NII2BIDS_RunAnat(bidsPar, studyPar, subjectSessionLabel, outSessionPath, listRuns, iRun)
+% FORMAT: xASL_imp_NII2BIDS_RunAnat(bidsPar, studyPar, subjectSessionLabel, outSessionPath, listRuns, iRun, nameSubjectSession)
 % 
 % INPUT:
 % imPar               - JSON file with structure with import parameter (STRUCT, REQUIRED)
@@ -28,14 +28,14 @@ function xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel
     %% 1. Define the pathnames
     if ~isempty(listRuns)
         anatLabel = num2str(iRun);
-        anatOutDirLabel = fullfile(outSessionPath,'anat');
-        anatOutLabel = fullfile(outSessionPath,'anat',['sub-' subjectSessionLabel '_run-' num2str(iRun)]);
-        anatOutLabelRelative = fullfile('anat',['sub-' subjectSessionLabel '_run-' num2str(iRun)]);
+        anatOutDirLabel = fullfile(outSessionPath, 'anat');
+        anatOutLabel = fullfile(outSessionPath, 'anat', ['sub-' subjectSessionLabel '_run-' num2str(iRun)]);
+        anatOutLabelRelative = fullfile('anat', ['sub-' subjectSessionLabel '_run-' num2str(iRun)]);
     else
         anatLabel = '';
-        anatOutDirLabel = fullfile(outSessionPath,'anat');
-        anatOutLabel = fullfile(outSessionPath,'anat',['sub-' subjectSessionLabel]);
-        anatOutLabelRelative = fullfile('anat',['sub-' subjectSessionLabel]);
+        anatOutDirLabel = fullfile(outSessionPath, 'anat');
+        anatOutLabel = fullfile(outSessionPath, 'anat', ['sub-' subjectSessionLabel]);
+        anatOutLabelRelative = fullfile('anat', ['sub-' subjectSessionLabel]);
     end
     
 
@@ -44,10 +44,10 @@ function xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel
 
         % Check if it exists
         anatPath = '';
-		if xASL_exist(fullfile(imPar.TempRoot,nameSubjectSession,[iAnatType{1} '_1'],[iAnatType{1},'.nii']),'file')
-			anatPath = fullfile(imPar.TempRoot,nameSubjectSession,[iAnatType{1} '_1'],iAnatType{1});
-		elseif xASL_exist(fullfile(imPar.TempRoot,nameSubjectSession,[iAnatType{1},'.nii']),'file')
-			anatPath = fullfile(imPar.TempRoot,nameSubjectSession,iAnatType{1});
+		if xASL_exist(fullfile(imPar.TempRoot, nameSubjectSession, [iAnatType{1} '_1'], [iAnatType{1} '.nii']), 'file')
+			anatPath = fullfile(imPar.TempRoot, nameSubjectSession, [iAnatType{1} '_1'], iAnatType{1});
+		elseif xASL_exist(fullfile(imPar.TempRoot, nameSubjectSession, [iAnatType{1} '.nii']), 'file')
+			anatPath = fullfile(imPar.TempRoot, nameSubjectSession, iAnatType{1});
 		end
 
         % If anatomical file of this type exist, then BIDSify its structure
@@ -57,25 +57,25 @@ function xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel
             xASL_adm_CreateDir(anatOutDirLabel);
 
             % Current scan name
-            [~, scanName] = xASL_fileparts([anatOutLabel,'_' iAnatType{1}]);
+            [~, scanName] = xASL_fileparts([anatOutLabel '_' iAnatType{1}]);
             
             % Print anat name
             fprintf('Scan %s ...\n', scanName);
             
             % Move the NiFTI file
-            anatNiiPath = [anatOutLabel,'_' iAnatType{1} '.nii.gz'];
-            xASL_Move([anatPath '.nii'],anatNiiPath,1);
+            anatNiiPath = [anatOutLabel '_' iAnatType{1} '.nii.gz'];
+            xASL_Move([anatPath '.nii'], anatNiiPath, 1);
 
             % Load the JSON
-            jsonAnat = spm_jsonread([anatPath,'.json']);
+            jsonAnat = spm_jsonread([anatPath '.json']);
 
             % Save the JSON
-            jsonAnat = xASL_bids_BIDSifyAnatJSON(jsonAnat,studyPar);
+            jsonAnat = xASL_bids_BIDSifyAnatJSON(jsonAnat, studyPar);
             jsonAnat = xASL_bids_VendorFieldCheck(jsonAnat);
-            jsonAnat = xASL_bids_JsonCheck(jsonAnat,'');
+            jsonAnat = xASL_bids_JsonCheck(jsonAnat, '');
             
-            jsonWritePath = [anatOutLabel,'_' iAnatType{1} '.json'];
-            spm_jsonwrite(jsonWritePath,jsonAnat);
+            jsonWritePath = [anatOutLabel '_' iAnatType{1} '.json'];
+            spm_jsonwrite(jsonWritePath, jsonAnat);
         end
     end
 
