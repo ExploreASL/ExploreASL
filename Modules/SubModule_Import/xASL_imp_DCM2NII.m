625a033118917b0e998340e1e9a54c0a1a9ee8bb
MichaelStritt <stritt.michael@gmail.com>
2021-09-04 11:37:49 +0200

    #805 xASL_imp_DCM2NII: cosmetics and full import logging to single file

--- Modules/SubModule_Import/xASL_imp_DCM2NII.m
+++ Modules/SubModule_Import/xASL_imp_DCM2NII.m
@@ -1,4 +1,4 @@
-function xASL_imp_DCM2NII(imPar, x)
+function xASL_imp_DCM2NII(imPar, x, newLogging)
 %xASL_imp_DCM2NII Run the dcm2nii part of the import.
 %
 % FORMAT: xASL_imp_DCM2NII(imPar, x)
@@ -6,6 +6,8 @@ function xASL_imp_DCM2NII(imPar, x)
 % INPUT:
 %   imPar              - JSON file with structure with import parameters (REQUIRED, STRUCT)
 %   x                  - ExploreASL x structure (REQUIRED, STRUCT)
+%   newLogging         - Option to create new xASL_module_Import log (BOOLEAN, OPTION, DEFAULT = true)
+%                        (using the false option you will get the well known dcm2niix log)
 %
 % OUTPUT:
 %   n/a
@@ -26,6 +28,10 @@ function xASL_imp_DCM2NII(imPar, x)
 % __________________________________
 % Copyright 2015-2021 ExploreASL
 
+    %% 0. Input check
+    if nargin<3 || isempty(newLogging)
+        newLogging = true;
+    end
     
     %% 1. Initialize defaults of dcm2nii
     fprintf('================================== DICOM to NIFTI CONVERSION =================================\n');
@@ -38,7 +44,8 @@ function xASL_imp_DCM2NII(imPar, x)
         imPar.dcm2nii_version = '20190902'; % OR for PARREC imPar.dcm2nii_version = '20101105'; THIS IS AUTOMATED BELOW
     end
     if ~isfield(imPar,'dcmExtFilter') || isempty(imPar.dcmExtFilter)
-        imPar.dcmExtFilter = '^(.*\.dcm|.*\.img|.*\.IMA|[^.]+|.*\.\d*)$'; % the last one is because some convertors save files without extension, but there would be a dot/period before a bunch of numbers
+        % dcmExtFilter: the last one is because some convertors save files without extension, but there would be a dot/period before a bunch of numbers
+        imPar.dcmExtFilter = '^(.*\.dcm|.*\.img|.*\.IMA|[^.]+|.*\.\d*)$';
     end
 	
 	if ~isfield(imPar,'SkipSubjectIfExists') || isempty(imPar.SkipSubjectIfExists)
@@ -51,21 +58,21 @@ function xASL_imp_DCM2NII(imPar, x)
 	end
 	
 	%% 2. Create the basic folder structure for sourcedata & derivative data
-	if ~exist(imPar.RawRoot, 'dir')
-		warning(['Couldnt find ' imPar.RawRoot ', trying to find a different folder instead...']);
-		
-		% find any folder except for temp, source, raw, derivatives
-		% xASL_adm_GetFileList uses regular expressions, to create a nice list of foldernames,
-		% with/without FullPath (FPList), with/without recursive (FPListRec)
-		% very powerful once you know how these work
-		FolderNames = xASL_adm_GetFileList(fullfile(imPar.RawRoot, imPar.studyID), '^(?!(temp|derivatives|source|sourcedata)).*$', 'FPList', [0 Inf], true);
-		
-		if length(FolderNames)==1
-			imPar.RawRoot = FolderNames{1};
-			fprintf('%s\n', ['Found ' imPar.RawRoot ' as sourcedata folder']);
-		else
-			error('Couldnt find a sourcedata folder, please rename one, or move other folders');
-		end
+    if ~exist(imPar.RawRoot, 'dir')
+        warning(['Couldnt find ' imPar.RawRoot ', trying to find a different folder instead...']);
+        
+        % find any folder except for temp, source, raw, derivatives
+        % xASL_adm_GetFileList uses regular expressions, to create a nice list of foldernames,
+        % with/without FullPath (FPList), with/without recursive (FPListRec)
+        % very powerful once you know how these work
+        FolderNames = xASL_adm_GetFileList(fullfile(imPar.RawRoot, imPar.studyID), '^(?!(temp|derivatives|source|sourcedata)).*$', 'FPList', [0 Inf], true);
+        
+        if length(FolderNames)==1
+            imPar.RawRoot = FolderNames{1};
+            fprintf('%s\n', ['Found ' imPar.RawRoot ' as sourcedata folder']);
+        else
+            error('Couldnt find a sourcedata folder, please rename one, or move other folders');
+        end
     end
 	
     % Create temp directory
@@ -93,15 +100,14 @@ function xASL_imp_DCM2NII(imPar, x)
 	
 	% Path to the dictionary to initialize - we need to keep track if the dictionary has been set, because
 	% Dicominfo can be used despite bUSEDCMTK==1 when DCMTK fails
-	x.modules.import.pathDcmDict = fullfile(x.opts.MyPath,'External','xASL_DICOMLibrary.txt');
-	if ~x.modules.import.settings.bUseDCMTK
-		% -----------------------------------------------------------------------------
-		% Initialize dicom dictionary by appending private philips stuff to a temporary copy
-		% -----------------------------------------------------------------------------
-		dicomdict('set', x.modules.import.pathDcmDict);
-	end
+    x.modules.import.pathDcmDict = fullfile(x.opts.MyPath,'External','xASL_DICOMLibrary.txt');
+    if ~x.modules.import.settings.bUseDCMTK
+        % Initialize dicom dictionary by appending private philips stuff to a temporary copy
+        dicomdict('set', x.modules.import.pathDcmDict);
+    end
 	
-	fid_summary = -1; % initialize to be able to catch errors and close if valid
+    % Initialize to be able to catch errors and close if valid
+	fid_summary = -1;
 	
 	% change dcmnii_version for PARREC if needed
 	if ~isempty(strfind(char(imPar.folderHierarchy(end)),'PAR'))
@@ -109,9 +115,14 @@ function xASL_imp_DCM2NII(imPar, x)
 	end
 	
 	%% 4. Redirect output to a log file
-	diary_filepath = fullfile(imPar.TempRoot, ['import_log_' imPar.studyID '_' datestr(now,'yyyymmdd_HHMMSS') '.txt']);
+    if newLogging
+        diary_filepath = fullfile(x.opts.DatasetRoot,'xASL_module_Import.log');
+    else
+        diary_filepath = fullfile(imPar.TempRoot, ['import_log_' imPar.studyID '_' datestr(now,'yyyymmdd_HHMMSS') '.txt']);
+    end
 	diary(diary_filepath);
 	
+    % Check if directories are files are supposed to be matched
 	if imPar.bMatchDirectories
 		strLookFor = 'Directories';
 	else
@@ -125,65 +136,73 @@ function xASL_imp_DCM2NII(imPar, x)
 	% that will be used to identify subjects, sessions and scans. In the loop below it is possible to translate the tokens
 	% to more convenient strings before using them in destination paths.
 	[matches, tokens] = xASL_adm_FindByRegExp(imPar.RawRoot, imPar.folderHierarchy, 'StripRoot', true, 'Match', strLookFor,'IgnoreCase',true);
+    
+    % Print matching files
 	if isempty(matches)
 		warning('No matching files, skipping');
 		return;
 	elseif imPar.bVerbose
-		fprintf('\nMatching files:\n');
-		disp(matches);
-		fprintf('#=%g\n',length(matches));
+		fprintf('\nMatching files (#=%g):\n',length(matches));
+        for iMatch=1:size(matches,1)
+            fprintf('%s\n', matches{iMatch,1});
+        end
 	end
 	
 	% Copy the columns into named vectors. This construction allows for arbitrary directory hierarchies.
 	% Make sure to select the columns that correspond to the folder ordering defined using the regular expressions above.
-	% Define Subjects
 	
-	% SUBJECTS
-	x.modules.import.listsIDs.vSubjectIDs = tokens(:,imPar.tokenOrdering(1)); % cell vector with extracted subject IDs (for all visits, sessions and scans; 
-    % so subjects may be repeated here
+    % Define Subjects (so subjects may be repeated here)
+    % vSubjectIDs: cell vector with extracted subject IDs (for all visits, sessions and scans; 
+	x.modules.import.listsIDs.vSubjectIDs = tokens(:,imPar.tokenOrdering(1));
 	
 	%% VISITS
 	if imPar.tokenOrdering(2)==0
 		% a zero means: no visits applicable
 		x.modules.import.settings.bUseVisits = false;
-		x.modules.import.listsIDs.vVisitIDs = cellfun(@(y) '1', x.modules.import.listsIDs.vSubjectIDs, 'UniformOutput', false); % each subject has a single visit
+        % vVisitIDs: each subject has a single visit
+		x.modules.import.listsIDs.vVisitIDs = cellfun(@(y) '1', x.modules.import.listsIDs.vSubjectIDs, 'UniformOutput', false);
 		imPar.tokenVisitAliases = {'^1$', '_1'};
 	else
 		x.modules.import.settings.bUseVisits = true;
-		x.modules.import.listsIDs.vVisitIDs = tokens(:,imPar.tokenOrdering(2)); % cell vector with extracted session IDs (for all subjects, sessions and scans)
+        % vVisitIDs: cell vector with extracted session IDs (for all subjects, sessions and scans)
+		x.modules.import.listsIDs.vVisitIDs = tokens(:,imPar.tokenOrdering(2));
 	end
 	
 	%% SESSIONS
 	if imPar.tokenOrdering(3)==0
 		% a zero means: no sessions applicable
 		x.modules.import.settings.bUseSessions = false;
-		x.modules.import.listsIDs.vSessionIDs = cellfun(@(y) '1', x.modules.import.listsIDs.vSubjectIDs, 'UniformOutput', false); % each subject-visit has a single session
+        % vSessionIDs: each subject-visit has a single session
+		x.modules.import.listsIDs.vSessionIDs = cellfun(@(y) '1', x.modules.import.listsIDs.vSubjectIDs, 'UniformOutput', false);
 		imPar.tokenSessionAliases = {'^1$', 'ASL_1'};
 	else
 		x.modules.import.settings.bUseSessions = true;
-		x.modules.import.listsIDs.vSessionIDs = tokens(:,imPar.tokenOrdering(3)); % cell vector with extracted session IDs (for all subjects and scans)
+        % vSessionIDs: Cell vector with extracted session IDs (for all subjects and scans)
+		x.modules.import.listsIDs.vSessionIDs = tokens(:,imPar.tokenOrdering(3));
 	end
 	
 	%% SCANTYPES
-	x.modules.import.listsIDs.vScanIDs = tokens(:,imPar.tokenOrdering(4)); % cell vector with extracted scan IDs (for all subjects, visits and sessions)
+    
+    % vScanIDs: cell vector with extracted scan IDs (for all subjects, visits and sessions)
+	x.modules.import.listsIDs.vScanIDs = tokens(:,imPar.tokenOrdering(4)); 
 	
 	% Convert the vectors to unique & sort sets by the output aliases
 	x.modules.import.listsIDs.subjectIDs  = sort(unique(x.modules.import.listsIDs.vSubjectIDs));
 	x.modules.import.numOf.nSubjects = length(x.modules.import.listsIDs.subjectIDs);
-	
 	x.modules.import.listsIDs.visitIDs  = unique(x.modules.import.listsIDs.vVisitIDs);
-	% sort by output
-	if length(x.modules.import.listsIDs.visitIDs)>1
-		for iV=1:length(x.modules.import.listsIDs.visitIDs)
-			IDrow(iV) = find(cellfun(@(y) strcmp(y,x.modules.import.listsIDs.visitIDs{iV}), imPar.tokenVisitAliases(:,1)));
-		end
-		x.modules.import.listsIDs.visitIDs = x.modules.import.listsIDs.visitIDs(IDrow);
-	end
+    
+	% Sort by output
+    if length(x.modules.import.listsIDs.visitIDs)>1
+        for iV=1:length(x.modules.import.listsIDs.visitIDs)
+            IDrow(iV) = find(cellfun(@(y) strcmp(y,x.modules.import.listsIDs.visitIDs{iV}), imPar.tokenVisitAliases(:,1)));
+        end
+        x.modules.import.listsIDs.visitIDs = x.modules.import.listsIDs.visitIDs(IDrow);
+    end
+    
+    % Get number of visits, session IDs, number of sessions, scan IDs, number of scans
 	x.modules.import.numOf.nVisits = length(x.modules.import.listsIDs.visitIDs);
-	
 	x.modules.import.listsIDs.sessionIDs  = sort(unique(x.modules.import.listsIDs.vSessionIDs));
 	x.modules.import.numOf.nSessions = length(x.modules.import.listsIDs.sessionIDs);
-	
 	x.modules.import.listsIDs.scanIDs = sort(unique(lower(x.modules.import.listsIDs.vScanIDs)));
 	x.modules.import.numOf.nScans = length(x.modules.import.listsIDs.scanIDs);
 	
@@ -192,8 +211,8 @@ function xASL_imp_DCM2NII(imPar, x)
 		if isempty(x.modules.import.listsIDs.visitIDs)
 			imPar.visitNames = cell(x.modules.import.numOf.nVisits,1);
 			for kk=1:x.modules.import.numOf.nVisits
-				imPar.visitNames{kk}=sprintf('ASL_%g',kk);
-                %% JAN PETR WHAT HAPPENS HERE, INCORRECT NAME ASL_
+				imPar.visitNames{kk} = sprintf('ASL_%g', kk);
+                % JAN PETR WHAT HAPPENS HERE, INCORRECT NAME ASL
 			end
 		else
 			imPar.visitNames = x.modules.import.listsIDs.visitIDs;
@@ -230,30 +249,26 @@ function xASL_imp_DCM2NII(imPar, x)
 		error('No scans')
 	end
 	
-	% preallocate space for (global) counts
-	x.modules.import.globalCounts.converted_scans = zeros(x.modules.import.numOf.nSubjects,...
-                                                          x.modules.import.numOf.nVisits,...
-                                                          x.modules.import.numOf.nSessions,...
-                                                          x.modules.import.numOf.nScans,'uint8'); % keep a count of all individual scans
-	x.modules.import.globalCounts.skipped_scans = zeros(x.modules.import.numOf.nSubjects,...
-                                                        x.modules.import.numOf.nVisits,...
-                                                        x.modules.import.numOf.nSessions,...
-                                                        x.modules.import.numOf.nScans,'uint8'); % keep a count of all individual scans
-	x.modules.import.globalCounts.missing_scans = zeros(x.modules.import.numOf.nSubjects,...
-                                                        x.modules.import.numOf.nVisits,...
-                                                        x.modules.import.numOf.nSessions,...
-                                                        x.modules.import.numOf.nScans,'uint8'); % keep a count of all individual scans
+    % Preallocate space for (global) counts
+    
+    % keep a count of all individual scans
+    x.modules.import.globalCounts.converted_scans = ...
+        zeros(x.modules.import.numOf.nSubjects, x.modules.import.numOf.nVisits, x.modules.import.numOf.nSessions, x.modules.import.numOf.nScans,'uint8');
+    % keep a count of all individual scans
+    x.modules.import.globalCounts.skipped_scans = ...
+        zeros(x.modules.import.numOf.nSubjects, x.modules.import.numOf.nVisits, x.modules.import.numOf.nSessions, x.modules.import.numOf.nScans,'uint8');
+    % keep a count of all individual scans
+    x.modules.import.globalCounts.missing_scans = ...
+        zeros(x.modules.import.numOf.nSubjects, x.modules.import.numOf.nVisits, x.modules.import.numOf.nSessions, x.modules.import.numOf.nScans,'uint8');
 	
 	% define a cell array for storing info for parameter summary file
 	xASL_adm_CreateDir(imPar.TempRoot);
-	x.modules.import.summary_lines = cell(x.modules.import.numOf.nSubjects,...
-                                          x.modules.import.numOf.nVisits,...
-                                          x.modules.import.numOf.nSessions,...
-                                          x.modules.import.numOf.nScans);
+	x.modules.import.summary_lines = ...
+        cell(x.modules.import.numOf.nSubjects, x.modules.import.numOf.nVisits, x.modules.import.numOf.nSessions, x.modules.import.numOf.nScans);
 	
     
 	%% 7. Import subject by subject, visit by visit, session by session, scan by scan
-	fprintf('%s\n', 'Running import (i.e. dcm2niiX)');
+	fprintf('\nRunning DCM2NIIX...\n');
     
     % Iterate over subjects
     for iSubject=1:x.modules.import.numOf.nSubjects
