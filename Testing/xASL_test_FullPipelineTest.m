f279d7fc350a32bb831bbd97e5ce0b12147c7f53
MichaelStritt <stritt.michael@gmail.com>
2021-09-14 13:16:17 +0200

    #834 xASL_test_FullPipelineTest: update logging behavior

--- Testing/xASL_test_FullPipelineTest.m
+++ Testing/xASL_test_FullPipelineTest.m
@@ -1,12 +1,13 @@
-function [testConfig, logContent] = xASL_test_FullPipelineTest
+function [flavors, testConfig, logContent] = xASL_test_FullPipelineTest
 %xASL_test_FullPipelineTest BIDS testing script
 %
-% FORMAT: [testConfig, logContent] = xASL_test_FullPipelineTest
+% FORMAT: [flavors, testConfig, logContent] = xASL_test_FullPipelineTest
 % 
 % INPUT:
 %   n/a
 %
 % OUTPUT:
+%   flavors        - Struct containing the loggingTable and other fields
 %   testConfig     - Struct containing all relevant testing fields from the corresponding JSON file
 %   logContent     - Struct containing the logged warnings and errors
 %                         
@@ -25,7 +26,7 @@ function [testConfig, logContent] = xASL_test_FullPipelineTest
 % }
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% EXAMPLE: [testConfig, logContent] = xASL_test_FullPipelineTest;
+% EXAMPLE: [flavors, testConfig, logContent] = xASL_test_FullPipelineTest;
 %
 % __________________________________
 % Copyright 2015-2021 ExploreASL
@@ -55,32 +56,40 @@ function [testConfig, logContent] = xASL_test_FullPipelineTest
         system(testConfig.cmdCloneFlavors);
     end
     
+    
+    %% Logging table
+    flavors.loggingTable = array2table(zeros(0,3), 'VariableNames',{'message','stack','name'});
+    
 
     %% Test execution
 
     % Remove exiting test data
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [1 0 0 0 0 0 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [1 0 0 0 0 0 0], x, flavors);
 
     % Convert to BIDS
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 1 0 0 0 0 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 1 0 0 0 0 0], x, flavors);
 
     % Check the BIDS conversion
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 1 0 0 0 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 1 0 0 0 0], x, flavors);
 
     % Convert BIDS to Legacy
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 1 0 0 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 1 0 0 0], x, flavors);
 
     % Check the Legacy conversion
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 1 0 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 1 0 0], x, flavors);
 
     % Run the pipeline
-    xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 0 1 0], x);
+    flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 0 1 0], x, flavors);
 
     % Check the pipeline results
-    %xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 0 0 1], x);
+    % flavors = xASL_test_Flavors(testConfig.pathExploreASL, testConfig.pathFlavorDatabase, [0 0 0 0 0 0 1], x, flavors);
 
     % Get warnings & errors from log files
     [logContent] = xASL_test_GetLogContent(testConfig.pathFlavorDatabase,0,1,2);
+    
+    % Clean-up (file handles etc.)
+    fclose('all');
+    diary off;
 
 
 end
