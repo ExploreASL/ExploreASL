9d3cf42f2e74e9683f5e6b5a99fcfd64695cb5a4
MichaelStritt <m.stritt@mediri.com>
2021-09-27 17:38:47 +0200

    #855 xASL_spm_reslice: unit test

--- Testing/UnitTests/xASL_ut_function_xASL_spm_reslice.m
+++ Testing/UnitTests/xASL_ut_function_xASL_spm_reslice.m
@@ -20,7 +20,7 @@ function UnitTest = xASL_ut_function_xASL_spm_reslice(TestRepository)
 %% Test run 1
 
 % Give your individual subtest a name
-UnitTest.tests(1).testname = 'Reslice a T1w image ...';
+UnitTest.tests(1).testname = 'Reslice a T1w image to an identical copy';
 
 % Start the test
 testTime = tic;
@@ -28,19 +28,22 @@ testTime = tic;
 % Set-up test NIfTI
 testDirsAndFiles.testSource = fullfile(TestRepository,'UnitTesting','dro_files','test_patient_2_3_0_pre_release','sub-001','anat');
 testDirsAndFiles.testDestination = fullfile(TestRepository,'UnitTesting','working_directory','testSubject');
-testDirsAndFiles.testFile = fullfile(testDirsAndFiles.testDestination,'test.nii.gz');
-testDirsAndFiles.testFileResult1 = fullfile(testDirsAndFiles.testDestination,'result1.nii.gz');
+testDirsAndFiles.testFileSource = fullfile(testDirsAndFiles.testDestination,'testSource.nii.gz');
+testDirsAndFiles.testFileReference = fullfile(testDirsAndFiles.testDestination,'testReference.nii.gz');
+testDirsAndFiles.testFileResult = fullfile(testDirsAndFiles.testDestination,'rtestSource.nii.gz');
 
 % Set up directory and copy NIfTI
 xASL_adm_CreateDir(testDirsAndFiles.testDestination);
-xASL_Copy(fullfile(testDirsAndFiles.testSource,'sub-001_acq-003_T1w.nii.gz'),testDirsAndFiles.testFile,1);
+xASL_Copy(fullfile(testDirsAndFiles.testSource,'sub-001_acq-003_T1w.nii.gz'),testDirsAndFiles.testFileSource,1);
+xASL_Copy(fullfile(testDirsAndFiles.testSource,'sub-001_acq-003_T1w.nii.gz'),testDirsAndFiles.testFileReference,1);
 
 % Fallback
 testCondition = true;
 
 % Test: Reslice
 try
-    % xASL_spm_reslice(testDirsAndFiles.testFile,...);
+    % Proof of concept test with identical images
+    xASL_spm_reslice(testDirsAndFiles.testFileReference,testDirsAndFiles.testFileSource);
 catch ME
     warning(ME.identifier, '%s', ME.message);
     testCondition = false;
@@ -49,21 +52,32 @@ end
 
 % Test: Load data
 try
-    im1 = xASL_io_Nifti2Im(testDirsAndFiles.testFileResult1);
+    imSource = xASL_io_Nifti2Im(testDirsAndFiles.testFileSource);
+    imResult = xASL_io_Nifti2Im(testDirsAndFiles.testFileResult);
 catch ME
-    im1 = [];
+    imSource = [];
+    imResult = [];
     warning(ME.identifier, '%s', ME.message);
     testCondition = false;
     diary off;
 end
 
 % Define one or multiple test conditions here
-
-
-
+threshold = 0.1;
+if isempty(imSource) || isempty(imResult)
+    testCondition = false;
+else
+    if isequal(size(imSource),size(imResult))
+        if xASL_ut_GetRMSE(imSource, imResult) > threshold
+            testCondition = false;
+        end
+    else
+        testCondition = false;
+    end
+end
 
 % Delete test data
-xASL_delete(testDirsAndFiles.testDestination),true)
+xASL_delete(testDirsAndFiles.testDestination,true);
 
 % Clean-up
 clearvars -except UnitTest TestRepository testCondition testTime 
@@ -73,6 +87,88 @@ UnitTest.tests(1).duration = toc(testTime);
 
 % Evaluate your test
 UnitTest.tests(1).passed = testCondition;
+
+
+%% Test run 2
+
+% Give your individual subtest a name
+UnitTest.tests(2).testname = 'Reslice a T1w image to a rotated copy';
+
+% Start the test
+testTime = tic;
+
+% Set-up test NIfTI
+testDirsAndFiles.testSource = fullfile(TestRepository,'UnitTesting','dro_files','test_patient_2_3_0_pre_release','sub-001','anat');
+testDirsAndFiles.testDestination = fullfile(TestRepository,'UnitTesting','working_directory','testSubject');
+testDirsAndFiles.testFileSource = fullfile(testDirsAndFiles.testDestination,'testSource.nii.gz');
+testDirsAndFiles.testFileReference = fullfile(testDirsAndFiles.testDestination,'testReference.nii.gz');
+testDirsAndFiles.testFileRotated = fullfile(testDirsAndFiles.testDestination,'rotatedtestSource.nii.gz');
+testDirsAndFiles.testFileResult = fullfile(testDirsAndFiles.testDestination,'rrotatedtestSource.nii.gz');
+
+% Set up directory and copy NIfTI
+xASL_adm_CreateDir(testDirsAndFiles.testDestination);
+xASL_Copy(fullfile(testDirsAndFiles.testSource,'sub-001_acq-003_T1w.nii.gz'),testDirsAndFiles.testFileSource,1);
+xASL_Copy(fullfile(testDirsAndFiles.testSource,'sub-001_acq-003_T1w.nii.gz'),testDirsAndFiles.testFileReference,1);
+
+% Flip the source NIfTI
+sourceNifti = xASL_io_Nifti2Im(testDirsAndFiles.testFileSource);
+rotatedNifti = xASL_im_rotate(sourceNifti, 180);
+xASL_io_SaveNifti(testDirsAndFiles.testFileSource, testDirsAndFiles.testFileRotated, rotatedNifti);
+
+
+% Fallback
+testCondition = true;
+
+% Test: Reslice
+try
+    % Proof of concept test with identical images
+    xASL_spm_reslice(testDirsAndFiles.testFileReference,testDirsAndFiles.testFileRotated);
+catch ME
+    warning(ME.identifier, '%s', ME.message);
+    testCondition = false;
+    diary off;
+end
+
+% Test: Load data
+try
+    imSource = xASL_io_Nifti2Im(testDirsAndFiles.testFileReference); % here we use the reference
+    imResult = xASL_io_Nifti2Im(testDirsAndFiles.testFileResult);
+catch ME
+    imSource = [];
+    imResult = [];
+    warning(ME.identifier, '%s', ME.message);
+    testCondition = false;
+    diary off;
+end
+
+% Define one or multiple test conditions here
+threshold = 1.5; % Here we have to be a bit more forgiving
+if isempty(imSource) || isempty(imResult)
+    testCondition = false;
+else
+    if isequal(size(imSource),size(imResult))
+        if xASL_ut_GetRMSE(imSource, imResult) > threshold
+            testCondition = false;
+        end
+    else
+        testCondition = false;
+    end
+end
+
+% Checking out the NIfTIs this does not seem to work that well ...
+
+% Delete test data
+xASL_delete(testDirsAndFiles.testDestination,true);
+
+% Clean-up
+clearvars -except UnitTest TestRepository testCondition testTime 
+
+% Get test duration
+UnitTest.tests(2).duration = toc(testTime);
+
+% Evaluate your test
+UnitTest.tests(2).passed = testCondition;
+
 
 
 %% End of testing
