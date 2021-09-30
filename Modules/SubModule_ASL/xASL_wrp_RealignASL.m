84e645ad444351f266d5d4a00c5f2c04e5412cb6
Beatriz Padrela <beatrizpadrela@hotmail.com>
2021-09-30 10:30:46 +0200

    #858 Replacing outdated variable HadamardType per TimeEncodedMatrixType

--- Modules/SubModule_ASL/xASL_wrp_RealignASL.m
+++ Modules/SubModule_ASL/xASL_wrp_RealignASL.m
@@ -122,7 +122,7 @@ xASL_delete(rpfile);
 %         save('rp_ASL4D.txt','MotionAllTEs','-ascii')
 %     end  
     
-if nFrames>2 && bSubtraction && (numel(unique(x.Q.Initial_PLD))>1 || numel(unique(x.EchoTime))>1 || x.modules.asl.HadamardType~=0) %multiPLD or multiTE or Hadamard
+if nFrames>2 && bSubtraction && (numel(unique(x.Q.Initial_PLD))>1 || numel(unique(x.EchoTime))>1 || isfield(x.modules.asl,'TimeEncodedMatrixType') %multiPLD or multiTE or Hadamard
     spm_realign(spm_vol(InputPath),flags,false);
     
 elseif nFrames>2 && bSubtraction
