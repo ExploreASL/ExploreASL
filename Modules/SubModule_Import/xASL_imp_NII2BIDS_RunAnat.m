a3577cb63b43e8b88c4dff8c7cb1bcb2524b5040
Petr <j.petr@hzdr.de>
2021-10-27 19:59:17 +0200

    #799 NII2BIDS: cosmetics

--- Modules/SubModule_Import/xASL_imp_NII2BIDS_RunAnat.m
+++ Modules/SubModule_Import/xASL_imp_NII2BIDS_RunAnat.m
@@ -26,7 +26,7 @@ function xASL_imp_NII2BIDS_RunAnat(imPar, bidsPar, studyPar, subjectSessionLabel
     
 
     %% 1. Define the pathnames
-    if ~isempty(listRuns)
+    if length(listRuns)>1
         anatLabel = num2str(iRun);
         anatOutDirLabel = fullfile(outSessionPath, 'anat');
         anatOutLabel = fullfile(outSessionPath, 'anat', ['sub-' subjectSessionLabel '_run-' num2str(iRun)]);
