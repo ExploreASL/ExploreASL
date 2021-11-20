8652272704e0fa0a9733847dae27205902608fd6
MichaelStritt <stritt.michael@gmail.com>
2021-11-20 13:01:38 +0100

    #930 xASL_Iteration: fix asl log

--- Functions/xASL_Iteration.m
+++ Functions/xASL_Iteration.m
@@ -110,7 +110,7 @@ function [bAborted, xOut] = xASL_Iteration(x, moduleName, dryRun, stopAfterError
     if ~isempty(regexp(ModName, '(DARTEL|Population|Analyze)', 'once'))
         dbSettings.diaryFile = ['<ROOT>/log/' moduleName '.log'];
     elseif ~isempty(regexp(ModName,'(ASL|func|dwi)', 'once'))
-        dbSettings.diaryFile = ['<SESSIONDIR>/' moduleName '.log'];
+        dbSettings.diaryFile = ['<ROOT>/log/' moduleName '_<SUBJECT>_<SESSION>.log'];
     elseif ~isempty(regexp(ModName,'(Import)', 'once'))
         dbSettings.diaryFile = ['<ROOT>/derivatives/ExploreASL/log/' moduleName '_<SUBJECT>.log'];
     else
