beaec407e2193c3b5c8a030ef59618e637a5a658
MichaelStritt <stritt.michael@gmail.com>
2021-11-20 13:02:54 +0100

    #930 xASL_Iteration: simplify code

--- Functions/xASL_Iteration.m
+++ Functions/xASL_Iteration.m
@@ -109,8 +109,6 @@ function [bAborted, xOut] = xASL_Iteration(x, moduleName, dryRun, stopAfterError
     % Diary file
     if ~isempty(regexp(ModName, '(DARTEL|Population|Analyze)', 'once'))
         dbSettings.diaryFile = ['<ROOT>/log/' moduleName '.log'];
-    elseif ~isempty(regexp(ModName,'(ASL|func|dwi)', 'once'))
-        dbSettings.diaryFile = ['<ROOT>/log/' moduleName '_<SUBJECT>_<SESSION>.log'];
     elseif ~isempty(regexp(ModName,'(Import)', 'once'))
         dbSettings.diaryFile = ['<ROOT>/derivatives/ExploreASL/log/' moduleName '_<SUBJECT>.log'];
     else
