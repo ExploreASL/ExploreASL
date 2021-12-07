ee8853694d3954a2dcc592e8ef74273f85a59d8a
Michael Stritt <stritt.michael@gmail.com>
2021-12-07 09:03:38 +0100

    #969 xASL_init_FileSystem: read MNI Structural automatically

--- Functions/xASL_init_FileSystem.m
+++ Functions/xASL_init_FileSystem.m
@@ -182,7 +182,6 @@ x.P.Atlas.TotalGM                           = fullfile(x.D.MapsSPMmodifiedDir, '
 x.P.Atlas.TotalWM                           = fullfile(x.D.MapsSPMmodifiedDir, 'TotalWM.nii');
 x.P.Atlas.DeepWM                            = fullfile(x.D.MapsSPMmodifiedDir, 'DeepWM.nii');
 x.P.Atlas.WholeBrain                        = fullfile(x.D.MapsSPMmodifiedDir, 'WholeBrain.nii');
-x.P.Atlas.MNI_Structural                    = fullfile(x.D.MapsSPMmodifiedDir, 'MNI_Structural.nii');
 x.P.Atlas.Tatu_ACA_MCA_PCA                  = fullfile(x.D.MapsSPMmodifiedDir, 'VascularTerritories', 'CortVascTerritoriesTatu.nii.gz');
 x.P.Atlas.Tatu_ICA_PCA                      = fullfile(x.D.MapsSPMmodifiedDir, 'VascularTerritories', 'TatuICA_PCA.nii.gz');
 x.P.Atlas.Tatu_ICA_L_ICA_R_PCA              = fullfile(x.D.MapsSPMmodifiedDir, 'VascularTerritories', 'LabelingTerritories.nii.gz');
