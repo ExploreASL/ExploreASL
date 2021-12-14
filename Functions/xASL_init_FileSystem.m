7f4799a17e398c28aaeee6fbbaf2c305c359a0bd
Michael Stritt <stritt.michael@gmail.com>
2021-12-14 16:30:46 +0100

    #978 xASL_init_FileSystem: legacy context name

--- Functions/xASL_init_FileSystem.m
+++ Functions/xASL_init_FileSystem.m
@@ -160,7 +160,7 @@ if isfield(x.P,'SubjectID')
     x.P.Pop_Path_MaskVascular = fullfile(x.D.PopDir, ['MaskVascular_' x.P.SubjectID '_' x.P.SessionID '.nii']);
     x.P.Path_Pop_MaskSusceptibility = fullfile(x.D.PopDir, ['rMaskSusceptibility_' x.P.SubjectID '_' x.P.SessionID '.nii']);    
 	
-	x.P.Path_ASL4D_aslcontext = fullfile(Path{2},'ASL4D_aslcontext.tsv');
+	x.P.Path_ASL4D_aslcontext = fullfile(Path{2},'ASL4Dcontext.tsv');
 end
 
 
