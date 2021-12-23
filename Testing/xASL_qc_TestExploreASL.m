c4c6fa06c0836ac4f498e6d3d18d996fef971fe2
HJMM Mutsaerts <henkjanmutsaerts@gmail.com>
2021-12-23 07:23:52 +0100

    #1023 xASL_qc_TestExploreASL: update log output folder

--- Testing/xASL_qc_TestExploreASL.m
+++ Testing/xASL_qc_TestExploreASL.m
@@ -309,7 +309,7 @@ end
 
 % Get list of data to test
 Dlist = xASL_adm_GetFileList(TestDirDest,'^.*$','List',[0 Inf], true);
-LogFiles = cellfun(@(y) fullfile(TestDirDest,y,'Population','xASL_module_Population.log'), Dlist, 'UniformOutput',false);
+LogFiles = cellfun(@(y) fullfile(TestDirDest,y,'log','xASL_module_Population.log'), Dlist, 'UniformOutput',false);
 
 for iList=1:length(Dlist)
     AnalysisDir = fullfile(TestDirDest,Dlist{iList});
@@ -367,8 +367,7 @@ end
 
 % ============================================================
 %% 6) Pause until all results exist (if running parallel in background)
-if RunMethod==2
-    LogsDontExist = max(cellfun(@(y) ~exist(y,'file'), LogFiles));
+if RunMethod==2 || RunMethod==4
 
     CountTime = 0;
     TimeStepSeconds = 30;
