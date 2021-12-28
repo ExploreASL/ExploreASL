a52abb073fc4e9408e458758fbc7c7b070323962
HJMM Mutsaerts <henkjanmutsaerts@gmail.com>
2021-12-28 09:56:08 +0100

    #1033 xASL_im_M0ErodeSmoothExtrapolate: add comments

--- Functions/xASL_im_M0ErodeSmoothExtrapolate.m
+++ Functions/xASL_im_M0ErodeSmoothExtrapolate.m
@@ -22,20 +22,7 @@ function [ImOut, VisualQC] = xASL_im_M0ErodeSmoothExtrapolate(ImIn, DirOutput, N
 %               It assumes that the M0 image is in standard space & that the GM & WM probability maps
 %               are aligned. Here, we mask the M0, to remove high CSF signal and low extracranial signal,
 %               enabling us to smooth the image without inserting wrong signal. See also
-%               the ExploreASL manuscript for a more extensive explanation. This function
-%               performs the following steps:
-%
-%               1. Mask: Load segmentations, create structural mask
-%               2. Mask: Create intensity-based mask to remove extracranial signal
-%                   This uses the LowThreshold input parameter, which
-%                   should be sufficiently high to exclude brain edges or susceptibility artifacts
-%                   but also sufficiently low not to exclude too much in case of biasfields 
-%               3. Mask: Erode the combined masks
-%               4. Mask: Determine any odd borders
-%               5. Smoothing
-%               6. Extrapolating only
-%               7. Scale back to the GM M0
-%               8. Print visual QC figure
+%               the ExploreASL manuscript for a more extensive explanation. 
 %
 %               A visual QC figure is created, showing the M0 image processing steps for a single transversal slice (slice 53 in `1.5 mm` MNI standard space)
 %               `OutputFile = fullfile(DirOutput,['M0_im_proc_' NameOutput '.jpg']);`
@@ -45,6 +32,20 @@ function [ImOut, VisualQC] = xASL_im_M0ErodeSmoothExtrapolate(ImIn, DirOutput, N
 %               This masked M0 image is smoothed with a 16x16x16 mm full- width-half-maximum Gaussian filter (Mutsaerts et al., 2018) (e)
 %               after which the signal at the border is smoothly extrapolated until the full image is filled (f).
 %               Whereas the masking avoids mixing with cerebrospinal fluid or extracranial signal, the extrapolation avoids M0 division artifacts
+%
+%               This function performs the following steps:
+%
+%               1.  Mask 1: structural mask
+%               2.  Mask 2: intensity-based mask to remove extracranial signal
+%               2.a Multiplication with brain mask centrality map
+%               2.b Mask the "dummy image" from 2.a based on intensities
+%               2.c Enforce to include the eroded brain in the mask
+%               3.  Mask 3: Erode the combined masks
+%               4.  Mask 4: Determine any odd borders
+%               5.  Smoothing
+%               6.  Extrapolating only
+%               7.  Scale back to the GM M0
+%               8.  Print visual QC figure
 %
 % EXAMPLE as used in ExploreASL (xASL_wrp_ProcessM0): [ImOut] = xASL_im_M0ErodeSmoothExtrapolate(x.P.Pop_Path_M0, x.D.M0regASLdir, x.P.SubjectID, x.P.Pop_Path_rc1T1, x.P.Pop_Path_rc2T1, path_BrainCentralityMap)
 % __________________________________
@@ -79,6 +80,7 @@ ImIn = xASL_io_Nifti2Im(ImIn);
 
 %% ------------------------------------------------------------------------------------------
 %% 1. Mask 1: structural mask
+% Here, we load the partial volume maps for the first mask
 GMim = xASL_io_Nifti2Im(pvGM);
 WMim = xASL_io_Nifti2Im(pvWM);
 
@@ -95,42 +97,59 @@ end
 
 %% ------------------------------------------------------------------------------------------
 %% 2. Mask 2: intensity-based mask to remove extracranial signal
-% Multiply the image with the brain mask centrality map
+% 2.a Multiplication with brain mask centrality map
+% We multiply the M0 image with the brain mask centrality map, to
+% decrease the values outside the brain in intensity, so the
+% intensity-based masking will be more biased towards the outside of the
+% brain. This helps in cases with low intensities inside the brain e.g.,
+% due to a biasfield. We call the resulting map here a "dummy image"
 dummyImage = ImIn.*xASL_io_Nifti2Im(brainCentralityMap);
 
 % Get NaNs
 SortInt = sort(dummyImage(:));
 SortInt = SortInt(~isnan(SortInt));
-
-% Create masks
 if isempty(SortInt)
     error('M0 image only contains NaN values, cannot process M0');
-else
-    fprintf('%s', 'intensity masking, ');
-    % Lower threshold
-    ThresholdN = SortInt(round(LowThreshold*length(SortInt)));
-    
-	% Remove peak signal as well
-	ThresholdN2 = SortInt(round(0.999*length(SortInt)));
-
-	% Combine these masks
-    Mask2 = (dummyImage>ThresholdN) & (dummyImage<ThresholdN2);
-    Mask2 = Mask1 & Mask2;
-    
-    % Enforce to include the eroded brain in the mask
-    Mask1Eroded = xASL_im_DilateErodeFull(Mask1, 'erode', xASL_im_DilateErodeSphere(4));
-	Mask2(Mask1Eroded) = 1;
 end
+
+% 2.b Mask the "dummy image" from 2.a based on intensities
+%   *   The high intensity masking removes residual high CSF values (for
+%       T2-weighted M0)
+%   *   The low intensity masking does an extra round of brainmasking,
+%       which is useful with e.g., susceptibility artifacts,
+%       in which cases the ASL brainmask should be smaller than the tissue
+%       brainmask created in 1.
+fprintf('%s', 'intensity masking, ');
+% Lower threshold
+ThresholdN = SortInt(round(LowThreshold*length(SortInt)));
+
+% Remove peak signal as well
+ThresholdN2 = SortInt(round(0.999*length(SortInt)));
+
+% Combine these masks
+Mask2 = (dummyImage>ThresholdN) & (dummyImage<ThresholdN2);
+Mask2 = Mask1 & Mask2;
+
+% 2.c Enforce to include the eroded brain in the mask
+% Sometimes, the low intensity masking in 2.b can still remove brain
+% regions. Here, we force that a strong eroded tissue brainmask is still
+% restored (we don't expect susceptibility artifacts in the brainmask)
+Mask1Eroded = xASL_im_DilateErodeFull(Mask1, 'erode', xASL_im_DilateErodeSphere(4));
+Mask2(Mask1Eroded) = 1;
 
 
 %% ------------------------------------------------------------------------------------------
 %% 3. Mask 3: Erode the combined masks
+% We erode the resulting mask, as misalignment of ASL with the tissue
+% masks could still result in suboptimal masking. To improve the masking here, we erode with a 2-voxel sphere. 
 fprintf('%s', 'eroding, ');
 Mask3 = xASL_im_DilateErodeFull(Mask2, 'erode', xASL_im_DilateErodeSphere(2));
 
 
 %% ------------------------------------------------------------------------------------------
 %% 4. Mask 4: Determine any odd borders
+% We remove sharp edges, that still have local intensity outliers
+
 % First get median inside the mask
 ValuesM0mask = ImIn(Mask3 & isfinite(ImIn));
 MedianN = median(ValuesM0mask);
@@ -140,7 +159,7 @@ TempIM(~Mask3) = MedianN;
 
 % Smooth ImOut, to check the voxels that change most by smoothing
 % (especially those with low values)
-ImOutSmooth = xASL_im_ndnanfilter(TempIM,'gauss',double([5 5 5]),0);
+ImOutSmooth = xASL_im_ndnanfilter(TempIM, 'gauss', double([5 5 5]), 0);
 DiffIM = abs(ImOutSmooth-TempIM);
 
 DiffIMvalues = DiffIM(Mask3 & isfinite(DiffIM));
@@ -148,7 +167,7 @@ DiffIMvalues = DiffIM(Mask3 & isfinite(DiffIM));
 MedianN = median(DiffIMvalues);
 MadN = xASL_stat_MadNan(DiffIMvalues);
 HiThresh = MedianN+4.5*MadN;
-% here we remove too many parts, but this is not bad, as it will be smoothed & extrapolated, anyway.
+% Here, we may remove too much, but this will be smoothed & extrapolated, anyway.
 Mask4 = Mask3 & ~(DiffIM>HiThresh);
 
 
@@ -180,13 +199,18 @@ ImOut = xASL_im_ndnanfilter(ImOut,'gauss',double([16 16 16]./VoxelSize),0);
 
 %% ------------------------------------------------------------------------------------------
 %% 6. Extrapolating only
-% Here we fill the residual NaNs (outside the mask) of the FoV
+% We fill the residual NaNs (outside the mask) of the FoV
 % to prevent ASL/M0 division artifacts
 ImOut = xASL_im_FillNaNs(ImOut, 1, 1, VoxelSize);
 
 
 %% ------------------------------------------------------------------------------------------
 %% 7. Scale back to the GM M0
+% Our relatively strict masking has mostly excluded the GM M0 and
+% substituted this with WM M0 values as these are more robust against
+% misalignment. To improve quantitative accuracy, we scale the M0 values
+% back to the median of GM M0.
+% Note that this is optimal for GM ASL but may be suboptimal for WM ASL
 OldGMM0 = ImIn(GMmask & isfinite(ImIn));
 OldGMM0 = median(OldGMM0);
 
