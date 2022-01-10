841732c4ca53e8ebbb8d447e3c8a8f56f48286eb
Michael Stritt <stritt.michael@gmail.com>
2022-01-10 10:22:17 +0100

    #1044 Review: update header

--- ExploreASL.m
+++ ExploreASL.m
@@ -4,59 +4,83 @@ function [x] = ExploreASL(varargin)
 % FORMAT: [x] = ExploreASL([DatasetRoot, Import, Deface, Process, bPause, iWorker, nWorkers])
 % 
 % INPUT:
-%   DatasetRoot    - Path to the BIDS dataset root directory (OPTIONAL, DEFAULT = EMPTY)
 %
-%   Import         - [DCM2NII, NII2BIDS] (OPTIONAL, BOOLEAN ARRAY)
-%                  - DCM2NII = Run the DICOM to NIFTI conversion (BOOLEAN, DEFAULT = 0)
-%                  - NII2BIDS = Run the NIFTI to BIDS conversion (BOOLEAN, DEFAULT = 0)
-%   Deface         - [DEFACE] (OPTIONAL, BOOLEAN SCALAR)
-%                  - DEFACE = Run the defacing (BOOLEAN, DEFAULT = 0)
-%   Process        - [BIDS2LEGACY, STRUCTURAL, ASL, POPULATION] (OPTIONAL, BOOLEAN ARRAY) 
-%                  - BIDS2LEGACY = Run the BIDS to LEGACY conversion (BOOLEAN, DEFAULT = 0)
-%                  - STRUCTURAL = Run the Structural Module (BOOLEAN, DEFAULT = 0)
-%                  - ASL = Run the ASL Module (BOOLEAN, DEFAULT = 0)
-%                  - POPULATION = Run the Population Module (BOOLEAN, DEFAULT = 0)
-%   bPause         - [bPause] (OPTIONAL, BOOLEAN SCALAR)
-%                  - TRUE = Pause workflow before ExploreASL pipeline (OPTIONAL, DEFAULT = FALSE)
-%   iWorker        - Allows parallelization when called externally. 
-%                    iWorker defines which of the parallel ExploreASL calls we are (OPTIONAL, DEFAULT=1)
-%   nWorkers       - Allows parallelization when called externally. 
-%                    nWorkers defines how many ExploreASL calls are made in parallel (OPTIONAL, DEFAULT=1)
+%   Definitions:
+%
+%   DatasetRoot - [DatasetRoot]                              (OPTIONAL, STRING, DEFAULT = [])
+%   Import      - [DCM2NII, NII2BIDS]                        (OPTIONAL, BOOLEAN, DEFAULT = 0)
+%   Deface      - [DEFACE]                                   (OPTIONAL, BOOLEAN, DEFAULT = 0)
+%   Process     - [BIDS2LEGACY, STRUCTURAL, ASL, POPULATION] (OPTIONAL, BOOLEAN, DEFAULT = 0)
+%   bPause      - [bPause]                                   (OPTIONAL, BOOLEAN, DEFAULT = 0)
+%   iWorker     - [iWorker]                                  (OPTIONAL, INTEGER, DEFAULT = 1)
+%   nWorkers    - [nWorkers]                                 (OPTIONAL, INTEGER, DEFAULT = 1)
+%
+%   Explanations:
+%
+%   DatasetRoot - Path to the BIDS dataset root directory (this should be the directory containing sourcedata/rawdata/derivatives)
+%   Import      - Run the DICOM to BIDS import workflow (scalar or vector input for the import modules)
+%   Deface      - Run the defacing (simple true/false boolean to deface the anatomical data)
+%   Process     - Run the ExploreASL processing pipeline (scalar or vector input for the processing modules)
+%   bPause      - Pause workflow before ExploreASL pipeline (simple true/false boolean to pause before processing)
+%   iWorker     - iWorker defines in which of the parallel ExploreASL calls we are (allows parallelization when called externally)
+%   nWorkers    - nWorkers defines how many ExploreASL calls are made in parallel (allows parallelization when called externally)              
 %
 % OUTPUT:
-%   x              - Struct containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%
+%   x              - Struct containing ExploreASL parameters
 %                         
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION:               This master script runs the following six modular workflows:
+% DESCRIPTION:               This master script runs the following five modular workflows:
 % 
-% I. Initialization:         ExploreASL_Initialize
-%                            This wrapper initializes ExploreASL: managing paths, deployment, etc.
-% II. Import:                ExploreASL_Import
-%                            Multi-step import workflow for DCM2NII, NII2BIDS & BIDS2LEGACY.
-% III. Deface:               ExploreASL_Deface
-%                            Iterate over subjects and deface the anatomical files.
-% IV. Process:               ExploreASL_Process
-%                            Multi-step processing workflow for the STRUCTURAL, ASL and POPULATION module.
-% V. Print user feedback:    xASL_init_PrintUserFeedback
-%                            This wrapper prints a final feedback after the processing pipeline
-%
-% This pipeline can be run from CLI or using the python GUI.
+% I.   Initialization:       ExploreASL_Initialize:       This wrapper initializes ExploreASL.
+% II.  Import:               ExploreASL_Import:           Multi-step import workflow for DCM2NII and NII2BIDS.
+% III. Deface:               ExploreASL_Deface:           Iterate over subjects and deface the anatomical files.
+% IV.  Process:              ExploreASL_Process:          Multi-step processing workflow for the Structural, ASL and Population module.
+% V.   Print user feedback:  xASL_init_PrintUserFeedback: This wrapper prints a final feedback after the processing pipeline
 % 
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 %
 % EXAMPLES: There are different options to run ExploreASL. The most common and easiest way is to use the scalar input shown below.
 %
-% Scalar input:
-% [x] = ExploreASL('/MyDisk/MyStudy', 1, 1, 1, 1, 1, 1);
+% Scalar input: Here we run all Import, the Deface and all Process modules. We pause before processing and define iWorker and nWorkers.
 %
-% Vector input:
-% [x] = ExploreASL('/MyDisk/MyStudy', [1 1], 1, [1 1 1 1], 1, 1, 1);
+%      DatasetRoot = '/MyDisk/MyStudy';
+%      Import = 1;
+%      Deface = 1;
+%      Process = 1;
+%      bPause = 1;
+%      iWorker = 1;
+%      nWorkers = 1;
 %
-% Character input:
-% [x] = ExploreASL('/MyDisk/MyStudy', '[1 1]', '1', '[1 1 1 1]', '1', '1', '1');
+% Vector input: Alternatively we can also choose to run only specific sub-modules of the import or processing workflow using the vector notation.
 %
-% For additional examples we recommend to check out the tutorial section: 
-% https://exploreasl.github.io/Documentation/latest/Tutorials-ASL-BIDS/
+%      DatasetRoot = '/MyDisk/MyStudy';
+%      Import = [1 1];
+%      Deface = 1;
+%      Process = [1 1 1 1];
+%      bPause = 1;
+%      iWorker = 1;
+%      nWorkers = 1;
+%
+% Character input: To run ExploreASL in compiled mode from a console it can be useful to have the possibility to use the character input.
+%
+%      DatasetRoot = '/MyDisk/MyStudy';
+%      Import = '[1 1]';
+%      Deface = '1';
+%      Process = '[1 1 1 1]';
+%      bPause = '1';
+%      iWorker = '1';
+%      nWorkers = '1';
+%
+% Command: You can use one of the input options shown above with the matlab command shown below.
+%
+%      [x] = ExploreASL(DatasetRoot, Import, Deface, Process, bPause, iWorker, nWorkers);
+%
+% For additional examples we recommend to check out the ASL-BIDS or the Basics tutorial sections:
+%
+% - https://exploreasl.github.io/Documentation/latest/Tutorials-ASL-BIDS/
+% - https://exploreasl.github.io/Documentation/latest/Tutorials-Basics/
+%
 % __________________________________
 % Copyright (c) 2015-2022 ExploreASL
 
