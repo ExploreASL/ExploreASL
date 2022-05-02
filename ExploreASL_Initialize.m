c2104ae698911cf551d6d342b7948939f1c8249c
jan-petr <j.petr@hzdr.de>
2022-05-02 10:59:20 +0200

    #1105 Fix addOptional in ExploreASL_Initialize

--- ExploreASL_Initialize.m
+++ ExploreASL_Initialize.m
@@ -51,16 +51,13 @@ function [x] = ExploreASL_Initialize(varargin)
 
     %% 1. Admin
 
-    % Define input parser
-    p = xASL_init_InputParsing(varargin{:});
-    
 	if ~exist('x','var') || isempty(x)
 		x = struct;
 	end
         
-    % Convert parsed input
-    x = xASL_init_convertParsedInput(x, p.Results);
-    
+    % Parse input parameters
+    x = xASL_init_InputParsing(x,varargin{:});
+	    
     % Check if the ExploreASL pipeline should be run or not
     x = xASL_init_GetBooleansImportProcess(x);
     
@@ -233,125 +230,135 @@ end
 
 %% -----------------------------------------------------------------------
 %% Define input parser
-function p = xASL_init_InputParsing(varargin)
+function x = xASL_init_InputParsing(x, varargin)
 
     % This subfunction receives the input arguments and parses them.
-    % The allowed input arguments are: DatasetRoot, bImport, 
-    % bProcess, bPause, iWorker, nWorkers. Right now we allow the user
-    % to insert a path to a directory or an empty variable for the
-    % DatasetRoot argument. All other arguments are either character
-    % arrays, empty variables, numerical values or logical values. bImport,
-    % and bProcess can also be logical arrays. This sub-function is
-    % closely related to xASL_init_convertParsedInput, so please check out
-    % the corresponding description.
+    % The allowed input arguments are: 
+	%     DatasetRoot - a path to a directory or an empty variable
+	%     bImport     - a boolean in the form of a char or numerical or logical array
+	%     bProcess    - a boolean in the form of a char or numerical or logical array
+	%     bPause      - a boolean in the form of a char or numerical or logical value
+	%     iWorker     - a boolean in the form of a char or numerical value
+	%     nWorkers    - a boolean in the form of a char or numerical value
+	% The outputs are written into the x.opts structure
 
-    % Initialize input parser
-    p = inputParser;
-    
-    % Define valid input variables
-    validDatasetRoot = @(variable) ischar(variable) || isempty(variable);
-    validImport = @(variable) ischar(variable) || isempty(variable) || isnumeric(variable) || islogical(variable);
-    validProcess = @(variable) ischar(variable) || isempty(variable) || isnumeric(variable) || islogical(variable);
-    validbPause = @(variable) ischar(variable) || isempty(variable) || isnumeric(variable) || islogical(variable);
-    validiWorker = @(variable) ischar(variable) || isempty(variable) || isnumeric(variable);
-    validnWorkers = @(variable) ischar(variable) || isempty(variable) || isnumeric(variable);
-    
-    % Define defaults
-    defaultDatasetRoot = [];
-    defaultImport = [0 0 0];
-    defaultProcess = [0 0 0];
-    defaultbPause = 0;
-    defaultiWorker = 1;
-    defaultnWorkers = 1;
-    
-    % Add definitions to the input parser
-    addOptional(p, 'DatasetRoot', defaultDatasetRoot, validDatasetRoot);
-    addOptional(p, 'bImport', defaultImport, validImport);
-    addOptional(p, 'bProcess', defaultProcess, validProcess);
-    addOptional(p, 'bPause', defaultbPause, validbPause);
-    addOptional(p, 'iWorker', defaultiWorker, validiWorker);
-    addOptional(p, 'nWorkers', defaultnWorkers, validnWorkers);
-    
-    % Parse input
-    parse(p,varargin{:});
+	% DatasetRoot
+	if length(varargin)<1 || isempty(varargin{1})
+		x.opts.DatasetRoot = '';
+	elseif ~ischar(varargin{1})
+		error('Parameter DatasetRoot does not have a valid format');
+	else
+		x.opts.DatasetRoot = varargin{1};
+	end
 
-end
-
-%% -----------------------------------------------------------------------
-%% Convert parsed input
-function x = xASL_init_convertParsedInput(x,parameters)
-
-    % This sub-function takes the parsed arguments and convertes them for
-    % our internal usage. Additionally we do some pipeline related checks.
-    % ExploreASL has the following input arguments: DatasetRoot, bImport, 
-    % bProcess, bPause, iWorker, nWorkers. Besides DatasetRoot,
-    % which is a path, and also iWorker & nWorkers, which are integers, all
-    % the other input options are meant to be used as booleans. This
-    % simplifies the usage for newcomers. They only have to decide whether
-    % they want to run the import, the defacing or the processing.
-    % To be able to run each import or processing sub-module individually,
-    % we allow the use of arrays, too. If the default case happens and a
-    % user inserts bImport or bProcess as booleans though, then we simply
-    % convert them to the arrays which are called bImport and bProcess.
-
-    % Check if inputs are empty or chars (the option to use character array input is important for the compiled mode)
-    if isempty(parameters.DatasetRoot),     parameters.DatasetRoot = '';                                    end
-    if ischar(parameters.bImport),          parameters.bImport = str2num(parameters.bImport);               end
-    if ischar(parameters.bProcess),         parameters.bProcess = str2num(parameters.bProcess);             end
-    if ischar(parameters.bPause),           parameters.bPause = str2num(parameters.bPause);                 end
-    if ischar(parameters.iWorker),          parameters.iWorker = str2num(parameters.iWorker);               end
-    if ischar(parameters.nWorkers),         parameters.nWorkers = str2num(parameters.nWorkers);             end
+	% bImport
+	if length(varargin)<2 || isempty(varargin{2})
+		x.opts.bImport = [0 0 0];
+	elseif ~( ischar(varargin{2}) || isnumeric(varargin{2}) || islogical(varargin{2}) )
+		error('Parameter bImport does not have a valid format');
+	else
+		x.opts.bImport = varargin{2};
+		if ischar(x.opts.bImport)
+			x.opts.bImport = str2num(x.opts.bImport);
+		end
+	end
+	
+	% bProcess
+	if length(varargin)<3 || isempty(varargin{3})
+		x.opts.bProcess = [0 0 0];
+	elseif ~( ischar(varargin{3}) || isnumeric(varargin{3}) || islogical(varargin{3}) )
+		error('Parameter bProcess does not have a valid format');
+	else
+		x.opts.bProcess = varargin{3};
+		if ischar(x.opts.bProcess)
+			x.opts.bProcess = str2num(x.opts.bProcess);
+		end
+	end
+	
+	% bPause
+	if length(varargin)<4 || isempty(varargin{4})
+		x.opts.bPause = 0;
+	elseif ~( ischar(varargin{4}) || isnumeric(varargin{4}) || islogical(varargin{4}) )
+		error('Parameter bPause does not have a valid format');
+	else
+		x.opts.bPause = varargin{4};
+		if ischar(x.opts.bPause)
+			x.opts.bPause = str2num(x.opts.bPause);
+		end
+	end
+	
+	% iWorker
+	if length(varargin)<5 || isempty(varargin{5})
+		x.opts.iWorker = 1;
+	elseif ~( ischar(varargin{5}) || isnumeric(varargin{5}) )
+		error('Parameter iWorker does not have a valid format');
+	else
+		x.opts.iWorker = varargin{5};
+		if ischar(x.opts.iWorker)
+			x.opts.iWorker = str2num(x.opts.iWorker);
+		end
+	end
+	
+	% nWorkers
+	if length(varargin)<6 || isempty(varargin{6})
+		x.opts.nWorkers = 1;
+	elseif ~( ischar(varargin{6}) || isnumeric(varargin{6}) )
+		error('Parameter nWorkers does not have a valid format');
+	else
+		x.opts.nWorkers = varargin{6};
+		if ischar(x.opts.nWorkers)
+			x.opts.nWorkers = str2num(x.opts.nWorkers);
+		end
+	end
     
     % Check length of arrays (single digit input)
-    if length(parameters.bImport)==1
-        % If a single value is given then turn on/off all the import modules ...
-        parameters.bImport(1:2) = logical(parameters.bImport(1));
-		parameters.bImport(3) = false;
-    elseif length(parameters.bImport)==4
+    if length(x.opts.bImport)==1
+        % If a single value is given then turn on/off all the import modules, but not defacing
+        x.opts.bImport(1:2) = logical(x.opts.bImport(1));
+		x.opts.bImport(3) = false;
+    elseif length(x.opts.bImport)==4
         % Old version
         warning('You are using the outdated format with 4 import modules, additional elements are skipped...');
-        parameters.bImport = parameters.bImport(1:3);
-    elseif length(parameters.bImport)<2
+        x.opts.bImport = logical(x.opts.bImport(1:3));
+    elseif length(x.opts.bImport)<3
         % Convert to a row vector
-        parameters.bImport = parameters.bImport(:)';
+        x.opts.bImport = logical(x.opts.bImport(:)');
         % Issue a warning
-        warning('Incorrect number of import modules (%s), missing sub-modules set to zero...', xASL_num2str(length(parameters.bImport)));
+        warning('Incorrect number of import modules (%s), missing sub-modules set to false...', xASL_num2str(length(x.opts.bImport)));
         % Fill in the missing fields with zeros
-        parameters.bImport(length(parameters.bImport)+1:3) = 0;
-    elseif length(parameters.bImport)>3
+        x.opts.bImport(length(x.opts.bImport)+1:3) = false;
+    elseif length(x.opts.bImport)>3
         % Skip additional elements
-        warning('Incorrect number of import modules (%s), additional elements are skipped...', xASL_num2str(length(parameters.bImport)));
-        parameters.bImport = parameters.bImport(1:3);
-    end
-    if length(parameters.bProcess)==1
+        warning('Incorrect number of import modules (%s), additional elements are skipped...', xASL_num2str(length(x.opts.bImport)));
+        x.opts.bImport = logical(x.opts.bImport(1:3));
+	end
+	
+    if length(x.opts.bProcess)==1
         % If a single value is given, then copy it to all submodules
-        parameters.bProcess(1:3) = parameters.bProcess(1);
-    elseif length(parameters.bProcess)<3
+        x.opts.bProcess(1:3) = logical(x.opts.bProcess(1));
+    elseif length(x.opts.bProcess)<3
         % Convert to a row vector
-        parameters.bProcess = parameters.bProcess(:)';
+        x.opts.bProcess = logical(x.opts.bProcess(:)');
         % Issue a warning
-        warning('Incorrect number of processing modules (%s), missing sub-modules set to zero...', xASL_num2str(length(parameters.bProcess)));
-        % Fill in the missing fields with zeros
-        parameters.bProcess(length(parameters.bProcess)+1:3) = 0;
-    elseif length(parameters.bProcess)>3
+        warning('Incorrect number of processing modules (%s), missing sub-modules set to zero...', xASL_num2str(length(x.opts.bProcess)));
+        % Fill in the missing fields with false
+        x.opts.bProcess(length(x.opts.bProcess)+1:3) = false;
+    elseif length(x.opts.bProcess)>3
         % Skip additional elements
-        warning('Incorrect number of processing modules (%s), additional elements are skipped...', xASL_num2str(length(parameters.bProcess)));
-        parameters.bProcess = parameters.bProcess(1:3);
+        warning('Incorrect number of processing modules (%s), additional elements are skipped...', xASL_num2str(length(x.opts.bProcess)));
+        x.opts.bProcess = logical(x.opts.bProcess(1:3));
     end
     
     % Make it impossible to set bPause to true in deployed mode
     if isdeployed
-        parameters.bPause = 0;
+        x.opts.bPause = false;
     end
     
     % Check nWorkers for the parallelization of ExploreASL
-    if numel(parameters.nWorkers)>1
-        warning('nWorkers is supposed to be an integer number, resetting to 1...');
-        parameters.nWorkers = 1;
+    if numel(x.opts.nWorkers)>1
+        warning('nWorkers is supposed to be a scalar, not a vector, resetting to the first element...');
+        x.opts.nWorkers = x.opts.nWorkers(1);
     end
-    
-    % Store parsed input
-    x.opts = parameters;
     
 end
 
