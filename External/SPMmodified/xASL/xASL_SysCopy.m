1a0d173e590bec3999eecffb22c2e5c379631e9f
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2022-12-24 11:17:33 +0100

    #1273 xASL_Sys(Move|Copy): replace escaping by quotes

--- External/SPMmodified/xASL/xASL_SysCopy.m
+++ External/SPMmodified/xASL/xASL_SysCopy.m
@@ -31,23 +31,29 @@ function xASL_SysCopy(SrcPath, DstPath, bOverwrite, bVerbose)
     
     
     %% Do the copying
-
+    if ispc()
+        quote = '"';
+    else
+        quote = '''';
+    end
+    
     if exist(SrcPath, 'dir') && exist(DstPath, 'dir') && bOverwrite
         if bVerbose
             warning('We merge folders now, be careful, this could go wrong when e.g. NIfTIs exist both unzipped & zipped');
         end
     end
 
-    if isunix || ismac
-        SrcPath = xASL_adm_UnixPath(SrcPath);
-        DstPath = xASL_adm_UnixPath(DstPath);
+    if isunix
+%         SrcPath = xASL_adm_UnixPath(SrcPath);
+%         DstPath = xASL_adm_UnixPath(DstPath);
+        
 		if exist(SrcPath, 'dir') && exist(DstPath, 'dir')
 			% If we copy a directory to a directory in Linux, then this has to be done carefully, we want to copy the contents of SrcPath inside the 
 			% DstPath. This has to be done using /*. Otherwise, it would have copied the entire SrcPath and put it inside DstPath
 			if bOverwrite
-				system(['cp -r -f ' SrcPath '/* ' DstPath]); % -n is short for --noclober
+				system(['cp -r -f ' quote SrcPath quote '/* ' quote DstPath quote]); % -n is short for --noclober
 			else
-				system(['cp -r ' SrcPath '/* ' DstPath]);
+				system(['cp -r ' quote SrcPath '/*' quote ' ' quote DstPath quote]);
 			end
         elseif exist(SrcPath, 'file') && exist(DstPath, 'dir')
             % If Srcpath is a file but DstPath is a folder, this goes
@@ -58,9 +64,9 @@ function xASL_SysCopy(SrcPath, DstPath, bOverwrite, bVerbose)
         else
 			% If one of SrcPath and DstPath is a file, then we copy normally
 			if bOverwrite
-				system(['cp -r -f ' SrcPath ' ' DstPath]); % -n is short for --noclober
+				system(['cp -r -f ' quote SrcPath quote ' ' quote DstPath quote]); % -n is short for --noclober
 			else
-				system(['cp -r ' SrcPath ' ' DstPath]);
+				system(['cp -r ' quote SrcPath quote ' ' quote DstPath quote]);
 			end
 		end
     else
