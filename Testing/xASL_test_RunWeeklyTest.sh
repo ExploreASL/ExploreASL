5ee3ae4af305053654a790bd497d0aea4b4e380f
Maarten Hammer <maartenhammer@gmail.com>
2023-04-26 09:42:34 +0200

    #1247 changed standard locations to different dir

--- Testing/xASL_test_RunWeeklyTest.sh
+++ Testing/xASL_test_RunWeeklyTest.sh
@@ -97,7 +97,6 @@ fi
 # Run Flavor Test Parallelize?
 if ${bFlavorTest}; then
 	cd ${FlavorDir}
-	# TODO : remove rawdata and Derivatives folders for clean rerun.
 	git pull
 	FlavorVersion=`git rev-parse --short HEAD` 
 	cd ${XASLDIR}
