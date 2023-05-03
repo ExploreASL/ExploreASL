23fe82654568b3c4edf149c8e597daeea8474ffa
Maarten Hammer <maartenhammer@gmail.com>
2023-05-03 11:33:30 +0200

    #1374 made Bullseye callable, rotate text properly.

--- Functions/Visualization/xASL_vis_Bullseye.m
+++ Functions/Visualization/xASL_vis_Bullseye.m
@@ -1,4 +1,4 @@
-function  xASL_vis_Bullseye(x, savePath)
+function  xASL_vis_Bullseye(savePath, dataMatrix, dataLabels)
     %xASL_vis_Bullseye uses plotting to make a bullseye plot.
     %
     % FORMAT:       xASL_vis_Bullseye(x,);
@@ -17,20 +17,21 @@ function  xASL_vis_Bullseye(x, savePath)
     % Copyright 2015-2023 ExploreASL
     
 %% Admin
-if nargin<2
+if nargin<1 || isempty(savePath)
     savePath = 'Bullseye';
 end
-% Test Values
-% Define the number of segments
-segmentCount = 9;
-layerCount = 4;
 
-% Generate n random values between 0 and 1
-values = rand(segmentCount, layerCount);
+if nargin<2 || isempty(dataMatrix)
+    segmentCount = 9;
+    layerCount = 4;
+    dataMatrix = rand(segmentCount, layerCount);
+end
 
-% Actual function
-% Define the segment labels
-labels = {'Frontal', 'Parietal', 'Temporal', 'Occipital', 'SubCortical', 'Parietal', 'Temporal', 'Occipital', 'Frontal'};
+if nargin<3 || isempty(dataLabels)
+    dataLabels = {'Frontal', 'Parietal', 'Temporal', 'Occipital', 'SubCortical', 'Parietal', 'Temporal', 'Occipital', 'Frontal'};
+end
+
+[segmentCount, layerCount, ~ ] = size(dataMatrix);
 
 % Define the segment boundaries in degrees
 ANGLE_OFFSET = 90;
@@ -40,7 +41,7 @@ segmentBoundaryAngles = linspace(0, 360, segmentCount+1) + ANGLE_OFFSET;
 fig = figure;
 colormap('autumn');
 title('Coronal Bullseye Plot');
-colorbar('Ticks', 1:length(values), 'TickLabels', labels);
+colorbar('Ticks', 1:length(dataMatrix), 'TickLabels', dataLabels);
 axis off;
 
 % Plot the segments with different colors
@@ -52,14 +53,15 @@ for iLayer = 1:layerCount
         depth1 = (iLayer+1)/(layerCount+1);
         vertexX = [depth1*[cosd(angle2), cosd(angle1)], depth2*[cosd(angle1), cosd(angle2)]];  
         vertexY = [depth1*[sind(angle2), sind(angle1)], depth2*[sind(angle1), sind(angle2)]];
-        patch(vertexX, vertexY, values(iSegment,iLayer), 'EdgeColor', 'k');
+        patch(vertexX, vertexY, dataMatrix(iSegment,iLayer), 'EdgeColor', 'k');
     end
 end
 
 % Add the segment labels
 for iSegment = 1:segmentCount
     angle = mean([segmentBoundaryAngles(iSegment), segmentBoundaryAngles(iSegment+1)]);
-    text(cosd(angle), sind(angle), labels{iSegment}, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+    
+    text(cosd(angle), sind(angle), dataLabels{iSegment}, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle', 'rotation', xASL_sub_textAngle(angle));
 end
 
 text(0, 0, 'VALUES ARE GENERATED RANDOMLY, NOT ACTUAL VALUES', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
@@ -69,3 +71,12 @@ clear
     
     
 end
+
+function output = xASL_sub_textAngle(inputAngle)
+    % Quick function to ensure the text in the plot is always facedown.
+    output = inputAngle + 90;
+    while output > 90 
+        output = output - 180;
+    end
+end
+    
