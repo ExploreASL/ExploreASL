b4d693b4dbc6ad3781cdb0eb0bc379da55ecb586
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2023-05-19 10:36:34 +0200

    #1419 xASL_io_ReadJson: add user-friendly hint when JSON decoding fails

--- Functions/InputOutput/xASL_io_ReadJson.m
+++ Functions/InputOutput/xASL_io_ReadJson.m
@@ -10,9 +10,8 @@ function json = xASL_io_ReadJson(pathJSON)
 %   json               - output Matlab structure with the decoded JSON
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION: This function reads a JSON file located at the given path and decodes it using the Matlab routine
-%              JSONDECODE into a Matlab structure.
-%
+% DESCRIPTION: This function reads a JSON file located at the given path and decodes it using the Matlab built-in function JSONDECODE. The output is a Matlab
+% structure with the decoded JSON content.
 %
 % EXAMPLE: json = xASL_io_ReadJson('/tmp/test.json');
 % __________________________________
@@ -22,27 +21,39 @@ function json = xASL_io_ReadJson(pathJSON)
 %% ------------------------------------------------------------------------------------------------
 %% 0.   Administration
 if nargin < 1 || isempty(pathJSON)
-	error('The path to the JSON file is missing.');
+	error('The path to the JSON file is missing.'); % If the path is missing, we exit with an error
 end
 
 if ~xASL_exist(pathJSON,'file')
 	error(['Cannot find the JSON file: ' pathJSON]);
 end
 
-%% 1. Read the JSON file
-fileID = fopen(pathJSON, 'r');
+%% ------------------------------------------------------------------------------------------------
+%% 1. Load the JSON file
+fileID = fopen(pathJSON, 'r'); % Open the file for reading
+
 try 
-	txt = fscanf(fileID, '%c');
+	txt = fscanf(fileID, '%c'); % Read the content of the file
 catch ME
 	% In case an error appears during reading or decoding, we close the file and exit
-	fclose(fileID);
-	error('%s',ME.getReport());
+	fclose(fileID); % Close the file
+	error('%s',ME.getReport()); % Display the error message
 end
 
 % Close the file
-fclose(fileID);
+fclose(fileID); % Close the file
 
+%% ------------------------------------------------------------------------------------------------
 %% 2. Decode the text content
-json = jsondecode(txt);
+
+try
+	json = jsondecode(txt); % Decode the JSON content
+catch ME
+	% In case an error appears during reading or decoding, we exit
+	warning('Something went wrong during the decoding of the JSON file: %s', pathJSON');
+	fprintf('HINT: The JSON file might be corrupted, you can check the JSON file content at https://jsonlint.com \n');
+	error('%s',ME.getReport()); % Display the error message
+end
+
 
 end
