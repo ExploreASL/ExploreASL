0853ca72bef2c79d51d6ed95c62b35405590528a
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2023-07-02 17:38:30 +0200

    #1495 xASL_module_BIDS2Legacy: ready for incorporating xASL_wrp_BIDS2Legacy

--- Modules/xASL_module_BIDS2Legacy.m
+++ Modules/xASL_module_BIDS2Legacy.m
@@ -1,33 +1,49 @@
-function [result, x] = xASL_module_BIDS2Legacy(x)
-%xASL_module_BIDS2Legacy BIDS2LEGACY conversion script which calls xASL_wrp_BIDS2Legacy.
+function [result, x] = xASL_module_BIDS2Legacy(x, bOverwrite, bVerbose)
+%xASL_module_BIDS2Legacy Convert BIDS rawdata to ExploreASL legacy format
 %
-% FORMAT: [x] = xASL_module_BIDS2Legacy(x);
+% FORMAT: [x] = xASL_module_BIDS2Legacy(x [,bOverwrite]);
 %
 % INPUT:
-%   x             - Struct containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%   x             - ExploreASL x structure, containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%   bOverwrite    - Boolean indicating whether to overwrite existing files (OPTIONAL, DEFAULT = true)
+%   bVerbose   - boolean, true for verbose output (OPTIONAL, DEFAULT = false)
 %
 % OUTPUT:
-%   x             - Struct containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%   x             - ExploreASL x structure, containing pipeline environment parameters, useful when only initializing ExploreASL/debugging
+%  result         - Boolean indicating whether module succeeded
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION:    BIDS to Legacy conversion script which calls xASL_module_BIDS2Legacy.
+% DESCRIPTION: This function converts BIDS rawdata (in pathStudy/rawdata/) 
+% to xASL legacy derivative format (e.g. pathStudy/derivatives/ExploreASL/)
 %
-% 0. Input check
-% 1. Run legacy conversion
-% 2. Finalize and unlock mutex for this module
+% Can be updated step-by-step when ExploreASL's derivative structure moves to BIDS
+% NB: ask how Visits/session layer is defined in bids-matlab (should be
+% separate layer within subjects, but now isn't?)
+%
+% This function performs the following steps:
+%
+% 0. Initialization
+
+% 6. Finalize and unlock mutex for this module
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% EXAMPLE:        n/a
+% EXAMPLE:        x = xASL_module_BIDS2Legacy(x);
 %
 % __________________________________
 % Copyright (c) 2015-2023 ExploreASL
 
 
     %% 0. Initialization
-    
+    if nargin<3 || isempty(bVerbose)
+        bVerbose = 0; % default to not have verbose output for each subject (the mutex set up will already do this)
+    end
+    if nargin<2 || isempty(bOverwrite)
+        bOverwrite = 1; % Default is to overwrite existing files
+    end
+
     % Make sure that logging is still active
     if isfield(x.dir,'diaryFile')
-        diary(x.dir.diaryFile);
+        diary(x.dir.diaryFile); % Start diary
     end
     
     x = xASL_init_InitializeMutex(x, 'BIDS2Legacy'); % Start Mutex
@@ -35,17 +51,31 @@ function [result, x] = xASL_module_BIDS2Legacy(x)
     xASL_adm_BreakString('BIDS to ExploreASL LEGACY CONVERSION'); % Print feedback
 	
 
-    %% 1. Run legacy conversion
     if ~x.mutex.HasState('010_BIDS2LEGACY')
-        x = xASL_wrp_BIDS2Legacy(x, 1);
+
+
+
+
+
+
+
+        x = xASL_wrp_BIDS2Legacy(x, bOverwrite, bVerbose); % Run BIDS2Legacy conversion
+
+
+
+
+
+
+
+
         x.mutex.AddState('010_BIDS2LEGACY');
     elseif x.mutex.HasState('010_BIDS2LEGACY')
         fprintf('%s\n', ['BIDS2Legacy already done, skipping ' x.SUBJECT '.    ']);
     end
 
     
-    %% 2. Finalize and unlock mutex for this module
-    x.mutex.AddState('999_ready');
-    x.mutex.Unlock();
+    %% 6. Finalize and unlock mutex for this module
+    x.mutex.AddState('999_ready'); % Add ready state
+    x.mutex.Unlock(); % Unlock mutex
         
 end
