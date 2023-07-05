db0e4376fa8f2fbd97632f2f28a7367adfae8773
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2023-07-05 17:35:32 +0200

    #1495 xASL_init_DefineDataDependentSettings: integrate xASL_init_Toolboxes

--- Functions/Initialization/xASL_init_DefineDataDependentSettings.m
+++ Functions/Initialization/xASL_init_DefineDataDependentSettings.m
@@ -204,3 +204,42 @@ function [x] = xASL_init_DefinePaths(x)
 
     
 
+%% =======================================================================================================================
+%% =======================================================================================================================
+function x  = xASL_init_Toolboxes(x)
+%xASL_init_Toolboxes Check & load ancillary toolboxes, versions and paths
+%
+% FORMAT: x  = xASL_init_Toolboxes(x)
+%
+% INPUT:
+%   x       - ExploreASL x structure (STRUCT, REQUIRED)
+%
+% OUTPUT:
+%   x       - ExploreASL x structure (STRUCT)
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: Check & load ancillary toolboxes, versions and paths.
+%
+% EXAMPLE:     This is part of the initialization workflow. Check out the usage there.
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% REFERENCES:  n/a
+%
+% Copyright 2015-2021 ExploreASL
+
+
+    x.D.SPMDIR = fullfile(x.opts.MyPath, 'External', 'SPMmodified');
+    x.D.SPMpath = x.D.SPMDIR;
+    x.external.SPMVERSION = 'SPM12';
+
+    if isdeployed
+        spm('asciiwelcome');
+        spm('defaults','pet');
+        spm_jobman('initcfg');
+    else
+        addpath(fullfile(x.D.SPMpath ,'compat'));
+    end
+
+    spm_get_defaults('cmd_line',true);
+
+end
