6715e86539379bf2c5884cc33205aae3230ee8b0
Maarten Hammer <maartenhammer@gmail.com>
2023-08-17 10:42:09 +0200

    #1225 xASL_adm_LoadPDFConfig cleanup, documentation and replaced functions to native ExploreASL functions

--- Functions/Administration/xASL_adm_LoadPDFConfig.m
+++ Functions/Administration/xASL_adm_LoadPDFConfig.m
@@ -1,54 +1,58 @@
-function [Config] = xASL_adm_LoadPDFConfig(x)
-% xASL_adm_LoadPDFConfig Loads parameters from a .json config file
-% Essentialy an alias of spm_jsonread.
+function [config] = xASL_adm_LoadPDFConfig(x, configPath)
+% xASL_adm_LoadPDFConfig loads parameters from a .json config file to be used in xASL_qc_GenerateReport.
+% Using the config file, the user can define the parameters of the report.
+% If no config file is given, it will first look in the derivatives folder if configReportPDF.json exists.
+% If nothing exists there a default config file is loaded and used. (source at in the ExploreASL/Functions/QualityControl folder)
 %
-% FORMAT: [config] = xASL_adm_LoadPDFConfig(x)
+% FORMAT: [config] = xASL_adm_LoadPDFConfig(x[, configPath])
 %
 % INPUT:
-%   ConfigPath  - path to json REQUIRES
+%   x           - Struct containing all ExploreAsl Parameters (REQUIRED)
+%   configPath  - Path to the config file (OPTIONAL)
 %
 %   OUTPUT:
 %   config      - Struct containing all parameters of the jsonfile
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION: This function loads a *.json Config file, and is used at 
+% DESCRIPTION: This function loads a *.json config file, and is used at 
 % xASL_qc_GenerateReport to make a report with defined parameters:
 %
-% 1. Load JSON file
-% 2. Deal with warnings
-% 3. return a struct containing the config parameters
+% 1. Argument check
+% 2. Load JSON file
+% 3. Deal with warnings and return output
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE: [config] = xASL_adm_LoadPDFConfig(x);
 % __________________________________
-% Copyright 2015-2022 ExploreASL
+% Copyright 2015-2023 ExploreASL
 
 
 
 %% ------------------------------------------------------------------------
-% 0. Argument check
-Config = struct; % default
+% 1. Argument check
 
 if nargin<1 || isempty(x)
-    warning('x was not specified, aborting');
-    return
+    error('No x structure provided');
 end
 
-ConfigPath = strcat(x.dir.xASLDerivatives, '/ConfigReportPDF.json');
+if nargin<2 || isempty(configPath)
+    configPath = fullfile(x.dir.xASLDerivatives, 'configReportPDF.json');
+end
+
 
 %% ------------------------------------------------------------------------
 %% 2. Load JSON file 
-if isfile(ConfigPath)
-    Config = spm_jsonread(ConfigPath);
+if (exist(configPath, 'file') == 2)
+    config = xASL_io_ReadJson(configPath);
 else
-    fprintf('configReportPDF.json does not exists, using default config \n');
-    standardPath = strcat(x.opts.MyPath, '/Functions/QualityControl/templateConfigReportPDF.json');
-    Config = spm_jsonread(standardPath);
+    fprintf([configPath ' does not exists, using default configuration for PDF generation.\n']);
+    standardPath = fullfile(x.opts.MyPath, 'Functions', 'QualityControl', 'templateConfigReportPDF.json');
+    config = xASL_io_ReadJson(standardPath);
 end
 
 %% ------------------------------------------------------------------------
 %% 3. Deal with warnings
-if isempty(fields(Config))
-    warning('config struct empty, Check if config JSON is configured correctly.');
+if isempty(fields(config))
+    warning('Check if configuration JSON for the PDF report is correct, no parameters were loaded.');
 end
 
 end
