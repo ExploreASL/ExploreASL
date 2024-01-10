374f4df132a037b2a632c91a03f67441e7aa2c1e
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2024-01-10 13:47:29 +0100

    #1587 xASL_adm_DeleteManyTempFiles: improvement based on Jans review

--- Modules/Module_Processing/Module_Population/xASL_adm_DeleteManyTempFiles.m
+++ Modules/Module_Processing/Module_Population/xASL_adm_DeleteManyTempFiles.m
@@ -8,7 +8,11 @@ function xASL_adm_DeleteManyTempFiles(x)
 % OUTPUT:       n/a
 % 
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% DESCRIPTION:  This function removes as many files as possible.
+% DESCRIPTION:  This function removes as many files as possible, for the following 2 spaces and 2 modules:
+% 1. Native space — structural module files (currently no files here)
+% 2. Native space — ASL module files
+% 3. Standard space — structural module files
+% 4. Standard space — ASL module files
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
 % EXAMPLE:      xASL_adm_DeleteManyTempFiles(x);
@@ -16,14 +20,15 @@ function xASL_adm_DeleteManyTempFiles(x)
 % Copyright 2015-2024 ExploreASL
 
 Files2DelNativeStructural = {}; % (currently no files here)
+% If we add files here, we need to decomment the code below under section 1
 Files2DelNativeASL = {'ATT_BiasField.nii' 'Mask_Template.nii' 'Mean_CBF_Template.nii' 'PseudoCBF.nii' 'RawTemplate.nii' 'VascularArtifact_Template.nii' 'mean_PWI_Clipped.nii' 'SliceGradient_extrapolated.nii' 'FoV.nii'};
 Files2DelStandardStructural = {'rT1_ORI_' 'rT1_' 'rFLAIR_' '(m|)rc\dT1_'};
 Files2DelStandardASL = {'noSmooth_M0_' 'mean_control_' 'PWI_' 'SliceGradient_' 'SNR'};
 
 %% First, we create a list of subjects and sessions of which the structural and ASL modules, respectively, are completed
-[listSubjects, listSessions] = xASL_adm_GetListCompletedModules(x);
+[listStructDone, listASLDone] = xASL_adm_GetListCompletedModules(x);
 
-if isfield(x,'D') && isfield(x.dir,'xASLDerivatives')
+if isfield(x,'dir') && isfield(x.dir,'xASLDerivatives')
 
     %% 1. Native space — structural module files (currently no files here)
     % fprintf('Deleting temporary structural module files in native space folders:    ');
@@ -31,9 +36,9 @@ if isfield(x,'D') && isfield(x.dir,'xASLDerivatives')
     % for iP=1:length(Files2DelNativeStructural)
     %     xASL_TrackProgress(iP, length(Files2DelNativeStructural));
     % 
-    %     for iSubject=1:length(listSubjects)
+    %     for iSubject=1:length(listStructDone)
     %         % Delete this file, if it exists
-    %         xASL_delete(fullfile(x.dir.xASLDerivatives, listSubjects{iSubject}, Files2DelNativeStructural{iP}));
+    %         xASL_delete(fullfile(x.dir.xASLDerivatives, listStructDone{iSubject}, Files2DelNativeStructural{iP}));
     %     end
     % end
     % fprintf('\n');
@@ -44,10 +49,10 @@ if isfield(x,'D') && isfield(x.dir,'xASLDerivatives')
     for iP=1:length(Files2DelNativeASL)
         xASL_TrackProgress(iP, length(Files2DelNativeASL));
         
-        for iSession=1:length(listSessions)
-            sessionIndex = regexp(listSessions{iSession}, 'ASL_\d*');
-            sessionIs = listSessions{iSession}(sessionIndex:end);
-            subjectIs = listSessions{iSession}(1:sessionIndex-2);
+        for iSession=1:length(listASLDone)
+            sessionIndex = regexp(listASLDone{iSession}, 'ASL_\d*$');
+            sessionIs = listASLDone{iSession}(sessionIndex:end);
+            subjectIs = listASLDone{iSession}(1:sessionIndex-2);
             
             % Delete this file, if it exists
             xASL_delete(fullfile(x.dir.xASLDerivatives, subjectIs, sessionIs, Files2DelNativeASL{iP}));
@@ -65,9 +70,9 @@ fprintf('Deleting temporary structural files in population folder:    ');
 for iPath=1:length(Files2DelStandardStructural)
     xASL_TrackProgress(iP,length(Files2DelStandardStructural));
 
-    for iSubject=1:length(listSubjects)
+    for iSubject=1:length(listStructDone)
         % Delete any existing files
-        xASL_adm_DeleteFileList(x.D.PopDir, ['^' Files2DelStandardStructural{iPath} listSubjects{iSubject} '.*$'], 1, [0 Inf]);
+        xASL_adm_DeleteFileList(x.D.PopDir, ['^' Files2DelStandardStructural{iPath} listStructDone{iSubject} '.*$'], 1, [0 Inf]);
     end
 end
 fprintf('\n');
@@ -79,9 +84,9 @@ fprintf('Deleting temporary ASL files in population folder:    ');
 for iPath=1:length(Files2DelStandardASL)
     xASL_TrackProgress(iP,length(Files2DelStandardASL));
     
-    for iSession=1:length(listSessions)
+    for iSession=1:length(listASLDone)
         % Delete any existing files
-        xASL_adm_DeleteFileList(x.D.PopDir, ['^' Files2DelStandardASL{iPath} listSessions{iSession} '.*$'], 1, [0 Inf]);
+        xASL_adm_DeleteFileList(x.D.PopDir, ['^' Files2DelStandardASL{iPath} listASLDone{iSession} '.*$'], 1, [0 Inf]);
     end
 end
 fprintf('\n');
