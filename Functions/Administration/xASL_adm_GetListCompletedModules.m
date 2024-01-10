9490630c3b08c2f78e385d2982e17656970bcffd
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2024-01-10 13:47:16 +0100

    #1587 xASL_adm_GetListCompleted: improvement based on Jans review

--- Functions/Administration/xASL_adm_GetListCompletedModules.m
+++ Functions/Administration/xASL_adm_GetListCompletedModules.m
@@ -1,7 +1,7 @@
 function [listStructDone, listASLDone] = xASL_adm_GetListCompletedModules(x)
 %xASL_adm_GetListCompletedModules This function obtains a list of completed modules
 %
-% FORMAT:       xASL_adm_GetListCompletedModules(x)
+% FORMAT:       [listStructDone, listASLDone] = xASL_adm_GetListCompletedModules(x)
 % 
 % INPUT:        x               - x structure of ExploreASL
 %
@@ -14,7 +14,7 @@ function [listStructDone, listASLDone] = xASL_adm_GetListCompletedModules(x)
 %               This can be useful for subsequent group processing in e.g., the ExploreASL population module.
 %
 % -----------------------------------------------------------------------------------------------------------------------------------------------------
-% EXAMPLE:      xASL_adm_GetListCompletedModules(x);
+% EXAMPLE:      [listStructDone, listASLDone] = xASL_adm_GetListCompletedModules(x);
 % __________________________________
 % Copyright 2015-2024 ExploreASL
 
@@ -28,8 +28,7 @@ listStructSubjects = xASL_adm_GetFileList(dirLockStruct, '.*', 'List', [], 1);
 listStructDone = {};
 
 for iList = 1:length(listStructSubjects)
-    currentDir = fullfile(dirLockStruct, listStructSubjects{iList});
-    lockFile999 = fullfile(currentDir, 'xASL_module_Structural', '999_ready.status');
+    lockFile999 = fullfile(dirLockStruct, listStructSubjects{iList}, 'xASL_module_Structural', '999_ready.status');
     if exist(lockFile999, 'file')
         listStructDone{end+1} = listStructSubjects{iList};
     end
@@ -38,11 +37,9 @@ end
 listASLSubjects = xASL_adm_GetFileList(dirLockASL, '.*', 'List', [], 1);
 listASLDone = {};
 for iList = 1:length(listASLSubjects)
-    currentDir = fullfile(dirLockASL, listASLSubjects{iList});
-    sessions = xASL_adm_GetFileList(currentDir, '^xASL_module_ASL_.*', 'List', [], 1);
+    sessions = xASL_adm_GetFileList(fullfile(dirLockASL, listASLSubjects{iList}, '^xASL_module_ASL_.*', 'List', [], 1);
     for iSession=1:length(sessions)
-        currentDir = fullfile(dirLockASL, listASLSubjects{iList}, sessions{iSession});
-        lockFile999 = fullfile(currentDir, '999_ready.status');
+        lockFile999 = fullfile(dirLockASL, listASLSubjects{iList}, sessions{iSession}, '999_ready.status');
         if exist(lockFile999, 'file')
             listASLDone{end+1} = [listASLSubjects{iList} '_' sessions{iSession}(length('xASL_module_ASL_')+1:end)];
         end
