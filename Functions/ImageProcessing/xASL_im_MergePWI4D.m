11977bbbf4e4c1e997dc303d7df1d0731d165034
jan-petr <j.petr@hzdr.de>
2024-02-07 10:56:22 +0100

    #1543 Fix merging PWI4D with JSONs

--- Functions/ImageProcessing/xASL_im_MergePWI4D.m
+++ Functions/ImageProcessing/xASL_im_MergePWI4D.m
@@ -1,38 +1,101 @@
 function [path_PWI4D, path_PWI4D_Pop] = xASL_im_MergePWI4D(x)
-% Merge several PWIs based on the list in x.modules.asl.sessionsToMerge
-PWI = []; % this becomes the total concatenated image matrix
+% xASL_im_MergePWI4D merges several PWIs based on the list in x.modules.asl.sessionsToMerge
+%
+% FORMAT: [path_PWI4D, path_PWI4D_Pop] = xASL_im_MergePWI4D(x)
+%
+% INPUT:
+%   x                   - structure containing fields with all information required to run this submodule (REQUIRED)
+%
+% OUTPUT: 
+%   path_PWI4D          - Path to the new merged file
+%   path_PWI4D_Pop      - Path to the new merged file in the Population directory
+% OUTPUT FILES: 
+%   NIfTI files (see paths above) containing the merged sessions.
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION: This function takes previously created PWI4D.nii from sessions described in the x.modules.asl.sessionsToMerge and 
+%              merges them to a single NII with a predefined name. It returns this new name for both native and standard space and it also creates the 
+%              appropriate JSON sidecars.
+%
+% EXAMPLE: 
+%   [path_PWI4D, path_PWI4D_Pop] = xASL_im_MergePWI4D(x)
+% __________________________________
+% Copyright (C) 2015-2024 ExploreASL
 
-fprintf('Concatenating PWI4D for sessions/runs (nVolumes):');
-
-for iSession = 1:numel(x.modules.asl.sessionsToMerge)
-	% Here we get the path of the first session, by replacing the foldername ASL_X
-    pathCurrentPWI4D = replace(x.P.Path_PWI4D, x.modules.asl.sessionsToMerge{end}, x.modules.asl.sessionsToMerge{iSession});
-    
-	if xASL_exist(pathCurrentPWI4D, 'file')
-		imPWI4Dcurrent = xASL_io_Nifti2Im(pathCurrentPWI4D);
-        
-        dim4(iSession) = size(imPWI4Dcurrent, 4);
-
-        fprintf([' ' x.modules.asl.sessionsToMerge{iSession} ' (' xASL_num2str(dim4(iSession)) ')']);
-
-		if iSession == 1
-			% Take the first volume as it is
-			PWI = imPWI4Dcurrent;
-		else
-			% For the following ones, check dimensions
-			if isequal(size(imPWI4Dcurrent, 1:3), size(PWI, 1:3))
-				% Here we concatenate (cat) over the 4rd dimension (4), PWI (total concatenated image matrix) with the new current PWI
-                PWI = cat(4, PWI, imPWI4Dcurrent);
-			else
-				error(['Cannot concatenate sessions for subject ' x.SUBJECT ' as session '  x.modules.asl.sessionsToMerge{iSession} ' has a different matrix size from the previous sessions.']);
-			end
-		end
-	else
-		% If one of the sessions are missing, then we issue an error
-		error(['Cannot concatenate sessions for subject ' x.SUBJECT ': session '  x.modules.asl.sessionsToMerge{iSession} ' is missing.']);
-	end
+% 0 Admin
+if nargin<1 || isempty(x)
+	error('Empty input parameters');
 end
 
-fprintf('\n%s\n', ['Concatenated PWI4Ds into a new PWI4D with name XXXXX #1543 and ' xASL_num2str(sum(dim4)) ' volumes']);
+fprintf('Concatenating PWI4D for sessions:');
+% Do this for native and standard space
+pathPWI4D = {x.P.Path_PWI4D x.P.Pop_Path_PWI4D};
 
+% Name of the merged session
+sessionName = [];
+
+for iSpace = 1:2
+	for iSession = 1:numel(x.modules.asl.sessionsToMerge)
+		% Here we get the path of the session, by replacing the session names 
+		pathCurrentPWI4D = replace(pathPWI4D{iSpace}, x.modules.asl.sessionsToMerge{end}, x.modules.asl.sessionsToMerge{iSession});
+
+		% Load NII and JSON
+		if xASL_exist(pathCurrentPWI4D, 'file')
+			[imPWI4Dcurrent, jsonPWI4Dcurrent] = xASL_io_Nifti2Im(pathCurrentPWI4D); % Don't even bother converting to Legacy before merging
+		else
+			% If one of the sessions are missing, then we issue an error
+			error(['Cannot concatenate sessions for subject ' x.SUBJECT ': session '  x.modules.asl.sessionsToMerge{iSession} ' is missing.']);
+		end
+
+		% Error with a missing JSON
+		if isempty(jsonPWI4Dcurrent)
+			error(['Cannot concatenate sessions for subject ' x.SUBJECT ': session '  x.modules.asl.sessionsToMerge{iSession} ' has missing JSON information.']);
+		end
+
+		% Print the name of the session being added
+		if iSpace == 1
+			fprintf([' ' x.modules.asl.sessionsToMerge{iSession}]);
+			sessionName = [sessionName '_' x.modules.asl.sessionsToMerge{iSession}];
+		end
+
+		if iSession == 1
+			% For the first session, we take both JSON and NII as they are
+			imPWI4DConcatenated = imPWI4Dcurrent; 
+
+			% Note the we don't convert to Legacy and work with BIDS format directly
+			jsonPWI4DConcatenated = struct();
+			jsonPWI4DConcatenated.EchoTime = jsonPWI4Dcurrent.EchoTime;
+			jsonPWI4DConcatenated.PostLabelingDelay = jsonPWI4Dcurrent.PostLabelingDelay;
+			jsonPWI4DConcatenated.LabelingDuration = jsonPWI4Dcurrent.LabelingDuration;
+		else	
+			% For following sessions, concatenate NII and JSON
+			% Check dimensions
+			if isequal(size(imPWI4Dcurrent, 1:3), size(imPWI4DConcatenated, 1:3))
+				% Here we concatenate (cat) over the 4rd dimension (4), PWI (total concatenated image matrix) with the new current PWI
+                imPWI4DConcatenated = cat(4, imPWI4DConcatenated, imPWI4Dcurrent);
+
+				% Concatenate the JSON info
+				jsonPWI4DConcatenated.EchoTime = [jsonPWI4DConcatenated.EchoTime; jsonPWI4Dcurrent.EchoTime];
+				jsonPWI4DConcatenated.PostLabelingDelay = [jsonPWI4DConcatenated.PostLabelingDelay; jsonPWI4Dcurrent.PostLabelingDelay];
+				jsonPWI4DConcatenated.LabelingDuration = [jsonPWI4DConcatenated.LabelingDuration; jsonPWI4Dcurrent.LabelingDuration];
+			else
+				error(['Cannot concatenate sessions for subject ' x.SUBJECT ' as session '  x.modules.asl.sessionsToMerge{iSession} ' has a different matrix size than the previous sessions.']);
+			end
+		end
+	end
+	% Create the specific new name
+	[fPath, fName, fExtension] = xASL_fileparts(pathPWI4D{iSpace});
+	pathOut = fullfile(fPath, [fName sessionName fExtension]);
+
+	if iSpace == 1
+		path_PWI4D = pathOut;
+	else
+		path_PWI4D_Pop = pathOut;
+	end
+
+	% Save NII with JSON
+	xASL_io_SaveNifti(pathPWI4D{iSpace}, pathOut, imPWI4DConcatenated, [], 0, [], 0, jsonPWI4DConcatenated);
+
+end
+fprintf('\n');
 end
