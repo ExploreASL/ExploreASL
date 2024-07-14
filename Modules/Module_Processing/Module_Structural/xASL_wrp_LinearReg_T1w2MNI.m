278c16d86fec78bae26676f94ef7b89488705ac7
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2024-07-14 19:07:10 +0200

    #1733 xASL_wrp_LinearReg_T1w2MNI: check ASL orientation matrices

--- Modules/Module_Processing/Module_Structural/xASL_wrp_LinearReg_T1w2MNI.m
+++ Modules/Module_Processing/Module_Structural/xASL_wrp_LinearReg_T1w2MNI.m
@@ -37,14 +37,10 @@ if ~necessaryPaths
     warning('Seemingly you are using xASL_wrp_LinearReg_T1w2MNI without defining all necessary paths...');
 end
 
-%% ---------------------------------------------------------------------------------------------------
-%% 1) Restore the orientation matrix of all images, in case we perform a re-run: but only when we don't have lesion maps
-Lesion_ROI_list = xASL_adm_GetFileList(x.dir.SUBJECTDIR, ['(?i)^(Lesion|ROI)_(' x.P.STRUCT '|' x.P.FLAIR ')_\d*\.nii$'], 'FPList', [0 Inf]);
 
 %% ---------------------------------------------------------------------------------------------------
-%% 2)Obtain lists of paths
-
-refPath = fullfile(x.D.SPMDIR, 'toolbox','OldNorm','T1.nii'); % = SPM8 T1 template, to make sure it is always the same
+%% 1. Obtain path lists
+refPath = fullfile(x.D.SPMDIR, 'toolbox', 'OldNorm', 'T1.nii'); % = SPM8 T1 template, to make sure it is always the same
 % This T1 reference image, is a blurred one. In SPM12 there are new reference images, but less blurry.
 % This one empirically works fine
 OtherList{1,1} = x.P.Path_FLAIR;
@@ -53,34 +49,66 @@ OtherList{end+1,1} = x.P.Path_T1c;
 OtherList{end+1,1} = x.P.Path_T2;
 
 % Add lesion masks to the registration list
+Lesion_ROI_list = xASL_adm_GetFileList(x.dir.SUBJECTDIR, ['(?i)^(Lesion|ROI)_(' x.P.STRUCT '|' x.P.FLAIR ')_\d*\.nii$'], 'FPList', [0 Inf]);
 for iS=1:length(Lesion_ROI_list)
     OtherList{end+1,1} = Lesion_ROI_list{iS};
 end
-% Add ASL images to the registration list
-% for iSess = 1:x.dataset.nSessions
-%     OtherList{end+1,1} = x.P.Path_ASL4D;
-%     OtherList{end+1,1} = x.P.Path_M0;
-%     OtherList{end+1,1} = x.P.Path_ASL4D_RevPE;
-%     OtherList{end+1,1} = x.P.Path_M0_RevPE;
     
-    % Check for other ScanTypes
-    SubjectsFolders = xASL_adm_GetFileList(x.dir.SUBJECTDIR, '^.*', 'FPList', [0 Inf],true); % true for folders
-    for iOther=1:length(SubjectsFolders)
-        DirOther = SubjectsFolders{iOther};
-        if exist(DirOther,'dir')
-            Path_Other = xASL_adm_GetFileList(DirOther, '^(?!y_).*\.nii$', 'FPList', [0 Inf]); % any DWI or func NIfTI (quick & dirty)
-            for iScan=1:length(Path_Other)
-                OtherList{end+1,1} = Path_Other{iScan};
-            end
+% Check for other ScanTypes that need to be in alignment of the T1w
+% Here, we assume that all NIfTIs from all other scan types (e.g., ASL, fMRI, DTI, etc)
+% need to remain aligned with the T1w
+otherFolders = xASL_adm_GetFileList(x.dir.SUBJECTDIR, '^.*', 'FPList', [0 Inf],true); % true for folders
+for iOther=1:length(otherFolders)
+    DirOther = otherFolders{iOther};
+    if exist(DirOther, 'dir')
+        Path_Other = xASL_adm_GetFileList(DirOther, '^(?!y_).*\.nii$', 'FPList', [0 Inf]); % any DWI or func NIfTI (quick & dirty)
+        for iScan=1:length(Path_Other)
+            OtherList{end+1,1} = Path_Other{iScan};
         end
     end
-% end
-
+end
 
 
 %% ---------------------------------------------------------------------------------------------------
-%% 3)Perform the registration
+%% 2. Issue warnings if any of the orientation matrices were changed (e.g. in an earlier processing run)
+% A potential error can occur if for whatever reason, in a previous rerun some where realigned (e.g., to MNI) and others were not.
+% So we will now issue a warning if any of the orientation matrices were changed (e.g. in an earlier processing run).
 
+% Note that this can be normal for FLAIR and its derivative WMH_SEGM, as these are first resampled to T1w.nii
+% For T1w.nii, this can also be normal if the T1w was already realigned to MNI in a previous run.
+% So we check this for ASL only, by comparing the nii.mat to nii.mat0. Either all were realigned, or none. If only some were realigned, we issue a warning.
+
+% Get all ASL NIfTIs
+checkList = [];
+for iOther=1:length(OtherList)
+    if xASL_exist(OtherList{iOther})
+        [~, folderName] = fileparts(fileparts(OtherList{iOther}));
+        if ~isempty(regexp(folderName, '^ASL_\d*$'))
+            checkList{end+1} = OtherList{iOther};
+        end
+    end
+end
+
+% Check orientation differences for all ASL NIfTIs
+% between nii.mat0 (original orientation from the scanner)
+% and nii.mat (potential new orientation after registration)
+% If no registration happened, these are equal
+for iCheck=1:length(checkList)
+    nii = xASL_io_ReadNifti(checkList{iCheck});
+    matEqual(iCheck) = min(min(nii.mat==nii.mat0));
+end
+
+% Now we issue a warning if some NIfTIs had unequal orientation matrices
+% — i.e., they were previously realigned —
+% and others did not. In this case, matEqual has both 1s and 0s.
+if length(unique(matEqual))>1
+    warning('Some ASL NIfTIs were previously realigned whereas others were not!!!');
+end
+
+
+%% ---------------------------------------------------------------------------------------------------
+%% 3. Perform the registration
+%  this will be estimated on the T1w & applied to all NIfTIs
 xASL_im_ClipExtremes(x.P.Path_T1, 0.999, 0, [], 1); % First we clip high vascular intensities & normalize to 4096, for more stable image contrast
 
 if bAutoACPC % Then start with center of mass detection & realign with this
@@ -89,7 +117,5 @@ end
 % Finally, run the SPM coregistration
 xASL_spm_coreg(refPath, x.P.Path_T1, OtherList, x);
 
-    
-    
-end
 
+end
