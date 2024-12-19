3507a6b753b1d17c8394dbeeb36c3e6710a3fa31
jan-petr <j.petr@hzdr.de>
2024-12-19 22:51:39 +0100

    #1855 AAL and WMPM atlas

---
+++ WorkInProgress/Administration/xASL_adm_CoregisterAtlas.m
@@ -0,0 +1,131 @@
+% Copyright 2015-2024 ExploreASL (Works In Progress code)
+% Licensed under Apache 2.0, see permissions and limitations at
+% https://github.com/ExploreASL/ExploreASL/blob/main/LICENSE
+% you may only use this file in compliance with the License.
+% __________________________________
+
+%% Please add here the path to the atlas files and to ExploreASL
+pathTPM = '/home/janpetr/ExploreASL/Atlas';
+pathTPMAtlasDir = 'mni_icbm152_nlin_asym_09c';
+pathTPMAtlasFile = 'mni_icbm152_gm_tal_nlin_asym_09c.nii';
+pathTPMAtlasFileWM = 'mni_icbm152_wm_tal_nlin_asym_09c.nii';
+pathTPMAtlasFileGMWM = 'mni_icbm152_gmwm_tal_nlin_asym_09c.nii';
+pathExploreASL = '/home/janpetr/ExploreASL/ExploreASL';
+
+
+%% Load the ICMB-152 atlas 
+% We used this one http://www.bic.mni.mcgill.ca/~vfonov/icbm/2009/mni_icbm152_nlin_asym_09a_nifti.zip
+% This is the reference template that we assume the atlases are registered to
+
+% We create a combined image
+imGM = xASL_io_Nifti2Im(fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFile));
+imWM = xASL_io_Nifti2Im(fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFileWM));
+imCombined = imGM;
+imCombined(:,:,:,2) = imWM;
+
+xASL_io_SaveNifti(fullfile(pathTPM,pathTPMAtlasDir, pathTPMAtlasFile),fullfile(pathTPM,pathTPMAtlasDir, pathTPMAtlasFileGMWM),imCombined);
+
+%% Presmooth the atlas and move it to the IXI resolution
+xASL_im_PreSmooth(fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_1_IXI555_MNI152.nii'),fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFileGMWM));
+xASL_im_PreSmooth(fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_1_IXI555_MNI152.nii'),fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFile));
+xASL_im_PreSmooth(fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_1_IXI555_MNI152.nii'),fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFileWM));
+
+% And resample to 1.5mm DARTEL space
+matlabbatch = [];
+matlabbatch{1}.spm.util.defs.comp{1}.id.space = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_1_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.util.defs.out{1}.pull.fnames = {fullfile(pathTPM, pathTPMAtlasDir, ['s' pathTPMAtlasFileGMWM])
+	fullfile(pathTPM, pathTPMAtlasDir, ['s' pathTPMAtlasFile])
+	fullfile(pathTPM, pathTPMAtlasDir, ['s' pathTPMAtlasFileWM])};
+matlabbatch{1}.spm.util.defs.out{1}.pull.savedir.savesrc = 1;
+matlabbatch{1}.spm.util.defs.out{1}.pull.interp = 4;
+matlabbatch{1}.spm.util.defs.out{1}.pull.mask = 1;
+matlabbatch{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
+spm_jobman('run',matlabbatch);
+%% Align the reference template to the IXI512 template
+
+matlabbatch = [];
+matlabbatch{1}.spm.tools.dartel.warp1.images = {
+                                                {fullfile(pathTPM, pathTPMAtlasDir, ['ws' pathTPMAtlasFile])}
+												{fullfile(pathTPM, pathTPMAtlasDir, ['ws' pathTPMAtlasFileWM])}
+                                                }';
+matlabbatch{1}.spm.tools.dartel.warp1.settings.rform = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).rparam = [4 2 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_1_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).rparam = [2 1 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_2_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).rparam = [1 0.5 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).K = 1;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_3_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).rparam = [0.5 0.25 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).K = 2;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_4_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).K = 4;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_5_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).K = 6;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_6_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.lmreg = 0.01;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.cyc = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.its = 3;
+
+spm_jobman('run',matlabbatch);
+%% Transform the template to the correct template, just as a test
+matlabbatch = [];
+matlabbatch{1}.spm.util.defs.comp{1}.dartel.flowfield = {fullfile(pathTPM, pathTPMAtlasDir, ['u_ws' pathTPMAtlasFile])};
+matlabbatch{1}.spm.util.defs.comp{1}.dartel.times = [1 0];
+matlabbatch{1}.spm.util.defs.comp{1}.dartel.K = 6;
+matlabbatch{1}.spm.util.defs.comp{1}.dartel.template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_6_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.util.defs.out{1}.pull.fnames = {fullfile(pathTPM, pathTPMAtlasDir, ['ws' pathTPMAtlasFile])};
+matlabbatch{1}.spm.util.defs.out{1}.pull.savedir.saveusr = {pathTPM};
+matlabbatch{1}.spm.util.defs.out{1}.pull.interp = 4;
+matlabbatch{1}.spm.util.defs.out{1}.pull.mask = 1;
+matlabbatch{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
+matlabbatch{1}.spm.util.defs.out{1}.pull.prefix = 'o';
+
+spm_jobman('run',matlabbatch);
+
+%% Transform the atlases
+% This has to be done label per label
+
+% We load atlas and count all labels
+imAtlas = xASL_io_Nifti2Im(fullfile(pathTPM, 'AAL3', 'AAL3v1_1mm.nii'));
+max(imAtlas(:))
+imAtlas = xASL_io_Nifti2Im(fullfile(pathTPM, 'WMPM', 'WMPM_Type_III.nii'));
+max(imAtlas(:))
+
+% Nearest neighbor transformation
+
+matlabbatch = [];
+matlabbatch{1}.spm.util.defs.comp{1}.id.space = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_6_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.util.defs.comp{2}.dartel.flowfield = {fullfile(pathTPM, pathTPMAtlasDir, ['u_ws' pathTPMAtlasFile])};
+matlabbatch{1}.spm.util.defs.comp{2}.dartel.times = [1 0];
+matlabbatch{1}.spm.util.defs.comp{2}.dartel.K = 6;
+matlabbatch{1}.spm.util.defs.comp{2}.dartel.template = {fullfile(pathExploreASL, 'External', 'SPMmodified', 'toolbox', 'cat12', 'templates_volumes' ,'Template_6_IXI555_MNI152.nii')};
+matlabbatch{1}.spm.util.defs.out{1}.pull.fnames = {fullfile(pathTPM, 'AAL3', 'AAL3v1_1mm.nii')
+	                                               fullfile(pathTPM, pathTPMAtlasDir, pathTPMAtlasFile)
+												   fullfile(pathTPM, 'WMPM', 'WMPM_Type_III.nii')};
+matlabbatch{1}.spm.util.defs.out{1}.pull.savedir.saveusr = {pathTPM};
+matlabbatch{1}.spm.util.defs.out{1}.pull.interp = 0;
+matlabbatch{1}.spm.util.defs.out{1}.pull.mask = 1;
+matlabbatch{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
+matlabbatch{1}.spm.util.defs.out{1}.pull.prefix = 'o';
+
+spm_jobman('run',matlabbatch);
+
+% Create MAT file
+IM = xASL_io_Nifti2Im(fullfile(pathTPM, 'oAAL3v1_1mm.nii'));
+save(fullfile(pathTPM, 'oAAL3v1_1mm.nii.mat'),'IM');
+xASL_adm_GzipNifti(fullfile(pathTPM, 'oAAL3v1_1mm.nii'));
+
+IM = xASL_io_Nifti2Im(fullfile(pathTPM, 'oWMPM_Type_III.nii'));
+save(fullfile(pathTPM, 'oWMPM_Type_III.nii.mat'),'IM');
+xASL_adm_GzipNifti(fullfile(pathTPM, 'oWMPM_Type_III.nii'));
