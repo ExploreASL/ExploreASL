435806ac5c53a2514222bc67b67c78ec577cbb4e
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2024-12-22 07:35:32 +0100

    #1855 WMPM: LeftRight>Bilateral

--- WorkInProgress/Administration/xASL_adm_AtlasConvert_LeftRight2Bilateral.m
+++ WorkInProgress/Administration/xASL_adm_AtlasConvert_LeftRight2Bilateral.m
@@ -29,6 +29,10 @@ TSV = TSV(1:90);
 
 xASL_io_SaveNifti(IMpath, IMpath, IM, []);
 xASL_tsvWrite(TSV', TSVpath, 1);
+IM = uint8(IM);
+save([IMpath '.mat'],'IM');
+gzip(IMpath);
+delete(IMpath);
 
 The left-right ROIs are averaged as ExploreASL divides ROIs in left-right-bilateral automatically,
 except for ROIs 113-120 (vermis) and 169-170 (raphe medial and dorsal) which are bilateral already).
@@ -38,3 +42,61 @@ Hence:
 121:168 -> 65:88
 169:170 -> 89:90
 
+%% The same for WMPM
+
+clear TSVnew
+
+IMpath = '/Users/hjmutsaerts/ExploreASL/ExploreASL/External/Atlases4ROIs/LicenseLimited/WMPM_Type_III.nii';
+IM = xASL_io_Nifti2Im(IMpath);
+TSVpath = '/Users/hjmutsaerts/ExploreASL/ExploreASL/External/Atlases4ROIs/LicenseLimited/WMPM_Type_III.tsv';
+TSV = xASL_tsvRead(TSVpath);
+
+IMnew = zeros(size(IM));
+
+% Remove double _
+TSV = strrep(TSV, '__', '_');
+
+% Get left or right
+LeftIs = contains(TSV(:,1), 'left');
+RightIs = contains(TSV(:,1), 'right');
+TSV(LeftIs,2) = {1}; % left
+TSV(RightIs,2) = {2}; % right
+
+% Remove trailing left/right
+TSV(:,1) = strrep(TSV(:,1), '_left', '');
+TSV(:,1) = strrep(TSV(:,1), 'left', '');
+TSV(:,1) = strrep(TSV(:,1), '_right', '');
+TSV(:,1) = strrep(TSV(:,1), 'right', '');
+
+% Bugfix
+TSV{108,1} = strrep(TSV{108,1}, '_(a_part_of_MCP)', '');
+
+iROInew = 1;
+reportSingleROI = {''};
+for iROI=1:length(TSV)
+    % is there a ROI with a similar name
+    if ~isempty(TSV{iROI,1})
+        indicesAre = find(strcmp(TSV(:,1), TSV{iROI,1}));
+        
+        if numel(indicesAre)==1
+            reportSingleROI{end+1,1} = TSV{iROI,1};
+            reportSingleROI{end,2} = iROI;
+        end
+
+        % assuming they are always either left or right
+        TSVnew{iROInew} = TSV{iROI,1};
+        for iIndices=1:length(indicesAre)
+            IMnew(IM==indicesAre(iIndices)) = iROInew;
+        end
+        % Remove current ROI
+        TSV(indicesAre,1) = {''};
+        iROInew = iROInew+1;
+    end
+end
+
+xASL_io_SaveNifti(IMpath, IMpath, IMnew, []);
+xASL_tsvWrite(TSVnew, TSVpath, 1);
+IM = uint8(IMnew);
+save([IMpath '.mat'],'IM');
+gzip(IMpath);
+delete(IMpath);
