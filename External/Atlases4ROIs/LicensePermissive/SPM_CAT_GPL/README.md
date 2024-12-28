07d3a1630ccc56d1c59ef80997931eeb7136c440
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2024-12-28 20:11:13 +0100

    #1858 Fix TotalGM->Total & remove TotalWM

--- External/Atlases4ROIs/LicensePermissive/SPM_CAT_GPL/README.md
+++ External/Atlases4ROIs/LicensePermissive/SPM_CAT_GPL/README.md
@@ -4,4 +4,4 @@ To differentiate from /External/SPMmodified/MapsAdded, which have the same licen
 
 DeepWM.nii: ICBM_152_nonlinear_symm pWM eroded to deep WM & resampled to 1.5 mm MNI
 Supratentorial_GM_WM.nii: same as ICBM_152_nonlinear_symm brainmask, resampled to 1.5 mm MNI (/External/SPM_modified/MapsAdded/brainmask.nii), but without structures outside the cerebrum. For which we excluded the following Mindboggle regions: 1 cerebellum exterior, 2 cerebellum WM, 10 ventral dentate nucleus, 11 basal forebrain, 12-14 cerebellar vermis. (see /CustomScripts/Atlases/Supratentorial.m; private ExploreASL repository)
-TotalGM.nii, TotalWM.nii, WholeBrain.nii: ICBM wholebrain mask, resampled to 1.5 mm MNI
+Total.nii, WholeBrain.nii: ICBM wholebrain mask, resampled to 1.5 mm MNI
--- WorkInProgress/Administration/xASL_adm_PreparePediatricTPM.m
+++ WorkInProgress/Administration/xASL_adm_PreparePediatricTPM.m
@@ -219,7 +219,7 @@ for ii=1:length(nameTPM)
 	end	
 	
 	% Copy normal files
-	listFile = {'TotalGM.tsv' 'TotalWM.tsv' 'WholeBrain.tsv' 'MNI_Structural.tsv' 'LeftRight.tsv' 'LabelColors.mat' 'GhostSignalRatio.tsv' 'DeepWM.tsv' 'Identity_sn.mat' 'Identity_Deformation_y_T1.nii.gz'...
+	listFile = {'Total.tsv' 'WholeBrain.tsv' 'MNI_Structural.tsv' 'LeftRight.tsv' 'LabelColors.mat' 'GhostSignalRatio.tsv' 'DeepWM.tsv' 'Identity_sn.mat' 'Identity_Deformation_y_T1.nii.gz'...
 		        fullfile('VascularTerritories','TatuICA_PCA.tsv'),fullfile('VascularTerritories','LabelingTerritories.tsv'),fullfile('VascularTerritories','CortVascTerritoriesTatu.tsv'),fullfile('VascularTerritories','ATTbasedFlowTerritories.tsv')};
 	for jj=1:length(listFile)
 		xASL_Copy(fullfile(pathMaps,listFile{jj}), fullfile(pathMaps,nameTPM{ii},listFile{jj}),true);
