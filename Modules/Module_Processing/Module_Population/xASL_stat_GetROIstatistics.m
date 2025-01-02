81fbca17639274f717faa8d4cceb44c46d0b47ca
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2025-01-02 15:27:59 +0100

    #1869 xASL_stat_GetROIstatistics: improve warnings empty ROIs

--- Modules/Module_Processing/Module_Population/xASL_stat_GetROIstatistics.m
+++ Modules/Module_Processing/Module_Population/xASL_stat_GetROIstatistics.m
@@ -756,11 +756,11 @@ for iSubject=1:x.dataset.nSubjects
 
                 % Now check for empty masks
                 if xASL_stat_SumNan(CurrentMaskNotVascular(:)) == 0
-                    fprintf('%s\n', ['*** Empty mask for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
+                    fprintf('%s\n', ['* Empty ' x.S.TissueMaskingLocal{1} ' CBF mask for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
                 elseif xASL_stat_SumNan(pvPrimary(:)) == 0
-                    fprintf('%s\n', ['*** Empty pv' pvPrimaryName ' for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
+                    fprintf('%s\n', ['* Empty pv' pvPrimaryName ' for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
                 elseif xASL_stat_SumNan(pvSecondary(:)) == 0
-                    fprintf('%s\n', ['*** Empty pv' pvSecondaryName ' for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
+                    fprintf('%s\n', ['* Empty pv' pvSecondaryName ' for ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]);
                 else                    
 
                     %% CoV
@@ -784,7 +784,7 @@ for iSubject=1:x.dataset.nSubjects
                         % Now check again for empty mask (as it was
                         % masked now also with a vascular artifact
                         % mask)
-                        fprintf('%s\n', ['Empty CBF mask for subject ' xASL_num2str(iSubject) '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI)]); % slightly different warning/mask as above for sCoV
+                        fprintf('%s\n', ['* Empty ' x.S.TissueMaskingLocal{1} ' non-vascular CBF mask for subject ' x.SUBJECTS{iSubject} '_ASL_' xASL_num2str(iSess) ', ROI ' xASL_num2str(iROI) ':' namesROIuse{iROI}]); % slightly different warning/mask as above for sCoV
                     else
 
                         % Visualization first (this differs from sCoV only by the vascular mask)
