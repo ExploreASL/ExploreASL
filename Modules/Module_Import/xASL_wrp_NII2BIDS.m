94c4cad7e2d538b6b39e6c0fddf43d7782a8fe73
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2025-03-25 20:14:39 +0100

    #1881 xASL_wrp_NII2BIDS: code simplification

--- Modules/Module_Import/xASL_wrp_NII2BIDS.m
+++ Modules/Module_Import/xASL_wrp_NII2BIDS.m
@@ -61,7 +61,7 @@ function x = xASL_wrp_NII2BIDS(x)
 	% The name always has to be assigned as it is used in the DatasetDescription
 	
 	if isfield(studyParAll,'StudyPars')
-		% For multi-studyParameter studyPar - add this to the first studyPar
+		% For multi-studyParameter studyPar - add the studyID name this to the first studyPar
 		if ~isfield(studyParAll.StudyPars{1},'Name')
 			studyParAll.StudyPars{1}.Name = x.modules.import.imPar.studyID;
 		end
@@ -77,11 +77,9 @@ function x = xASL_wrp_NII2BIDS(x)
 	datasetDescription = xASL_bids_CreateDatasetDescriptionTemplate(studyParFirstParameters, x.Version);
 	
 	% Make the output directory and save the description
-    if ~xASL_exist(x.modules.import.imPar.BidsRoot,'dir')
-        xASL_adm_CreateDir(x.modules.import.imPar.BidsRoot);
-    end
+    xASL_adm_CreateDir(x.modules.import.imPar.BidsRoot);
 	
-	xASL_io_WriteJson(fullfile(x.modules.import.imPar.BidsRoot,[bidsPar.datasetDescription.filename '.json']),datasetDescription);
+	xASL_io_WriteJson(fullfile(x.modules.import.imPar.BidsRoot, [bidsPar.datasetDescription.filename '.json']), datasetDescription);
 	
 	%% 3. Go through all subjects and check all the M0 and ASLs and modify the JSONs
 	% This step should be completely automatic, just taking the info filled above and using it to convert to full BIDS.
