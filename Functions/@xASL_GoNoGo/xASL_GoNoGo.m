ee50a4056afd9fc7e4e023d93d56064b19b7cfe0
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2026-03-02 22:00:02 +0100

    #2012 xASL_GoNoGo: add Apache 2.0

--- Functions/@xASL_GoNoGo/xASL_GoNoGo.m
+++ Functions/@xASL_GoNoGo/xASL_GoNoGo.m
@@ -22,7 +22,10 @@
 %
 % __________________________________
 % Copyright (c) 2015-2023 ExploreASL
-%
+% Licensed under Apache 2.0, see permissions and limitations at
+% https://github.com/ExploreASL/ExploreASL/blob/main/LICENSE
+% you may only use this file in compliance with the License.
+% __________________________________
 
 classdef xASL_GoNoGo < handle
     properties
