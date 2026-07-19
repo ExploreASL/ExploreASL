8d7443ae20f4cc37e7d521f6e5226c0b6953e654
Henk Mutsaerts <h.j.mutsaerts@amsterdamumc.nl>
2026-07-19 20:52:31 +0200

    #2103 ExploreASL_Process: allow running xASL_module_DCE

--- ExploreASL_Process.m
+++ ExploreASL_Process.m
@@ -133,7 +133,13 @@ function [x] = ExploreASL_Process(x)
         % Use this module for additional additional -subject registration/creating templates
         [~, x] = xASL_init_Iteration(x,'xASL_module_DARTEL');
     end
-    
+
+    if isfield(x.modules, 'bRunDCE') && x.modules.bRunDCE
+        addpath(genpath('WorkInProgress'));
+        xASL_init_Iteration(x,'xASL_module_dce');
+    end
+
+
     % -----------------------------------------------------------------------------
     %% 2    xASL_module_ASL  
     if x.opts.bProcess(2)==1
