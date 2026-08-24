9c62abd449e04140817bd73422e6e7012616c92e
Jan Petr <j.petr@hzdr.de>
2026-08-24 16:14:48 +0200

    #2146 imp_DCM2NII_ReorderTE Minor fix

--- Modules/Module_Import/DCM2NII/xASL_imp_DCM2NII_ReorderTimeEncoded.m
+++ Modules/Module_Import/DCM2NII/xASL_imp_DCM2NII_ReorderTimeEncoded.m
@@ -48,7 +48,8 @@ if numel(nii_files)>=1
 				% This is only relevant for non gammastar sequence versions. For gammaStar, this step can be skipped. 
 				% Note that there are two different DICOM tags that can be used to identify gammaStar - both are checked in case some were removed during data anonymization
 				if (~isfield(resultJSON, 'PulseSequenceDetails') || isempty(regexpi(resultJSON.PulseSequenceDetails, 'fme_gammastar', 'once'))) &&...
-			       (~isfield(resultJSON, 'SequenceName') || isempty(regexpi(resultJSON.SequenceName, 'gammastar', 'once')))
+			       (~isfield(resultJSON, 'SequenceName') || isempty(regexpi(resultJSON.SequenceName, 'gammastar', 'once'))) &&...
+				   (~isfield(resultJSON, 'PulseSequenceName') || isempty(regexpi(resultJSON.PulseSequenceName, 'gammastar', 'once')))
 					if numel(vectorPLD) > numberPLDs
 						for iRepetition = 2:floor(numel(vectorPLD)/numberPLDs)
 							if ~isequal(vectorPLD(1:numberPLDs),vectorPLD((1:numberPLDs)+numberPLDs*(iRepetition-1)))
