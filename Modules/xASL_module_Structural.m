4a77f2ecf971f85e4cf4fe78703acb2cc2367eb4
Jan Petr <j.petr@hzdr.de>
2026-09-10 19:42:04 +0200

    #2135 Reslicing and deleting fix

--- Modules/xASL_module_Structural.m
+++ Modules/xASL_module_Structural.m
@@ -430,8 +430,8 @@ end
 %% 8    Resample to standard space
 iState = 8;
 FileMissing = false; % Check availability NIfTIs in standard space, else redo warping
-List2CheckNativeSpace = {x.P.Path_T1,        x.P.Path_T1,         x.P.Path_T2,     x.P.Path_T1c};   % Source path
-List2Check =            {x.P.Pop_Path_PV_pGM x.P.Pop_Path_PV_pWM  x.P.Pop_Path_rT2 x.P.Pop_Path_rT1c}; % Population path
+List2CheckNativeSpace = {x.P.Path_T1,         x.P.Path_T1,         x.P.Path_T1,      x.P.Path_T2,      x.P.Path_T1c};   % Source path
+List2Check =            {x.P.Pop_Path_PV_pGM, x.P.Pop_Path_PV_pWM, x.P.Pop_Path_rT1, x.P.Pop_Path_rT2, x.P.Pop_Path_rT1c}; % Population path
 for iCheck=1:length(List2Check)
     if xASL_exist(List2CheckNativeSpace{iCheck},'file') && ~xASL_exist(List2Check{iCheck},'file')
 		% If the source file in the native space exist and the file in the population folder doesn't
